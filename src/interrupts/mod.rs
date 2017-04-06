73b16967846d86abb2139ac78a3bf47248e05e3a
Kevin Boos <kevinaboos@gmail.com>
2017-04-06 15:52:57 -0500

    fixed interrupt table indexing issue

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -22,31 +22,51 @@ lazy_static! {
     static ref IDT: Idt = {
         let mut idt = ::x86_64::structures::idt::Idt::new();
 
-		// set up exceptions
+		// SET UP FIXED EXCEPTION HANDLERS
         idt.divide_by_zero.set_handler_fn(divide_by_zero_handler);
+        // missing: 0x01 debug exception
+        // missing: 0x02 non-maskable interrupt exception
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        // missing: 0x04 overflow exception
+        // missing: 0x05 bound range exceeded exception
         idt.invalid_opcode.set_handler_fn(invalid_opcode_handler);
-        idt.page_fault.set_handler_fn(page_fault_handler);
-        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
-        
-        
-        // fill all IDT entries with an unimplemented IRQ handler
-        //FIXME:  this should be from 32..255, but 224 doesn't work for some reason
-        for i in 32..224 {
-	        idt.interrupts[i].set_handler_fn(unimplemented_interrupt_handler);
-//	        println!("set interrupt {}", i);
-        }
-        
-        // set our custom interrupts 
-        idt.interrupts[0x0 /* 0x20 */].set_handler_fn(timer_handler); // int 32
-        idt.interrupts[0x1 /* 0x21 */].set_handler_fn(keyboard_handler); // int 33
-	
+        idt.device_not_available.set_handler_fn(device_not_available_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(DOUBLE_FAULT_IST_INDEX as u16); // use a special stack for the DF handler
         }
+        // reserved: 0x09 coprocessor segment overrun exception
+        // missing: 0x0a invalid TSS exception
+        idt.segment_not_present.set_handler_fn(segment_not_present_handler);
+        // missing: 0x0c stack segment exception
+        // missing: 0x0d general protection exception
+        idt.page_fault.set_handler_fn(page_fault_handler);
+        // reserved: 0x0f vector 15
+        // missing: 0x10 floating point exception 
+        // missing: 0x11 alignment check exception
+        // missing: 0x12 machine check exception
+        // missing: 0x13 SIMD floating point exception
+        // missing: 0x14 virtualization vector 20
+        // missing: 0x15 - 0x1d SIMD floating point exception
+        // missing: 0x1e security exception
+        // reserved: 0x1f 
+        
+        
+        // fill all IDT entries with an unimplemented IRQ handler
+        for i in 32..255 {
+	        idt[i].set_handler_fn(unimplemented_interrupt_handler);
+        }
+        
+        
+        
+		// SET UP CUSTOM INTERRUPT HANDLERS 
+		// we can directly index the "idt" object because it implements the Index/IndexMut traits
+        idt[0x20].set_handler_fn(timer_handler); // int 32
+        idt[0x21].set_handler_fn(keyboard_handler); // int 33
+	
+        
 
-        idt
+        idt // return idt so it's set to the static ref IDT above
     };
 }
 
@@ -111,24 +131,43 @@ pub fn init(memory_controller: &mut MemoryController) {
 //
 //define_all_interrupt_handlers!();
 
-
+/// interrupt 0x00
 extern "x86-interrupt" fn divide_by_zero_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("\nEXCEPTION: DIVIDE BY ZERO\n{:#?}", stack_frame);
     loop {}
 }
 
+/// interrupt 0x03
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("\nEXCEPTION: BREAKPOINT at {:#x}\n{:#?}",
              stack_frame.instruction_pointer,
              stack_frame);
 }
 
+/// interrupt 0x06
 extern "x86-interrupt" fn invalid_opcode_handler(stack_frame: &mut ExceptionStackFrame) {
     println!("\nEXCEPTION: INVALID OPCODE at {:#x}\n{:#?}",
              stack_frame.instruction_pointer,
              stack_frame);
     loop {}
 }
+
+/// interrupt 0x07
+/// see this: http://wiki.osdev.org/I_Cant_Get_Interrupts_Working#I_keep_getting_an_IRQ7_for_no_apparent_reason
+extern "x86-interrupt" fn device_not_available_handler(stack_frame: &mut ExceptionStackFrame) {
+    println!("\nEXCEPTION: DEVICE_NOT_AVAILABLE at {:#x}\n{:#?}",
+             stack_frame.instruction_pointer,
+             stack_frame);
+    
+	// TODO: handle this
+	/* When any IRQ7 is received, simply read the In-Service Register
+		 outb(0x20, 0x0B); unsigned char irr = inb(0x20);
+		and check if bit 7
+		irr & 0x80
+		is set. If it isn't, then return from the interrupt without sending an EOI.
+	*/
+}
+
 
 extern "x86-interrupt" fn page_fault_handler(stack_frame: &mut ExceptionStackFrame, error_code: PageFaultErrorCode) {
     use x86_64::registers::control_regs;
@@ -147,7 +186,8 @@ extern "x86-interrupt" fn double_fault_handler(stack_frame: &mut ExceptionStackF
 
 
 
-
+/// this shouldn't really ever happen, but I added the handler anyway
+/// because I noticed the interrupt 0xb happening when other interrupts weren't properly handled
 extern "x86-interrupt" fn segment_not_present_handler(stack_frame: &mut ExceptionStackFrame, error_code: u64) {
     use x86_64::registers::control_regs;
     println!("\nEXCEPTION: SEGMENT_NOT_PRESENT FAULT\nerror code: \
@@ -168,6 +208,9 @@ extern "x86-interrupt" fn timer_handler(stack_frame: &mut ExceptionStackFrame) {
 
 extern "x86-interrupt" fn keyboard_handler(stack_frame: &mut ExceptionStackFrame) {
 	println!("\nKEYBOARD interrupt:\n{:#?}", stack_frame);
+	
+	
+	
 	unsafe { PIC.lock().notify_end_of_interrupt(0x21u8); }
 }
 
@@ -177,110 +220,3 @@ extern "x86-interrupt" fn unimplemented_interrupt_handler(stack_frame: &mut Exce
 	
 }
 
-
-
-///////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////
-//////////////////////        from toyos             //////////////////////
-///////////////////////////////////////////////////////////////////////////
-
-/*
-
-/// Various data available on our stack when handling an interrupt.
-///
-/// Only `pub` because `rust_interrupt_handler` is.
-#[repr(C, packed)]
-pub struct InterruptContext {
-    rsi: u64,
-    rdi: u64,
-    r11: u64,
-    r10: u64,
-    r9: u64,
-    r8: u64,
-    rdx: u64,
-    rcx: u64,
-    rax: u64,
-    int_id: u32,
-    _pad_1: u32,
-    error_code: u32,
-    _pad_2: u32,
-}
-
-
-/// Print our information about a CPU exception, and loop.
-fn cpu_exception_handler(ctx: &InterruptContext) {
-
-    // Print general information provided by x86::irq.
-    println!("{}, error 0x{:x}",
-             x86::irq::EXCEPTIONS[ctx.int_id as usize],
-             ctx.error_code);
-
-    // Provide detailed information about our error code if we know how to
-    // parse it.
-    match ctx.int_id {
-        14 => {
-            let err = x86::irq::PageFaultError::from_bits(ctx.error_code);
-            println!("{:?}", err);
-        }
-        _ => {}
-    }
-
-    loop {}
-}
-
-/// Called from our assembly-language interrupt handlers to dispatch an
-/// interrupt.
-#[no_mangle]
-pub unsafe extern "C" fn rust_interrupt_handler(ctx: &InterruptContext) {
-    match ctx.int_id {
-        0x00...0x0F => cpu_exception_handler(ctx),
-        0x20 => { } // timer
-        0x21 => {
-            if let Some(input) = keyboard::read_char() {
-                if input == '\r' {
-                    println!("");
-                } else {
-                    print!("{}", input);
-                }
-            }
-        }
-        0x80 => println!("Not actually Linux, sorry."),
-        _ => {
-            println!("UNKNOWN INTERRUPT #{}", ctx.int_id);
-            loop {}
-        }
-    }
-
-    PICS.lock().notify_end_of_interrupt(ctx.int_id as u8);
-}
-
-
-
-
-
-//=========================================================================
-//  Initialization
-
-/// Use the `int` instruction to manually trigger an interrupt without
-/// actually using `sti` to enable interrupts.  This is highly recommended by
-/// http://jvns.ca/blog/2013/12/04/day-37-how-a-keyboard-works/
-#[allow(dead_code)]
-pub unsafe fn test_interrupt() {
-    println!("Triggering interrupt.");
-    int!(0x80);
-    println!("Interrupt returned!");
-}
-
-/// Platform-independent initialization.
-pub unsafe fn initialize() {
-    PICS.lock().initialize();
-    IDT.lock().initialize();
-
-    // Enable this to trigger a sample interrupt.
-    test_interrupt();
-
-    // Turn on real interrupts.
-    x86::irq::enable();
-}
-
-*/
