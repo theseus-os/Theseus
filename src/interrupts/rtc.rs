fe512e8199ebc8339b29771a89f1dc5e24ffe1ad
AryanSef <aryansefidi@gmail.com>
2017-05-25 11:38:56 -0500

    I had made some errors in mod.rs and pit_clock.rs

--- src/interrupts/rtc.rs
+++ src/interrupts/rtc.rs
@@ -1,16 +1,20 @@
 //can be used to read and write to ports, look in docs.rs for details. 
-use cpuio::Port;
+//extern crate cpuio;
 //access to registers? x86_64 crate is probably needed but have to find which  part is relevant
 use x86_64::registers;
 
 
-//initializing variables to be read from RTC
+
 fn main(){
+    
+    //initializing variables to be read from RTC
     let mut second: u64 = 0;
     let mut minute: u64 = 0;
     let mut hour: u64 = 0;
     let mut day: u64 = 0;
     let mut month: u64 = 0;
     let mut year: u64 = 0;
+
+
 }
 
