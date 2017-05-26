304daa5f5412c172d14a75bc6b4320e09dc1c3fd
AryanSef <aryansefidi@gmail.com>
2017-05-26 11:28:45 -0500

    created rtc file and began adding register reading

--- src/interrupts/rtc.rs
+++ src/interrupts/rtc.rs
@@ -2,10 +2,12 @@
 //extern crate cpuio;
 //access to registers? x86_64 crate is probably needed but have to find which  part is relevant
 use x86_64::registers;
+use spin::{Mutex, Once};
+use port_io::InOut;
 
 
 
-fn main(){
+pub fn read_rtc(){
     
     //initializing variables to be read from RTC
     let mut second: u64 = 0;
@@ -14,6 +16,11 @@ fn main(){
     let mut day: u64 = 0;
     let mut month: u64 = 0;
     let mut year: u64 = 0;
+
+    //set register
+    InOut::port_out(0x70, 0x02);
+    //read register
+    second = InOut::port_in(0x71: u8);
 
 
 }
