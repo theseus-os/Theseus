9daf5847fa25d33ecbebbbfd181418f5d06223d0
Kevin Boos <kevinaboos@gmail.com>
2017-08-28 04:45:26 -0500

    interrupts aren't being properly enabled when returning via sysret, which is weird. But interrupts do work inside the syscall handler itself now. TODO: change  jump_to_userspace from using iret to using sysret

--- userspace/syscall_test/syscall_test.s
+++ userspace/syscall_test/syscall_test.s
@@ -10,15 +10,32 @@ section .text
     ; r9  -- fifth argument 
     ; r8  -- sixth argument
     
-main:
-    mov rax, 0
-    mov rdi, 1
-    mov rsi, 2
-    mov rdx, 3
-    mov r10, 4
-    mov r9 , 5
-    mov r8 , 6
+    mov rbx, 0
 
+main:
+    mov rax, rbx ; rbx is holding ground/accumulator for syscall num
+    mov rdi, 20
+    mov rsi, 30
+    mov rdx, 40
+    mov r10, 50
+    mov r9 , 60
+    mov r8 , 70
+
+    push rbx
     syscall
 
-jmp $ ; infinite loop
+    pop rbx
+    add rbx, 1 ; syscall num increments each time for easy tracking
+    ; mov rcx, 0x40000000
+    mov rcx, 0x400000
+
+loopstart:
+    
+    add rax,  1
+
+    dec rcx
+    jnz loopstart
+
+
+    ;  infinite loop
+    jmp main
