ba4d80773581d0680c202dcf62308eab476117f2
Kevin Boos <kevinaboos@gmail.com>
2017-10-13 18:42:44 -0500

    first attempt at using the state_store, it doesn't quite work

--- kernel/vga_buffer/src/lib.rs
+++ kernel/vga_buffer/src/lib.rs
@@ -4,14 +4,18 @@
 
 #![no_std]
 #![feature(collections)]
+#![feature(alloc)]
 #![feature(const_fn)]
 #![feature(unique)]
 
 extern crate spin;
 extern crate volatile;
+extern crate alloc;
 extern crate collections;
 extern crate serial_port;
 extern crate kernel_config;
+extern crate state_store;
+#[macro_use] extern crate lazy_static;
 
 use core::ptr::Unique;
 use core::fmt;
@@ -20,7 +24,7 @@ use volatile::Volatile;
 use collections::string::String;
 use serial_port::serial_out;
 use kernel_config::memory::KERNEL_OFFSET;
-
+use state_store::{SSCached, get_state, insert_state};
 
 /// defined by x86's physical memory maps
 const VGA_BUFFER_PHYSICAL_ADDR: usize = 0xb8000;
@@ -30,10 +34,17 @@ const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
 
 
-static VGA_WRITER: Mutex<VgaWriter> = Mutex::new(VgaWriter {
-    column_position: 0,
-    buffer: unsafe { Unique::new((VGA_BUFFER_PHYSICAL_ADDR + KERNEL_OFFSET) as *mut _) },
-});
+lazy_static! {
+    static ref VGA_WRITER: SSCached<Mutex<VgaWriter>> = {
+        let vga_writer = VgaWriter {
+            column_position: 0,
+            buffer: unsafe { Unique::new((VGA_BUFFER_PHYSICAL_ADDR + KERNEL_OFFSET) as *mut _) },
+        };
+        insert_state(Mutex::new(vga_writer));
+        get_state::<Mutex<VgaWriter>>()
+    };
+}
+
 
 
 /// This is UNSAFE because it bypasses the VGA Buffer lock. Use print!() instead. 
@@ -48,8 +59,13 @@ macro_rules! print_unsafe {
 
 #[doc(hidden)]
 pub fn print_args_unsafe(args: fmt::Arguments) -> fmt::Result {
-    unsafe { VGA_WRITER.force_unlock(); }
-    print_args(args)
+    if let Some(writer) = VGA_WRITER.get() {
+        unsafe{ writer.force_unlock() };
+        print_args(args)
+    }
+    else {
+        Err(fmt::Error)
+    }
 }
 
 /// This is UNSAFE because it bypasses the VGA Buffer lock. Use println!() instead. 
@@ -69,30 +85,45 @@ pub fn print_string(s: &String) -> fmt::Result {
 
 pub fn print_str(s: &str) -> fmt::Result {
     use core::fmt::Write;
-    VGA_WRITER.lock().write_str(s)
+    if let Some(writer) = VGA_WRITER.get() {
+        writer.lock().write_str(s)
+    }
+    else {
+        Err(fmt::Error)
+    }
 }
 
 pub fn print_args(args: fmt::Arguments) -> fmt::Result {
     use core::fmt::Write;
-    VGA_WRITER.lock().write_fmt(args)
+    if let Some(writer) = VGA_WRITER.get() {
+        writer.lock().write_fmt(args)
+    }
+    else {
+        Err(fmt::Error)
+    }
 }
 
-pub fn clear_screen() {
-    let mut locked_Vgawriter = VGA_WRITER.lock(); 
-    for _ in 0..BUFFER_HEIGHT {
-        locked_Vgawriter.new_line();
+pub fn clear_screen() -> Result<(), ()> {
+    if let Some(writer) = VGA_WRITER.get() {
+        let mut locked_vgawriter = writer.lock(); 
+        for _ in 0..BUFFER_HEIGHT {
+            locked_vgawriter.new_line();
+        }
+        Ok(())
+    }
+    else {
+        Err(())
     }
 }
 
 pub fn show_splash_screen() {
-    print_str(WELCOME_STRING);
+    let _ = print_str(WELCOME_STRING);
 }
 
 pub struct VgaWriter {
     column_position: usize,
     buffer: Unique<Buffer>,
 }
-
 impl VgaWriter {
     pub fn write_byte_with_color(&mut self, byte: u8, color_code: ColorCode) {
         match byte {
---
+++ libs/atomic_linked_list/src/atomic_map.rs
@@ -0,0 +1,214 @@
+//! A generic, singly-linked list data structure that is lock-free 
+//! and uses `AtomicPtr`s to ensure safety in the face of multithreaded access.
+//! Each node remains in the same spot in memory once it is allocated,
+//! and will not be reallocated,
+//! which allows an external thread to maintain a reference to it safely 
+//! (but really, only a Weak reference is safe to maintain, to catch possible Node deletion).
+//!
+//! New elements are inserted at the head of the list, and then the head's next pointer 
+//! is set up to the point to the node that was previously the head. 
+//! Thus, the head always points to the most recently added node. 
+
+
+use alloc::boxed::Box;
+use core::sync::atomic::{AtomicPtr, Ordering};
+use core::marker::PhantomData;
+
+
+struct Node<K, V> where K: PartialEq {
+    key: K,
+	value: V,
+    next: AtomicPtr<Node<K, V>>,
+}
+impl<K, V> Node<K, V> where K: PartialEq {
+    fn new(k: K, v: V) -> Node<K, V> {
+        Node { 
+            key: k,
+			value: v,
+            next: AtomicPtr::default(), // null ptr by default
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AtomicMap<K, V> where K: PartialEq {
+    head: AtomicPtr<Node<K, V>>,
+}
+
+impl<K, V> AtomicMap<K, V> where K: PartialEq {
+    /// create a new empty AtomicMap.
+    pub fn new() -> AtomicMap<K, V> {
+        AtomicMap {
+            head: AtomicPtr::default(), // null ptr
+        }
+    }
+
+    /// Adds a new key-value pair to the map. 
+	/// If the given key is already present, its corresponding value will be overwritten.
+    pub fn insert(&self, key: K, value: V) -> Option<V> {
+        let res = self.insert_timeout(key, value, u64::max_value());
+        res.unwrap_or(None) // return the Option<V> in Ok(), or None if Err()
+    }
+
+    
+	/// Adds a new key-value pair to the map. 
+	/// If the given key is already present, its corresponding value will be overwritten.
+	/// If it fails to do so atomically after the given number of attempts, it will abort and return Err.
+    pub fn insert_timeout(&self, key: K, value: V, max_attempts: u64) -> Result<Option<V>, ()> {
+
+		// first, we check to see if the key exists in the list already.
+		// if it does, simply update the corresponding value.
+		for pair in self.iter_mut() {
+			if key == *pair.0 {
+                let old_val = ::core::mem::replace(&mut *pair.1, value);
+				return Ok(Some(old_val));
+			}
+		}
+
+
+		// here, if the key did not exist, add a new node including that key-value pair
+        let mut node_ptr = Box::into_raw(Box::new(Node::new(key, value))); // we must wrap Nodes in Box to keep them around
+
+        let max_attempts = if max_attempts == 0 { 1 } else { max_attempts }; // ensure we try at least once 
+        for _attempt in 0..max_attempts {
+            // start the attempt by grabbing the head value
+            let orig_head_ptr = self.head.load(Ordering::Acquire);
+
+            // the new "node" will become the new head, so set the node's next pointer to orig_head_ptr
+            // SAFE: we know the node_ptr is valid since we just created it above
+            unsafe {
+                (*node_ptr).next = AtomicPtr::new(orig_head_ptr);
+            }
+
+            // now try to atomically swap the new node ptr into the current head ptr
+            let prev_stored_ptr = self.head.compare_and_swap(orig_head_ptr, node_ptr, Ordering::AcqRel); 
+            
+            // if compare_and_swap returns the same value we orig_head_ptr, then it was properly updated
+            // we do this so we can check if another process 
+            if prev_stored_ptr == orig_head_ptr {
+                // it worked! i.e., no other process snuck in and changed head while we were setting up node.next
+                return Ok(None);
+            }
+            else {
+                // it didn't work, the head value wasn't updated, meaning that another process updated it before we could
+                // so we need to start over by reading the head ptr again and trying to swap it in again
+                #[test]
+                println!("        attempt {}", _attempt);
+            }
+        }
+
+        // clean up the unboxed node and allow it to be dropped
+        // SAFE: no one has touched this node except for us when we created it above
+        unsafe {
+            let _ = Box::from_raw(node_ptr);
+        }
+
+        Err(())
+    }
+
+	/// Returns a reference to the value matching the given key, if present. 
+	/// Otherwise, returns None. 
+	pub fn get(&self, key: K) -> Option<&V> {
+		for pair in self.iter() {
+			if key == *pair.0 {
+				return Some(pair.1);
+			}
+		}
+		None
+	}
+
+    /// Returns a forward iterator through this linked list. 
+    pub fn iter(&self) -> AtomicMapIter<K, V> {
+        AtomicMapIter {
+            curr: &self.head, //load(Ordering::Acquire),
+            // _phantom: PhantomData,
+        }
+    }
+
+    /// returns a forward iterator through this linked list,
+    /// allowing mutation of inner values but not keys.
+    pub fn iter_mut(&self) -> AtomicMapIterMut<K, V> {
+        AtomicMapIterMut {
+            curr: &self.head, //load(Ordering::Acquire),
+            // _phantom: PhantomData,
+        }
+    }
+}
+
+
+
+pub struct AtomicMapIter<'a, K: PartialEq + 'a, V: 'a> {
+    curr: &'a AtomicPtr<Node<K, V>>,
+    // _phantom: PhantomData<&'a K, V>, // we don't need this with the &'a above
+}
+impl<'a, K: PartialEq + 'a, V: 'a> Iterator for AtomicMapIter<'a, K, V> {
+    type Item = (&'a K, &'a V); 
+
+    fn next(&mut self) -> Option<(&'a K, &'a V)> {
+        let curr_ptr = self.curr.load(Ordering::Acquire);
+        if curr_ptr == (0 as *mut _) {
+            return None;
+        }
+        // SAFE: curr_ptr was checked for null
+        let curr_node: &Node<K, V> = unsafe { &*curr_ptr };
+        self.curr = &curr_node.next; // advance the iterator
+        Some((&curr_node.key, &curr_node.value))
+    }
+}
+
+
+
+pub struct AtomicMapIterMut<'a, K: PartialEq + 'a, V: 'a> {
+    curr: &'a AtomicPtr<Node<K, V>>,
+    // _phantom: PhantomData<&'a K, V>, // we don't need this with the &'a above
+}
+impl<'a, K: PartialEq + 'a, V: 'a> Iterator for AtomicMapIterMut<'a, K, V> {
+    type Item = (&'a K, &'a mut V); 
+
+    fn next(&mut self) -> Option<(&'a K, &'a mut V)> {
+        let curr_ptr = self.curr.load(Ordering::Acquire);
+        if curr_ptr == (0 as *mut _) {
+            return None;
+        }
+        // SAFE: curr_ptr was checked for null
+        let curr_node: &mut Node<K, V> = unsafe { &mut *curr_ptr };
+        self.curr = &curr_node.next; // advance the iterator
+        Some((&curr_node.key, &mut curr_node.value))
+    }
+}
+
+
+#[test]
+/// To run this test, execute: `cargo test test_map -- --nocapture`
+fn test_map() {
+    use alloc::arc::Arc;
+    use std::thread;
+
+    let list: AtomicMap<&'static str, u64> = AtomicMap::new();
+    
+	let should_be_none = list.get("yo");
+	println!("should_be_none = {:?}", should_be_none);
+
+	list.insert("yo", 2); 
+
+	println!("after yo 2");
+    for i in list.iter() {
+        println!("    {:?}", i);
+    }
+
+	list.insert("hi", 45);
+	let old_yo = list.insert("yo", 1234); 
+    println!("old_yo = {:?}", old_yo);
+
+	println!("after yo 4");
+    for i in list.iter() {
+        println!("    {:?}", i);
+    }
+
+	let should_be_45 = list.get("hi");
+	println!("should_be_45 = {:?}", should_be_45);
+
+    let should_now_be_1234 = list.get("yo");
+    println!("should_now_be_1234 = {:?}", should_now_be_1234);
+    
+}
