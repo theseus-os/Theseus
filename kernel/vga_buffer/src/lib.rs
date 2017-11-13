d9c63f97ac4cde6bcf2820e05939213d34a24cf3
Kevin Boos <kevinaboos@gmail.com>
2017-11-13 17:57:16 -0600

    reworked TSC code, much cleaner now

--- kernel/vga_buffer/src/lib.rs
+++ kernel/vga_buffer/src/lib.rs
@@ -245,10 +245,9 @@ struct Buffer {
 
 // this doesn't line up as shown here because of the escaped backslashes,
 // but it lines up properly when printed :)
-static WELCOME_STRING: &'static str = "\n
-    ____           _    __       _        ___  ____  
-   |  _ \\ ___  ___| |_ / _|_   _| |      / _ \\/ ___| 
-   | |_) / _ \\/ __| __| |_| | | | |     | | | \\___ \\ 
-   |  _ <  __/\\__ \\ |_|  _| |_| | |     | |_| |___) |
-   |_| \\_\\___||___/\\__|_|  \\__,_|_|      \\___/|____/ \n";
-                                               
+const WELCOME_STRING: &'static str = "\n\n
+ _____ _                              
+|_   _| |__   ___  ___  ___ _   _ ___ 
+  | | | '_ \\ / _ \\/ __|/ _ \\ | | / __|
+  | | | | | |  __/\\__ \\  __/ |_| \\__ \\
+  |_| |_| |_|\\___||___/\\___|\\__,_|___/ \n\n";
