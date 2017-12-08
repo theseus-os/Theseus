98a5684c09664e09cf202416b01c7befcc3b4410
Kevin Boos <kevinaboos@gmail.com>
2017-12-08 16:58:57 -0600

    overhauled Makefiles and build system structure. Now we accommodate unarchiving .rlib files and moving the inner object file to the grub module directory.

--- kernel/test_lib/src/lib.rs
+++ kernel/test_lib/src/lib.rs
@@ -1,4 +1,5 @@
 #![no_std]
+// #![feature(lang_items)]
 
 // #[macro_use] extern crate vga_buffer; // for println_unsafe!
 // #[macro_use] extern crate log;
@@ -7,3 +8,34 @@ pub fn test_lib_func(arg: u8) -> u8 {
 	// warn!("yo in pub func!");
 	arg * 10
 }
+
+pub struct DeezNuts {
+	item1: u32,
+	item2: u64,
+}
+impl DeezNuts {
+	pub fn new(i1: u32, i2: u64) -> DeezNuts {
+		DeezNuts {
+			item1: i1,
+			item2: i2,
+		}
+	}
+}
+
+
+// #[cfg(not(test))]
+// #[lang = "eh_personality"]
+// extern "C" fn eh_personality() {}
+
+
+// #[cfg(not(test))]
+// #[lang = "panic_fmt"]
+// #[no_mangle]
+// pub extern "C" fn panic_fmt(_fmt: core::fmt::Arguments, _file: &'static str, _line: u32) -> ! {
+//     // println_unsafe!("\n\nPANIC in {} at line {}:", file, line);
+//     // println_unsafe!("    {}", fmt);
+
+//     // TODO: check out Redox's unwind implementation: https://github.com/redox-os/kernel/blob/b364d052f20f1aa8bf4c756a0a1ea9caa6a8f381/src/arch/x86_64/interrupt/trace.rs#L9
+
+//     loop {}
+// }
