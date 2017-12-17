d9e69360a26dc8e9cd2e23806958432e36c8e1f5
Kevin Boos <kevinaboos@gmail.com>
2017-12-17 15:39:12 -0600

    loading of rodata sections and PC32 relative relocations work!

--- kernel/nano_core/src/mod_mgmt/metadata.rs
+++ kernel/nano_core/src/mod_mgmt/metadata.rs
@@ -14,6 +14,7 @@ lazy_static! {
             Mutex::new(BTreeMap::new());
 }
 
+#[derive(Debug)]
 pub struct LoadedCrate {
     pub crate_name: String,
     pub sections: Vec<LoadedSection>,
@@ -22,17 +23,36 @@ pub struct LoadedCrate {
 }
 
 
+#[derive(Debug)]
 pub enum LoadedSection{
     Text(TextSection),
     Rodata(RodataSection), // TODO: add type
-    Data,  // TODO: add type
+    Data(DataSection),  // TODO: add type
 }
+impl LoadedSection {
+    pub fn virt_addr(&self) -> VirtualAddress {
+        match self {
+            &LoadedSection::Text(ref text) => text.virt_addr,
+            &LoadedSection::Rodata(ref rodata) => rodata.virt_addr,
+            &LoadedSection::Data(ref data) => data.virt_addr,
+        }
+    }
+    pub fn size(&self) -> usize {
+        match self {
+            &LoadedSection::Text(ref text) => text.size,
+            &LoadedSection::Rodata(ref rodata) => rodata.size,
+            &LoadedSection::Data(ref data) => data.size,
+        }
+    }
+}
+
 
 /// Represents a .text section in a crate, which in Rust,
 /// corresponds to a single function. 
+#[derive(Debug)]
 pub struct TextSection {
     /// The String representation of just this symbol,
-    /// without any 
+    /// without any preceding crate namespaces or anything.
     pub symbol: String,
     /// The full String representation of this symbol. 
     /// Format <crate>::<module>::<struct>::<fn_name>
@@ -50,19 +70,19 @@ pub struct TextSection {
 
 
 /// Represents a .rodata section in a crate.
+#[derive(Debug)]
 pub struct RodataSection {
-    /// The String representation of just this symbol,
-    /// without any 
-    pub symbol: String,
-    /// The full String representation of this symbol. 
-    /// Format <crate>::<module>::<struct>::<fn_name>
-    /// For example, test_lib::MyStruct::new
-    pub abs_symbol: String,
-    /// the unique hash generated for this function by the Rust compiler,
-    /// which can be used as a version identifier. 
-    /// Not all symbols will have a hash, like those that are not mangled.
-    pub hash: Option<String>,
-    /// The virtual address of where this text section is loaded
+    /// The virtual address of where this section is loaded
+    pub virt_addr: VirtualAddress,
+    /// The size in bytes of this section
+    pub size: usize,
+}
+
+
+/// Represents a .data section in a crate.
+#[derive(Debug)]
+pub struct DataSection {
+    /// The virtual address of where this section is loaded
     pub virt_addr: VirtualAddress,
     /// The size in bytes of this section
     pub size: usize,
