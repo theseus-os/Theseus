692d4809c66ad098830f69c69da21b559e3f6d0b
Kevin Boos <kevinaboos@gmail.com>
2018-03-18 04:01:18 -0500

    Fixed issue with multiply-defined symbols. Now allowing symbols with GLOBAL HIDDEN visibility, since apparently those symbols may still be publicly accessible at the Rust level too. (Previously we were only allowing GLOBAL DEFAULT.) Building is still weird, you have to run `make clean` for each rebuild, and the xargo libcore/alloc cache is also rebuilt each time.

--- kernel/nano_core/src/reexports.rs
+++ kernel/nano_core/src/reexports.rs
@@ -8,40 +8,15 @@
 use compiler_builtins;
 
 
+// static REF_FLOATUNDIDF: extern "C" fn(u64) -> f64 = compiler_builtins::float::conv::__floatundidf;
+// static REF_MULOTI4: extern "C" fn(i128, i128, &mut i32) -> i128 = compiler_builtins::int::mul::__muloti4;
+// static REF_UDIVTI3: extern "C" fn(u128, u128) -> u128 = compiler_builtins::int::udiv::__udivti3;
+// static REF_UMODTI3: extern "C" fn(u128, u128) -> u128 = compiler_builtins::int::udiv::__umodti3;
+
+
+/// currently the compiler_builtins crate hasn't yet implemented this,
+/// see: https://github.com/rust-lang-nursery/compiler-builtins/issues/216 
 #[no_mangle]
 pub fn __floatundisf(_a: i64) -> f32 {
-    // currently the compiler_builtins crate hasn't yet implemented this,
-    // see: https://github.com/rust-lang-nursery/compiler-builtins/issues/216 
     unimplemented!();
-}
-
-#[no_mangle]
-pub fn __floatundidf(a: u64) -> f64 {
-    compiler_builtins::float::conv::__floatundidf(a)
-}
-
-
-// TODO FIXME: this is not right. If we disable them for release builds, the symbol that does get automatically included from libcore
-//             has the same problem as before -- it only has GLOBAL HIDDEN visibility, not GLOBAL DEFAULT like our symbols defined explicitly here.  :(
-// The following three compiler_builtins are only needed for Debug mode,
-// indicated by the conditional compilation flag on "debug_assertions",
-// see here: https://stackoverflow.com/questions/39204908/how-to-check-release-debug-builds-using-cfg-in-rust/39205417#39205417
-// These functions are automatically included in the nano_core in release mode,
-// so we don't need to compile them.
-// #[cfg(debug_assertions)]
-#[no_mangle]
-pub fn __muloti4(a: i128, b: i128, oflow: &mut i32) -> i128 {
-    compiler_builtins::int::mul::__muloti4(a, b, oflow)
-}
-
-// #[cfg(debug_assertions)]
-#[no_mangle]
-pub fn __udivti3(n: u128, d: u128) -> u128 {
-    compiler_builtins::int::udiv::__udivti3(n, d)
-}
-
-// #[cfg(debug_assertions)]
-#[no_mangle]
-pub fn __umodti3(n: u128, d: u128) -> u128 {
-    compiler_builtins::int::udiv::__umodti3(n, d)
 }
