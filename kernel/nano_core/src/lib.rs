0cde93ec0391566769e0e40798e7362abbbb7961
snoword <qiuj@mail.bnu.edu.cn>
2018-03-22 16:46:48 -0400

    implement another application to draw next pixel according to the direction key

--- kernel/nano_core/src/lib.rs
+++ kernel/nano_core/src/lib.rs
@@ -162,15 +162,8 @@ fn test_framebuffer(_: Option<u64>) {
     let height=150;
     let color = 0xe4cf8e;
     let window = window_manager::get_window_obj(x,y,width + 2,height + 2).unwrap();
-    //window.print();
-unsafe{
 
-    /*if (window.is_err()){
-        return;
-    }
-    
-    let window = window.unwrap();
-    window.print();*/
+unsafe{
            
     use keycodes_ascii::Keycode;
     (*window).draw_square(x, y, 20, 20, 0xe4cf8e);
@@ -245,91 +238,46 @@ unsafe{
 }
 
 fn test_window(_: Option<u64>) {
-    let mut x=250;
-    let mut y=150;
+    
     let width=300;
     let height=200;
+    let mut x=width/2;
+    let mut y=height/2;
     let color = 0xe4cf8e;
-    let window = window_manager::get_window_obj(x,y,width + 2,height + 2).unwrap();
-    //window.print();
+    let window = window_manager::get_window_obj(300,200,width + 2,height + 2).unwrap();
 unsafe{
-
-    /*if (window.is_err()){
-        return;
-    }
-    
-    let window = window.unwrap();
-    window.print();*/
            
     use keycodes_ascii::Keycode;
-    (*window).draw_square(x, y, 20, 20, 0xe4cf8e);
+    (*window).draw_pixel(x, y, color);
 
     let mut direction = Keycode::Right;
-    unsafe { 
-        loop{
-            let window = *window;
-            let keycode = window.get_key_code();
-           
-            if(keycode.is_some()){
-                direction = keycode.unwrap();
-            }
+    loop{
+        let window = *window;
+        let keycode = window.get_key_code();
+        
+        if(keycode.is_some()){
+            direction = keycode.unwrap();
+        
             match direction {
                 Keycode::Right => {
-                    if((y+20) < height) {
-                        window.draw_line(x, y, x, y+20, 0x000000);
-                        window.draw_line((20+x)%width, y, (20+x)%width, y+20, 0xe4cf8e);
-                    } else {
-                        window.draw_line(x, y, x, height, 0x000000);
-                        window.draw_line(x, 0, x, (y+20)%height, 0x000000);
-                        window.draw_line((20+x)%width, y, (20+x)%width, height, 0xe4cf8e);
-                        window.draw_line((20+x)%width, 0, (20+x)%width, (y+20)%height, 0xe4cf8e);                        
-                    }
-                    x = (x + 1)%width;
+                    x = (x+1)%width;
                 }
                 Keycode::Left => { 
-                    if((y+20) < height) {               
-                        window.draw_line((x+19)%width, y, (x+19)%width, y+20, 0x000000);
-                        x = (x + width -1)%width;
-                        window.draw_line(x, y, x, y+20, 0xe4cf8e);
-                    } else {
-                        window.draw_line((x+19)%width, y, (x+19)%width, height, 0x000000);
-                        window.draw_line((x+19)%width, 0, (x+19)%width, (y+20)%height, 0x000000);
-                        x = (x + width -1)%width;
-                        window.draw_line(x, y, x, height, 0xe4cf8e);    
-                        window.draw_line(x, 0, x, (y+20)%height, 0xe4cf8e);    
-                    }                            
+                    x = (x + width -1)%width;
                 }
                 Keycode::Up => {
-                    if (x+20<width)  {             
-                        window.draw_line(x, (y+19)%height, x+20, (y+19)%height, 0x000000);
-                        y = (y + height -1)%height;
-                        window.draw_line(x, y, x+20, y, 0xe4cf8e);                            
-                    } else {
-                        window.draw_line(x, (y+19)%height, width, (y+19)%height, 0x000000);
-                        window.draw_line(0, (y+19)%height, (x+20)%width, (y+19)%height, 0x000000);
-                        y = (y + height -1)%height;
-                        window.draw_line(x, y, width, y, 0xe4cf8e);                            
-                        window.draw_line(0, y, (x+20)%width, y, 0xe4cf8e);                            
-                    }
+                    y = (y + height -1)%height;    
                 }
-                Keycode::Down => {    
-                    if (x+20<width) {      
-                        window.draw_line(x, y, (x+20)%width, y, 0x000000);
-                        window.draw_line(x, (y+20)%height, (20+x)%width, (y+20)%height, 0xe4cf8e);
-                        y = (y + 1)%height;
-                    } else {
-                        window.draw_line(x, y, width, y, 0x000000);
-                        window.draw_line(0, y, (x+20)%width, y, 0x000000);
-                        window.draw_line(x, (y+20)%height, width, (y+20)%height, 0xe4cf8e);
-                        window.draw_line(0, (y+20)%height, (20+x)%width, (y+20)%height, 0xe4cf8e);
-                        y = (y + 1)%height;
-                    }                         
+                Keycode::Down => {       
+                        y = (y + 1)%height;                          
                 }
                 
                 _ => {}
             }
-        }       
-    }
+            window.draw_pixel(x, y, color);
+        }
+    }       
+
 }
 }
 
