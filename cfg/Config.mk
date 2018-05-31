749b46af827201daeb12d71516f56db196bca8fe
Kevin Boos <kevinaboos@gmail.com>
2018-05-31 01:54:00 -0500

    Improved Makefiles to fix issue of application crates having other dependencies that weren't part of or used in the kernel. Now they will be included in the iso's boot modules. As a result, the static contrived dependency on the `rtc` crate has been removed from the captain.

--- cfg/Config.mk
+++ cfg/Config.mk
@@ -1,5 +1,5 @@
-### This makefile is run from each kernel crate, so it needs to use paths
-### relative to a subdirectory, not the actual directory contaiing this file.
+### This makefile is used for any crate that runs in the kernel (kernel/ and applications/)
+### so it needs to use paths relative to a subdirectory, not the actual directory contaiing this file.
 ### So, to access the directory containing this file, you would use "../"
 
 .DEFAULT_GOAL := all
@@ -10,16 +10,11 @@ ARCH ?= x86_64
 ## this should not point directly to the .json target spec file, but it should have the same name.
 TARGET ?= $(ARCH)-theseus
 
-PWD := $(shell pwd)
-
-# KERNEL_BUILD_DIR ?= ${PWD}/../build
-KERNEL_BUILD_DIR ?= ${PWD}/build
-
 ## The top level directory of the Theseus project
-ROOT_DIR ?= ${PWD}/..
+ROOT_DIR := $(abspath $(dir $(lastword $(MAKEFILE_LIST))))/..
 
 ## specifies where the configuration files are kept, like target json files
-CFG_DIR ?= ${ROOT_DIR}/cfg
+CFG_DIR := $(ROOT_DIR)/cfg
 
 
 ## Build modes:  debug is default (dev mode), release is release with full optimizations.
