810aa0991208016e806069c961ce6b980a757c7a
apham727 <31870004+apham727@users.noreply.github.com>
2018-06-06 18:56:49 -0500

    adding initial version of terminal shell
    * added functionality to track keypresses into the console. Records them as a command string and a vector of arguments
    
    * New crate for core utils, currently has 'date' module
    
    * terminal will accept and run commands on different thread, implements overtype and cursor
    
    * Recommit so I can begin work on spawning with new application system
    
    * Refactored console code so that it is object-oriented
    
    * Changed the terminal to be object-oriented so we can spawn multiple terminal windows.

--- applications/date/src/lib.rs
+++ applications/date/src/lib.rs
@@ -14,7 +14,7 @@ use alloc::{Vec, String};
 #[no_mangle]
 pub fn main(_args: Vec<String>) -> isize {
     let now = rtc::read_rtc();
-    console::print_to_console(format!("{}!\n", now));
+    let _result = console::print_to_console(format!("{}!\n", now));
 
     0
 }
