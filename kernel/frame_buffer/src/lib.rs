db87f32933bfc38d30bdad0c53fd8a231d0afa10
snoword <qiuj@mail.bnu.edu.cn>
2018-08-14 15:54:07 -0400

    polish

--- kernel/frame_buffer/src/lib.rs
+++ kernel/frame_buffer/src/lib.rs
@@ -55,21 +55,24 @@ pub fn init() -> Result<(), &'static str > {
     
     let VESA_DISPLAY_PHYS_START:PhysicalAddress;
     let VESA_DISPLAY_PHYS_SIZE: usize;
-    {
+    let (width, height) = {
         let graphic_info = acpi::madt::GRAPHIC_INFO.lock();
+        if graphic_info.address == 0 {
+            return Err("Fail to get graphic mode infomation!");
+        }
         VESA_DISPLAY_PHYS_START = graphic_info.address as usize;
         let mut drawer = FRAME_DRAWER.lock();
-        drawer.set_resolution(graphic_info.x as usize, graphic_info.y as usize);
+        //drawer.set_resolution(graphic_info.x as usize, graphic_info.y as usize);
         VESA_DISPLAY_PHYS_SIZE= (graphic_info.x*graphic_info.y) as usize * PIXEL_BYTES;
+        (graphic_info.x as usize, graphic_info.y as usize)
+    };
 
-    }
     let rs = font::init();
     match font::init() {
         Ok(_) => { trace!("frame_buffer text initialized."); },
         Err(err) => { return Err(err); }
     }
 
-    //const VESA_DISPLAY_PHYS_START: PhysicalAddress = 0x9000_0000;
     
 
     // get a reference to the kernel's memory mapping information
@@ -96,12 +99,6 @@ pub fn init() -> Result<(), &'static str > {
                 None => { return Err("framebuffer::init() Couldn't get frame allocator"); }
             }
 
-            let rs = FRAME_DRAWER.lock().init_frame_buffer(pages.start_address());
-            match rs {
-                Ok(_) => { },
-                Err(err) => { return Err(err); }
-            }
-
             let mut allocator = try!(allocator_mutex.ok_or("allocate frame buffer")).lock();
             let mapped_frame_buffer = try!(active_table.map_allocated_pages_to(
                 pages, 
@@ -110,11 +107,7 @@ pub fn init() -> Result<(), &'static str > {
                 allocator.deref_mut())
             );
 
-            //let mut pages = FRAME_BUFFER_PAGES.lock();
-            //*pages = Some(mapped_frame_buffer);
-            let mut drawer = FRAME_DRAWER.lock();
-            drawer.pages = Some(mapped_frame_buffer);
-
+            let rs = FRAME_DRAWER.lock().set_mode_info(width, height, mapped_frame_buffer);
 
             Ok(())
         }
@@ -124,104 +117,13 @@ pub fn init() -> Result<(), &'static str > {
     }
 }
 
-fn init_info() -> Result<(), &'static str > {
-   //Allocate VESA frame buffer
-    const GRAPHIC_MODE_INFO: PhysicalAddress = 0xF100;
-    //const VESA_DISPLAY_PHYS_START: PhysicalAddress = 0x9000_0000;
-    
-    const GRAPHIC_MODE_SIZE: usize = 0x300;
-
-    // get a reference to the kernel's memory mapping information
-    let kernel_mmi_ref = get_kernel_mmi_ref().expect("KERNEL_MMI was not yet initialized!");
-    let mut kernel_mmi_locked = kernel_mmi_ref.lock();
-
-    // destructure the kernel's MMI so we can access its page table
-    let MemoryManagementInfo { 
-        page_table: ref mut kernel_page_table, 
-        .. // don't need to access other stuff in kernel_mmi
-    } = *kernel_mmi_locked;
-    
-    match kernel_page_table {
-        &mut PageTable::Active(ref mut active_table) => {
-            let pages = match allocate_pages_by_bytes(GRAPHIC_MODE_INFO) {
-                Some(pages) => { pages },
-                None => { return Err("frame_buffer_3d::init() couldn't allocate pages."); }
-            };
-            
-            let vesa_display_flags = EntryFlags::PRESENT | EntryFlags::WRITABLE | EntryFlags::GLOBAL | EntryFlags::NO_CACHE;
-            let allocator_mutex = FRAME_ALLOCATOR.try();
-            match allocator_mutex {
-                Some(_) => { },
-                None => { return Err("framebuffer::init() Couldn't get frame allocator"); }
-            }
-
-            let rs = GRAPHIC_BUFFER.lock().init_mode_buffer(pages.start_address());
-            match rs {
-                Ok(_) => { },
-                Err(err) => { return Err(err); }
-            }
-
-            let mut allocator = try!(allocator_mutex.ok_or("allocate frame buffer")).lock();
-            let mapped_frame_buffer = try!(active_table.map_allocated_pages_to(
-                pages, 
-                Frame::range_inclusive_addr(GRAPHIC_MODE_INFO, GRAPHIC_MODE_SIZE), 
-                vesa_display_flags, 
-                allocator.deref_mut())
-            );
-
-            let mut pages = GRAPHIC_MODE_PAGE.lock();
-            *pages = Some(mapped_frame_buffer);
-
-            Ok(())
-        }
-        _ => { 
-            return Err("");
-        }
-    }
-}
-
 static FRAME_DRAWER: Mutex<Drawer> = {
     Mutex::new(Drawer {
-        start_address:0,
         width:0,
         height:0,
         pages:None,
-        buffer: unsafe {Unique::new_unchecked((VGA_BUFFER_ADDR) as *mut _) },
     })
 };
-
-static GRAPHIC_BUFFER: Mutex<VideoMode> = {
-    Mutex::new(VideoMode {
-            start_address:0,
-            buffer: unsafe {Unique::new_unchecked((VGA_BUFFER_ADDR) as *mut _) 
-        }
-    })
-};
-
-struct VideoMode {
-    start_address:usize,
-
-    buffer: Unique<Buffer>,
-}
-
-impl VideoMode {
-
-    fn init_mode_buffer(&mut self, virtual_address:usize) -> Result<(), &'static str>{
-        if self.start_address == 0 {
-            self.start_address = virtual_address;
-            match Unique::new((virtual_address) as *mut _) {
-                Some(buffer) => { 
-                    self.buffer = buffer; 
-                    trace!("Set frame buffer address {:#x}", virtual_address);
-                },
-                None => { return Err("Fail to new virtual frame buffer"); }
-            }
-        }
-        Ok(())
-
-    }
-}  
-
 
 /// draw a pixel with coordinates and color
 pub fn draw_pixel(x:usize, y:usize, color:u32) {
@@ -250,11 +152,9 @@ pub struct Point {
 }
 
 pub struct Drawer {
-    start_address: usize,
     width:usize,
     height:usize,
     pages:Option<MappedPages>,
-    buffer: Unique<Buffer>,
 }
 
 impl Drawer {
@@ -266,9 +166,10 @@ impl Drawer {
             : "cc", "memory", "rdi", "rcx"
             : "intel", "volatile");
     }*/
-    fn set_resolution(&mut self, x:usize, y:usize) {
-        self.width = x;
-        self.height = y;
+    fn set_mode_info(&mut self, width:usize, height:usize, pages:MappedPages) {
+        self.width = width;
+        self.height = height;
+        self.pages = Some(pages);
     }
 
     pub fn get_resolution(&self) -> (usize, usize) {
@@ -402,21 +303,6 @@ impl Drawer {
             None => { return Err("no allocated pages in framebuffer") }
         }
     }
-
-    fn init_frame_buffer(&mut self, virtual_address:usize) -> Result<(), &'static str>{
-        if self.start_address == 0 {
-            self.start_address = virtual_address;
-            match Unique::new((virtual_address) as *mut _) {
-                Some(buffer) => { 
-                    self.buffer = buffer; 
-                    trace!("Set frame buffer address {:#x}", virtual_address);
-                },
-                None => { return Err("Fail to new virtual frame buffer"); }
-            }
-        }
-        Ok(())
-
-    }  
 }
 
 pub struct Buffer {
