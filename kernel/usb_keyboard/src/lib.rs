c3809983e2eef42a0bf036596850d000ebfc45c6
Bowen Liu <bl28@rice.edu>
2018-09-08 15:53:11 -0500

    Debugging the USB keyboard drive

---
+++ kernel/usb_keyboard/src/lib.rs
@@ -0,0 +1,80 @@
+#![no_std]
+#![feature(alloc)]
+
+#![allow(dead_code)]
+
+extern crate alloc;
+extern crate owning_ref;
+extern crate usb_uhci;
+extern crate usb_desc;
+extern crate usb_device;
+extern crate memory;
+extern crate spin;
+extern crate volatile;
+#[macro_use] extern crate log;
+
+use alloc::boxed::Box;
+use spin::{Once, Mutex};
+use volatile:: ReadOnly;
+use usb_desc::{UsbEndpDesc,UsbDeviceDesc,UsbConfDesc,UsbIntfDesc};
+use usb_device::{UsbControlTransfer,UsbDevice,Controller,HIDType};
+use usb_uhci::{box_dev_req,box_config_desc,box_device_desc,box_inter_desc,box_endpoint_desc,map};
+use memory::{get_kernel_mmi_ref,MemoryManagementInfo,FRAME_ALLOCATOR,Frame,PageTable, ActivePageTable, PhysicalAddress, VirtualAddress, EntryFlags, MappedPages, allocate_pages ,allocate_frame};
+use owning_ref:: BoxRefMut;
+
+static USB_KEYBOARD_INPUT_BUFFER: Once<Mutex<BoxRefMut<MappedPages, [ReadOnly<u8>;8]>>> = Once::new();
+
+const TD_PACKET_IN : u8=                    0x69;
+const TD_PACKET_OUT : u8=                   0xe1;
+
+pub fn init(active_table: &mut ActivePageTable, device:& UsbDevice)-> Result<(),&'static str>{
+
+    let v_buffer_pointer = usb_uhci::buffer_pointer_alloc(0)
+        .ok_or("Couldn't get virtual memory address for the buffer pointer in get_config_desc request for device in UHCI!!")?;
+    let data_buffer_pointer = active_table.translate(v_buffer_pointer as usize)
+        .ok_or("Couldn't translate the virtual memory address of the buffer pointer to phys_addr!!")?;
+
+    let _x = recieve_data(active_table,device,data_buffer_pointer as u32)?;
+    let k_buffer = box_keyboard_buffer(active_table,data_buffer_pointer)?;
+
+    USB_KEYBOARD_INPUT_BUFFER.call_once(||{
+        Mutex::new(k_buffer)
+    });
+
+    Ok(())
+
+}
+
+pub fn recieve_data(active_table: &mut ActivePageTable, device:& UsbDevice, data_buffer_pointer: u32)-> Result<(),&'static str>{
+
+
+    let speed= device.speed;
+    let addr = device.addr;
+    let max_size = device.maxpacketsize;
+    let mut toggle = 0;
+    let (packet_add,packet_index) = usb_uhci::td_alloc().unwrap()?;
+    usb_uhci::init_td(packet_index,0,0,speed ,addr, 0,toggle, TD_PACKET_IN as u32,
+                      max_size,data_buffer_pointer);
+    let packet_add = active_table.translate(packet_add).unwrap();
+
+    let (qh_physical_add,qh_index) = usb_uhci::qh_alloc().unwrap()?;
+    let qh_physical_add = active_table.translate(qh_physical_add).unwrap();
+    usb_uhci::init_qh(qh_index,usb_uhci::TD_PTR_TERMINATE,packet_add as u32);
+
+    let frame_index = usb_uhci:: qh_link_to_framelist(qh_physical_add as u32).unwrap()?;
+    Ok(())
+
+}
+
+/// Box the the keyboard input data buffer
+pub fn box_keyboard_buffer(active_table: &mut ActivePageTable, frame_base: PhysicalAddress)
+                      -> Result<BoxRefMut<MappedPages, [ReadOnly<u8>;8]>, &'static str>{
+
+
+    let buffer: BoxRefMut<MappedPages, [ReadOnly<u8>;8]>  = BoxRefMut::new(Box::new(map(active_table,frame_base)?))
+        .try_map_mut(|mp| mp.as_type_mut::<[ReadOnly<u8>;8]>(0))?;
+
+
+    Ok(buffer)
+}
+
