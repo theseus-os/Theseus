ddcab1da0ad3a1fdb31333b485d2da6143ca7f0c
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2018-09-27 21:00:27 -0500

    reset heap sizea

--- kernel/kernel_config/src/memory.rs
+++ kernel/kernel_config/src/memory.rs
@@ -91,8 +91,7 @@ pub const KERNEL_TEXT_MAX_SIZE: usize = ADDRESSABILITY_PER_P4_ENTRY - (2 * 1024 
 /// which is the slot right below the recursive P4 entry (510)
 /// actual value: 0o177777_776_000_000_000_0000, or 0xFFFF_FF00_0000_0000
 pub const KERNEL_HEAP_START: usize = 0xFFFF_0000_0000_0000 | (KERNEL_HEAP_P4_INDEX << (P4_INDEX_SHIFT + PAGE_SHIFT));
-// pub const KERNEL_HEAP_INITIAL_SIZE: usize = 16 * 1024 * 1024; // 16 MiB
-pub const KERNEL_HEAP_INITIAL_SIZE: usize = 256 * 1024 * 1024; // 256 MiB
+pub const KERNEL_HEAP_INITIAL_SIZE: usize = 16 * 1024 * 1024; // 16 MiB
 /// the kernel heap gets the whole 509th P4 entry.
 pub const KERNEL_HEAP_MAX_SIZE: usize = ADDRESSABILITY_PER_P4_ENTRY;
 
