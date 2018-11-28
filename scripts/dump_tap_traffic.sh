fd5661d51f59316d237d5aef5b2ecfb1b7ad6832
Kevin Boos <kevinaboos@gmail.com>
2018-11-27 20:33:36 -0800

    rename network tap/bridge scripts appropriately

--- scripts/dump_tap_traffic.sh
+++
@@ -1,1 +0,0 @@
-sudo tcpdump -nettti tap0 '(ether dst host ff:ff:ff:ff:ff:ff)'
--- scripts/tuntap_host.sh
+++
@@ -1,6 +0,0 @@
-sudo ip link add br0 type bridge
-sudo ip tuntap add dev tap0 mod tap user $(whoami)
-sudo ip link set tap0 master br0
-#sudo ip addr add 192.168.69.100/24 dev tap0
-sudo ip link set dev br0 up
-sudo ip link set dev tap0 up
