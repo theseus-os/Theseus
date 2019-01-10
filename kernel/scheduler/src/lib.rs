d0a0b905a4673091d204facce8f01d33ab57e447
namitha <namitha@rice.edu>
2019-01-10 15:33:19 -0600

    Broke schedulers into seperate crates

--- kernel/scheduler/src/lib.rs
+++ kernel/scheduler/src/lib.rs
@@ -7,6 +7,7 @@ extern crate irq_safety;
 extern crate apic;
 extern crate task;
 extern crate runqueue;
+extern crate scheduler_priority;
 
 
 use core::ops::DerefMut;
@@ -14,6 +15,7 @@ use irq_safety::{disable_interrupts};
 use apic::get_my_apic_id;
 use task::{Task, TaskRef, get_my_current_task};
 use runqueue::RunQueue;
+
 
 
 /// This function performs a task switch.
@@ -60,7 +62,7 @@ pub fn schedule() -> bool {
     //Since timeslice is constant we replaced it with a constant
     curr.weighted_runtime = curr.weighted_runtime + 1000 / (curr_priority + 1);
 
-    if let Some(selected_next_task) = select_next_task_priority(apic_id) {
+    if let Some(selected_next_task) = scheduler_round_robin::select_next_task(apic_id) {
         next_task = selected_next_task.lock_mut().deref_mut();  // as *mut Task;
     }
     else {
@@ -113,131 +115,4 @@ pub fn schedule() -> bool {
     // trace!("AFTER TASK_SWITCH CALL (current={}), interrupts are {}", new_current, ::interrupts::interrupts_enabled());
  
     true
-}
-
-
-
-/// this defines the round robin scheduler policy.
-/// returns None if there is no schedule-able task
-fn select_next_task_round_robin(apic_id: u8) -> Option<TaskRef>  {
-
-    let mut runqueue_locked = match RunQueue::get_runqueue(apic_id) {
-        Some(rq) => rq.write(),
-        _ => {
-            error!("BUG: select_next_task(): couldn't get runqueue for core {}", apic_id); 
-            return None;
-        }
-    };
-    
-    let mut idle_task_index: Option<usize> = None;
-    let mut chosen_task_index: Option<usize> = None;
-
-    for (i, taskref) in runqueue_locked.iter().enumerate() {
-        let t = taskref.lock();
-
-        // we skip the idle task, and only choose it if no other tasks are runnable
-        if t.is_an_idle_task {
-            idle_task_index = Some(i);
-            continue;
-        }
-
-        // must be runnable
-        if !t.is_runnable() {
-            continue;
-        }
-
-        // if this task is pinned, it must not be pinned to a different core
-        if let Some(pinned) = t.pinned_core {
-            if pinned != apic_id {
-                // with per-core runqueues, this should never happen!
-                error!("select_next_task() (AP {}) found a task pinned to a different core: {:?}", apic_id, *t);
-                return None;
-            }
-        }
-            
-        // found a runnable task!
-        chosen_task_index = Some(i);
-        // debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
-        break; 
-    }
-
-    // idle task is a backup iff no other task has been chosen
-    chosen_task_index
-        .or(idle_task_index)
-        .and_then(|index| runqueue_locked.move_to_end(index))
-}
-
-/// This defines the priority scheduler policy.
-/// Task with the minimum weighted run time is picked
-/// Returns None if there is no schedule-able task
-fn select_next_task_priority(apic_id: u8) -> Option<TaskRef>  {
-
-    let mut runqueue_locked = match RunQueue::get_runqueue(apic_id) {
-        Some(rq) => rq.write(),
-        _ => {
-            error!("BUG: select_next_task(): couldn't get runqueue for core {}", apic_id); 
-            return None;
-        }
-    };
-    
-    let mut idle_task_index: Option<usize> = None;
-    let mut chosen_task_index: Option<usize> = None;
-    let mut minimum_weighted_run_time = 0;
-
-    for (i, taskref) in runqueue_locked.iter().enumerate() {
-        let t = taskref.lock();
-
-        // we skip the idle task, and only choose it if no other tasks are runnable
-        if t.is_an_idle_task {
-            idle_task_index = Some(i);
-            continue;
-        }
-
-        // must be runnable
-        if !t.is_runnable() {
-            continue;
-        }
-
-        //if let Some(priority) = t.priority {
-        //	if priority < -1 {
-        //        continue;
-        //    }
-        //}
-
-        // if this task is pinned, it must not be pinned to a different core
-        if let Some(pinned) = t.pinned_core {
-            if pinned != apic_id {
-                // with per-core runqueues, this should never happen!
-                error!("select_next_task() (AP {}) found a task pinned to a different core: {:?}", apic_id, *t);
-                return None;
-            }
-        }
-            
-        // found a runnable task!
-        // Mark it as the running task if it has he minimum weighted run time
-        chosen_task_index = match chosen_task_index{
-        	None => {
-        		minimum_weighted_run_time = t.weighted_runtime;
-        		Some(i)
-        	},
-        	Some(chosen_task_index) => {
-        		if(t.weighted_runtime < minimum_weighted_run_time){
-        			minimum_weighted_run_time = t.weighted_runtime;
-        			Some(i)
-        		}
-        		else{
-        			Some(chosen_task_index) 
-        		}
- 
-        	}
-        }
-        
-        //debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
-        //break; 
-    }
-
-    // idle task is a backup iff no other task has been chosen
-    chosen_task_index
-        .or(idle_task_index)
-        .and_then(|index| runqueue_locked.move_to_end(index))
 }
---
+++ kernel/scheduler_round_robin/src/lib.rs
@@ -0,0 +1,142 @@
+#![no_std]
+#![feature(alloc)]
+
+extern crate alloc;
+#[macro_use] extern crate log;
+extern crate irq_safety;
+extern crate apic;
+extern crate task;
+extern crate runqueue;
+
+
+use core::ops::DerefMut;
+use irq_safety::{disable_interrupts};
+use apic::get_my_apic_id;
+use task::{Task, TaskRef, get_my_current_task};
+use runqueue::RunQueue;
+
+
+/// this defines the round robin scheduler policy.
+/// returns None if there is no schedule-able task
+pub fn select_next_task(apic_id: u8) -> Option<TaskRef>  {
+
+    let mut runqueue_locked = match RunQueue::get_runqueue(apic_id) {
+        Some(rq) => rq.write(),
+        _ => {
+            error!("BUG: select_next_task(): couldn't get runqueue for core {}", apic_id); 
+            return None;
+        }
+    };
+    
+    let mut idle_task_index: Option<usize> = None;
+    let mut chosen_task_index: Option<usize> = None;
+
+    for (i, taskref) in runqueue_locked.iter().enumerate() {
+        let t = taskref.lock();
+
+        // we skip the idle task, and only choose it if no other tasks are runnable
+        if t.is_an_idle_task {
+            idle_task_index = Some(i);
+            continue;
+        }
+
+        // must be runnable
+        if !t.is_runnable() {
+            continue;
+        }
+
+        // if this task is pinned, it must not be pinned to a different core
+        if let Some(pinned) = t.pinned_core {
+            if pinned != apic_id {
+                // with per-core runqueues, this should never happen!
+                error!("select_next_task() (AP {}) found a task pinned to a different core: {:?}", apic_id, *t);
+                return None;
+            }
+        }
+            
+        // found a runnable task!
+        chosen_task_index = Some(i);
+        // debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
+        break; 
+    }
+
+    // idle task is a backup iff no other task has been chosen
+    chosen_task_index
+        .or(idle_task_index)
+        .and_then(|index| runqueue_locked.move_to_end(index))
+}
+
+/// This defines the priority scheduler policy.
+/// Task with the minimum weighted run time is picked
+/// Returns None if there is no schedule-able task
+fn select_next_task_priority(apic_id: u8) -> Option<TaskRef>  {
+
+    let mut runqueue_locked = match RunQueue::get_runqueue(apic_id) {
+        Some(rq) => rq.write(),
+        _ => {
+            error!("BUG: select_next_task(): couldn't get runqueue for core {}", apic_id); 
+            return None;
+        }
+    };
+    
+    let mut idle_task_index: Option<usize> = None;
+    let mut chosen_task_index: Option<usize> = None;
+    let mut minimum_weighted_run_time = 0;
+
+    for (i, taskref) in runqueue_locked.iter().enumerate() {
+        let t = taskref.lock();
+
+        // we skip the idle task, and only choose it if no other tasks are runnable
+        if t.is_an_idle_task {
+            idle_task_index = Some(i);
+            continue;
+        }
+
+        // must be runnable
+        if !t.is_runnable() {
+            continue;
+        }
+
+        //if let Some(priority) = t.priority {
+        //	if priority < -1 {
+        //        continue;
+        //    }
+        //}
+
+        // if this task is pinned, it must not be pinned to a different core
+        if let Some(pinned) = t.pinned_core {
+            if pinned != apic_id {
+                // with per-core runqueues, this should never happen!
+                error!("select_next_task() (AP {}) found a task pinned to a different core: {:?}", apic_id, *t);
+                return None;
+            }
+        }
+            
+        // found a runnable task!
+        // Mark it as the running task if it has he minimum weighted run time
+        chosen_task_index = match chosen_task_index{
+        	None => {
+        		minimum_weighted_run_time = t.weighted_runtime;
+        		Some(i)
+        	},
+        	Some(chosen_task_index) => {
+        		if(t.weighted_runtime < minimum_weighted_run_time){
+        			minimum_weighted_run_time = t.weighted_runtime;
+        			Some(i)
+        		}
+        		else{
+        			Some(chosen_task_index) 
+        		}
+ 
+        	}
+        }
+        
+        //debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
+        //break; 
+    }
+
+    // idle task is a backup iff no other task has been chosen
+    chosen_task_index
+        .or(idle_task_index)
+        .and_then(|index| runqueue_locked.move_to_end(index))
+}
