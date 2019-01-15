1e18cf69b3b5565cfbab20b0098e3910b06bb96e
namitha <namitha@rice.edu>
2019-01-14 18:35:36 -0600

    Token based scheduler working. needs renaming

--- kernel/scheduler_priority/src/lib.rs
+++ kernel/scheduler_priority/src/lib.rs
@@ -17,24 +17,106 @@ use task::{Task, TaskRef, get_my_current_task};
 use runqueue::RunQueueTrait;
 use runqueue_priority::RunQueue;
 
+struct next_task_result{
+    taskref : Option<TaskRef>,
+    idle_task : bool,
+}
+
 
 
 /// this defines the priority scheduler policy.
 /// returns None if there is no schedule-able task
 pub fn select_next_task(apic_id: u8) -> Option<TaskRef>  {
-    let taskref = select_next_task_priority(apic_id);
-    // match taskref {
-    //     // The division was valid
-    //     Some(x) => {
-    //         Some(x)
-    //     }
-    //     // The division was invalid
-    //     None    => {
-    //         assign_weights(apic_id);
-    //         select_next_task_priority(apic_id)
-    //     }
-    // }
-    taskref
+    let taskref_with_result = select_next_task_p2(apic_id); 
+    match taskref_with_result {
+        // The division was valid
+        Some(x) => {
+            //debug!("Some coe running: {}", apic_id);
+            
+            if(x.idle_task == true){
+                assign_weights(apic_id);
+                select_next_task_p2(apic_id).and_then(|m| m.taskref)
+            }
+            else {
+                x.taskref
+            }
+        }
+        // The division was invalid
+        None    => {
+            assign_weights(apic_id);
+            select_next_task_p2(apic_id).and_then(|m| m.taskref)
+        }
+    }
+    //taskref_with_result.and_then(|m| m.taskref)
+}
+
+/// this defines the round robin scheduler policy.
+/// returns None if there is no schedule-able task
+fn select_next_task_p2(apic_id: u8) -> Option<next_task_result>  {
+
+    let mut runqueue_locked = match RunQueue::get_runqueue(apic_id) {
+        Some(rq) => rq.write(),
+        _ => {
+            error!("BUG: select_next_task(): couldn't get runqueue for core {}", apic_id); 
+            return None;
+        }
+    };
+    
+    let mut idle_task_index: Option<usize> = None;
+    let mut chosen_task_index: Option<usize> = None;
+    let mut idle_task = true;
+
+    for (i, taskref) in runqueue_locked.iter().enumerate() {
+        let t = taskref.lock();
+
+        // we skip the idle task, and only choose it if no other tasks are runnable
+        if t.is_an_idle_task {
+            idle_task_index = Some(i);
+            continue;
+        }
+
+        // must be runnable
+        if !t.is_runnable() {
+            continue;
+        }
+
+        // if this task is pinned, it must not be pinned to a different core
+        if let Some(pinned) = t.pinned_core {
+            if pinned != apic_id {
+                // with per-core runqueues, this should never happen!
+                error!("select_next_task() (AP {}) found a task pinned to a different core: {:?}", apic_id, *t);
+                return None;
+            }
+        }
+
+        if taskref.get_weight() == 0{
+            continue;
+        }
+            
+        // found a runnable task!
+        chosen_task_index = Some(i);
+        idle_task = false;
+        // debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
+        break; 
+    }
+
+    // idle task is a backup iff no other task has been chosen
+    // chosen_task_index
+    //     .or(idle_task_index)
+    //     .and_then(|index| runqueue_locked.move_to_end(index))
+
+    let modified_weight = {
+        let chosen_task = chosen_task_index.and_then(|index| runqueue_locked.get_priority_task_ref(index));
+        chosen_task.map(|m| m.get_weight()).unwrap_or(1) - 1
+    };
+
+    chosen_task_index
+        .or(idle_task_index)
+        .and_then(|index| runqueue_locked.update_and_move_to_end(index, modified_weight))
+        .map(|index| next_task_result {
+            taskref : Some(index),
+            idle_task  : idle_task, 
+        })
 }
 
 
@@ -84,9 +166,9 @@ fn select_next_task_priority(apic_id: u8) -> Option<TaskRef>  {
             }
         }
 
-        if taskref.get_weight() == 0{
-            continue;
-        }
+        //if taskref.get_weight() == 0{
+        //    continue;
+        //}
 
         // found a runnable task!
         // Mark it as the running task if it has he minimum weighted run time   
@@ -170,13 +252,15 @@ fn assign_weights(apic_id: u8) -> bool  {
             
         // found a runnable task!
         // Mark it as the running task if it has he minimum weighted run time
-        total_priorities = total_priorities + taskref.get_weight();
+        total_priorities = total_priorities + t.priority.unwrap_or(0) as u32;
         
         
         //debug!("select_next_task(): AP {} chose Task {:?}", apic_id, *t);
         //break; 
     }
-
+    if(apic_id == 1){
+        debug!("total priorities(): AP {} priorities {}", apic_id, total_priorities);
+    }
     //for (i, taskref) in runqueue_locked.iter().enumerate() {
     let mut i = 0;
     let mut len = 0;
@@ -193,11 +277,13 @@ fn assign_weights(apic_id: u8) -> bool  {
             // we skip the idle task, and only choose it if no other tasks are runnable
             if t.is_an_idle_task {
                 idle_task_index = Some(i);
+                i = i+1;
                 continue;
             }
 
             // must be runnable
             if !t.is_runnable() {
+                i = i+1;
                 continue;
             }
 
@@ -215,15 +301,15 @@ fn assign_weights(apic_id: u8) -> bool  {
                     return false;
                 }
             }
-            task_weight = 1000 * t.priority.unwrap() as u32 / total_priorities;
+            task_weight = 100 * t.priority.unwrap() as u32 / total_priorities;
         }
         // found a runnable task!
         // Mark it as the running task if it has he minimum weighted run time
         
         {
             let mut mut_task = runqueue_locked.get_priority_task_ref_as_mut(i);
-            //mut_task.map(|m| m.update_weight(task_weight));
-            mut_task.map(|m| m.update_weight(1000));
+            mut_task.map(|m| m.update_weight(task_weight));
+            //mut_task.map(|m| m.update_weight(1000));
         }
         
         i = i+1;
