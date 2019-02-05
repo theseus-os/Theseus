8f78505cca705aa6ec2527be71c20814be016a3c
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2019-02-05 14:44:48 -0800

    Redesign of TASKLIST with proper task removal
    * Use BTreeMap for TASKLIST instead of AtomicMap
    
    * get_task() returns a cloned TaskRef
    
    * When a task's exit value is taken with `take_exit_value()`, the Reaped task is removed.

--- applications/ps/src/lib.rs
+++ applications/ps/src/lib.rs
@@ -40,7 +40,7 @@ pub fn main(args: Vec<String>) -> isize {
     // Print all tasks
     let mut num_tasks = 0;
     let mut task_string = String::new();
-    for (id, taskref) in TASKLIST.iter() {
+    for (id, taskref) in TASKLIST.lock().iter() {
         num_tasks += 1;
         let task = taskref.lock();
         let name = &task.name;
