a23be2dbaee4a424b500bbc795b2f5f76b2e0917
Andrew Pham <apham727@gmail.com>
2019-03-03 12:23:52 -0600

    addressed entry flags errors in memfile write

--- kernel/memfs/src/lib.rs
+++ kernel/memfs/src/lib.rs
@@ -28,7 +28,8 @@ use fs_node::{FileOrDir, FileRef};
 pub struct MemFile {
     /// The name of the file
     name: String,
-    // The size of the file in bytes
+    // The size of the file in bytes (this is the actual length of meaningful content in the file rather than the size of this file's 
+    // mapped pages collection)
     size: usize,
     /// The contents or a seqeunce of bytes as a file: this primitive can be changed into a more complex struct as files become more complex
     mp: MappedPages,
@@ -72,6 +73,11 @@ impl File for MemFile {
         if offset > self.size {
             return Err("write offset exceeds file size");
         }
+
+        if !self.mp.flags().is_writable() {
+            return Err("MemFile::write(): existing MappedPages were not writable");
+        }
+
         if buffer.len() + offset <= self.mp.size_in_bytes() {
             { // scoped this so that the mutable borrow on mapped_pages ends as soon as possible
                 // Gets a mutuable reference to the byte portion of the newly mapped pages
@@ -85,6 +91,7 @@ impl File for MemFile {
             }    
             Ok(self.mp.size_in_bytes())
         } else { // we'll allocate a new set of mapped pages
+            let prev_flags = self.mp.flags();
             // Obtain the active kernel page table
             let kernel_mmi_ref = memory::get_kernel_mmi_ref().ok_or("KERNEL_MMI was not yet initialized!")?;
             if let memory::PageTable::Active(ref mut active_table) = kernel_mmi_ref.lock().page_table {
@@ -93,14 +100,14 @@ impl File for MemFile {
                 // we'll allocate the buffer length plus the offset because that's guranteed to be the most bytes we
                 // need (because it entered this conditional statement)
                 let pages = memory::allocate_pages_by_bytes(buffer.len() + offset).ok_or("could not allocate pages")?;
-                let mut mapped_pages = active_table.map_allocated_pages(pages,  EntryFlags::WRITABLE, allocator.lock().deref_mut())?;            
+                let mut mapped_pages = active_table.map_allocated_pages(pages, prev_flags, allocator.lock().deref_mut())?;            
                 { // scoped so that the mutable borrow on mapped_pages ends as soon as possible
                     // first need to copy over the bytes from the previous mapped pages
                     // this copies bytes up to the write offset 
                     let existing_bytes = self.mp.as_slice(0, offset)?;
                     let mut copy_slice = mapped_pages.as_slice_mut::<u8>(0, offset)?;
                     copy_slice.copy_from_slice(existing_bytes);
-                }
+                } 
                 {
                     // now write the new content into the mapped pages
                     // Gets a mutable reference to the byte portion of the newly mapped pages
@@ -108,7 +115,7 @@ impl File for MemFile {
                     dest_slice.copy_from_slice(buffer); // writes the desired contents into the correct area in the mapped page
                 }
                 self.mp = mapped_pages;
-                self.size = buffer.len();
+                self.size = buffer.len() + offset;
                 return Ok(buffer.len());
             }
             return Err("could not get active table");
