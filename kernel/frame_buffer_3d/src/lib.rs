d1776f2839e33aff8c78bde79ca3b42c1a3d0f50
snoword <qiuj@mail.bnu.edu.cn>
2019-05-10 21:31:25 -0500

    add personality

--- kernel/frame_buffer_3d/src/lib.rs
+++ kernel/frame_buffer_3d/src/lib.rs
@@ -23,12 +23,10 @@ use memory::{FRAME_ALLOCATOR, Frame, PageTable, PhysicalAddress,
 use core::ops::DerefMut;
 use alloc::boxed::Box;
 
-//The buffer for text printing
-pub mod text_buffer;
-//The font for text printing
-pub mod font;
 
 const PIXEL_BYTES:usize = 4;
+
+// #[cfg(framebuffer3d)]
 const COLOR_BITS:usize = 24;
 
 /// Init the frame buffer. Allocate a block of memory and map it to the frame buffer frames.
@@ -49,12 +47,6 @@ pub fn init() -> Result<(), &'static str > {
         buffer_height = graphic_info.height as usize;
         vesa_display_phys_size= buffer_width * buffer_height * PIXEL_BYTES;
     };
-
-    // init the font for text printing
-    match font::init() {
-        Ok(_) => { trace!("frame_buffer text initialized."); },
-        Err(err) => { return Err(err); }
-    }    
 
     // get a reference to the kernel's memory mapping information
     let kernel_mmi_ref = get_kernel_mmi_ref().expect("KERNEL_MMI was not yet initialized!");
@@ -88,7 +80,7 @@ pub fn init() -> Result<(), &'static str > {
                 allocator.deref_mut())
             );
 
-            FRAME_DRAWER.lock().set_mode_info(buffer_width, buffer_height, mapped_frame_buffer);
+            FRAME_BUFFER.lock().set_mode_info(buffer_width, buffer_height, mapped_frame_buffer);
 
             Ok(())
         }
@@ -98,222 +90,51 @@ pub fn init() -> Result<(), &'static str > {
     }
 }
 
-// Applications use this drawer to draw/print to the screen
-static FRAME_DRAWER: Mutex<Drawer> = {
-    Mutex::new(Drawer {
+//The only instance of the drawer structure
+pub static FRAME_BUFFER: Mutex<Buffer> = {
+    Mutex::new(Buffer {
         width:0,
         height:0,
         pages:None,
     })
 };
 
-/// draw a pixel with coordinates and color
-pub fn draw_pixel(x:usize, y:usize, color:u32) {
-    FRAME_DRAWER.lock().draw_pixel_3d(x, y, 0, color);
-}
-
-
-///draw a pixel in 3D mode with coordinates and color.
-pub fn draw_pixel_3d(x:usize, y:usize, z:u8, color:u32) {
-    FRAME_DRAWER.lock().draw_pixel_3d(x, y, z, color)
-}
-
-/// draw a line with start and end coordinates and color
-pub fn draw_line(start_x:usize, start_y:usize, end_x:usize, end_y:usize, color:u32) {
-    FRAME_DRAWER.lock().draw_line_3d(start_x as i32, start_y as i32, end_x as i32, end_y as i32, 0, color);
-}
-
-/// draw a line with start and end coordinates and color
-pub fn draw_line_3d(start_x:usize, start_y:usize, end_x:usize, end_y:usize, z:u8, color:u32) {
-    FRAME_DRAWER.lock().draw_line_3d(start_x as i32, start_y as i32, end_x as i32, end_y as i32, 
-        z, color);
-}
-
-/// draw a rectangle with upper left coordinates, width, height and color
-pub fn draw_rectangle(start_x:usize, start_y:usize, width:usize, height:usize, color:u32) {
-    FRAME_DRAWER.lock().draw_rectangle_3d(start_x, start_y, width, height, 0, color)
-}
-
-/// draw a rectangle with upper left coordinates, width, height and color
-pub fn draw_rectangle_3d(start_x:usize, start_y:usize, width:usize, height:usize, z:u8, color:u32) {
-    FRAME_DRAWER.lock().draw_rectangle_3d(start_x, start_y, width, height, z, color)
-}
-
-/// fill a rectangle with upper left coordinates, width, height and color
-pub fn fill_rectangle(start_x:usize, start_y:usize, width:usize, height:usize, color:u32) {
-    FRAME_DRAWER.lock().fill_rectangle_3d(start_x, start_y, width, height, 0, color)
-}
-
-/// fill a rectangle with upper left coordinates, width, height and color in 3D mode
-pub fn fill_rectangle_3d(start_x:usize, start_y:usize, width:usize, height:usize, z:u8, color:u32) {
-    FRAME_DRAWER.lock().fill_rectangle_3d(start_x, start_y, width, height, z, color)
-}
-/*pub struct Point {
-    pub x: usize,
-    pub y: usize,
-    pub color: usize,
-}*/
-
-// The drawer is responsible for drawing/printing to the screen
-pub struct Drawer {
-    pub width:usize,
+pub struct Buffer {
+    width:usize,
     height:usize,
-    pages:Option<MappedPages>,
+    pages:Option<MappedPages>
 }
 
-impl Drawer {
-    /*unsafe fn set_background(&mut self, offset:usize, len:usize, color:u32) {
-        asm!("cld
-            rep stosd"
-            :
-            : "{rdi}"(self.start_address + offset), "{eax}"(color), "{rcx}"(len)
-            : "cc", "memory", "rdi", "rcx"
-            : "intel", "volatile");
-    }*/
-
-    // set the graphic mode information of the buffer
+impl Buffer {
+        // set the graphic mode information of the buffer
     fn set_mode_info(&mut self, width:usize, height:usize, pages:MappedPages) {
         self.width = width;
         self.height = height;
         self.pages = Some(pages);
     }
 
-    //get the resolution of the screen
-    fn get_resolution(&self) -> (usize, usize) {
+    /// Get the resolution of the screen
+    pub fn get_resolution(&self) -> (usize, usize) {
         (self.width, self.height)
     }
 
-    fn draw_pixel_3d(&mut self, x:usize, y:usize, z:u8, color:u32) {
-        let (buffer_width, buffer_height) = {self.get_resolution()};
-        let index = self.get_index_fn();
-        let mut buffer;
-        match self.buffer() {
-            Ok(rs) => {buffer = rs;},
-            Err(err) => { debug!("Fail to get frame buffer: {}", err); return; },
-        }
-        if check_in_range(x as usize,y as usize, buffer_width, buffer_height) {
-            draw_in_buffer_3d(index(x, y), z, color, &mut buffer);
-        }
+    // Check if a point is in the screen
+    pub fn check_in_range(&self, x:usize, y:usize)  -> bool {
+        x < self.width && y < self.height
     }
 
-
-
-    pub fn draw_line_3d(&mut self, start_x:i32, start_y:i32, end_x:i32, end_y:i32, z:u8, 
-        color:u32) {
-        let width:i32 = end_x-start_x;
-        let height:i32 = end_y-start_y;
-        let mut drawer = FRAME_DRAWER.lock();
-        let (buffer_width, buffer_height) = {drawer.get_resolution()};
-        let index = drawer.get_index_fn();
-
-        let buffer = match drawer.buffer() {
-            Ok(rs) => {rs},
-            Err(err) => { debug!("Fail to get frame buffer: {}", err); return; },
-        };
-
-        if width.abs() > height.abs() {
-            let mut y;
-            let mut x = start_x;
-            let step = if width > 0 {1} else {-1};
-
-            loop {
-                if x == end_x {
-                    break;
-                }          
-                y = (x - start_x) * height / width + start_y;
-                if check_in_range(x as usize,y as usize, buffer_width, buffer_height) {
-                    draw_in_buffer_3d(index(x as usize, y as usize), z, color, buffer);
-                }
-                x += step;
-            }
-        } else {
-            let mut x;
-            let mut y = start_y;
-            let step = if height > 0 {1} else {-1};
-            loop {
-                if y == end_y {
-                    break;
-                }
-                x = (y - start_y) * width / height + start_x;
-                if check_in_range(x as usize,y as usize, buffer_width, buffer_height) {
-                    draw_in_buffer_3d(index(x as usize, y as usize), z, color, buffer);
-                }
-                y += step;   
-            }
+    fn draw(&mut self, index:usize, color:u32) {
+        match self.pages {
+            Some(ref mut pages) => {
+                let buffer = match (pages.as_slice_mut(0, self.width*self.height)) {
+                    Ok( bf) => { bf },
+                    Err(_) => { error!("Fail to transmute buffer"); return; }
+                };
+                buffer[index] = color;
+            },
+            None => { error!("Fail to get buffer") }
         }
     }
-
-    pub fn draw_rectangle_3d(&mut self, start_x:usize, start_y:usize, width:usize, height:usize, z:u8,
-     color:u32) {
-        let mut drawer = FRAME_DRAWER.lock();
-        let (buffer_width, buffer_height) = {drawer.get_resolution()};
-        let index = drawer.get_index_fn();
-
-        let buffer = match drawer.buffer() {
-            Ok(rs) => {rs},
-            Err(err) => { debug!("Fail to get frame buffer: {}", err); return; },
-        };
-
-        let end_x:usize = {if start_x + width < buffer_width { start_x + width } 
-            else { buffer_width }};
-        let end_y:usize = {if start_y + height < buffer_height { start_y + height } 
-            else { buffer_height }};  
-
-        let mut x = start_x;
-        loop {
-            if x == end_x {
-                break;
-            }
-            
-            draw_in_buffer_3d(index(x, start_y), z, color, buffer);
-            draw_in_buffer_3d(index(x, end_y-1), z, color, buffer);
-            x += 1;
-        }
-
-        let mut y = start_y;
-        loop {
-            if y == end_y {
-                break;
-            }
-
-            draw_in_buffer_3d(index(start_x, y), z, color, buffer);
-            draw_in_buffer_3d(index(end_x-1, y), z, color, buffer);
-            y += 1;
-        }
-    }
-
-    pub fn fill_rectangle_3d(&mut self, start_x:usize, start_y:usize, width:usize, height:usize, z:u8,
-        color:u32) {
-        let mut drawer = FRAME_DRAWER.lock();
-        let (buffer_width, buffer_height) = {drawer.get_resolution()};
-        let index = drawer.get_index_fn();
-
-        let buffer = match drawer.buffer() {
-            Ok(rs) => {rs},
-            Err(err) => { debug!("Fail to get frame buffer: {}", err); return; },
-        };
-        
-        let end_x:usize = {if start_x + width < buffer_width { start_x + width } 
-            else { buffer_width }};
-        let end_y:usize = {if start_y + height < buffer_height { start_y + height } 
-            else { buffer_height }};  
-
-        let mut x = start_x;
-        let mut y = start_y;
-        loop {
-            if x == end_x {
-                y += 1;
-                if y == end_y {
-                    break;
-                }
-                x = start_x;
-            }
-
-            draw_in_buffer_3d(index(x, y), z, color, buffer);
-            x += 1;
-        }
-    }
-
 
     // return the framebuffer
     pub fn buffer(&mut self) -> Result<&mut[u32], &'static str> {
@@ -335,18 +156,17 @@ impl Drawer {
 
 }
 
-// Get the resolution of the screen
-pub fn get_resolution() -> (usize, usize) {
-    FRAME_DRAWER.lock().get_resolution()
+pub fn draw_in_buffer(index:usize, color:u32,  buffer:&mut[u32]) {
+    draw_in_buffer_3d(index, 0, color,  buffer)
 }
 
-// Check if a point is in the screen
-fn check_in_range(x:usize, y:usize, width:usize, height:usize)  -> bool {
-    x < width && y < height
-}
-
-fn draw_in_buffer_3d(index:usize, z:u8, color:u32,  buffer:&mut[u32]) {
+pub fn draw_in_buffer_3d(index:usize, z:u8, color:u32,  buffer:&mut[u32]) {
     if  (buffer[index] >> COLOR_BITS) <= z as u32 {
         buffer[index] = ((z as u32) << COLOR_BITS) + color;
     }
+}
+
+// Check if a point is in the screen
+pub fn check_in_range(x:usize, y:usize, width:usize, height:usize)  -> bool {
+    x < width && y < height
 }
