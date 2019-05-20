9fad9263d3c08e78bed7f6ae43dc380543bfaace
snoword <qiuj@mail.bnu.edu.cn>
2019-05-20 07:19:19 -0500

    merge

---
+++ ttttt/src/lib.rs
@@ -0,0 +1,45 @@
+#![no_std]
+
+/// Defines the methods that a TextDisplay must have
+pub struct TextDisplay {
+    x:usize,
+    y:usize,
+    width:usize,
+    height:usize,
+}
+
+impl TextDisplay
+{
+    pub fn new(x:usize, y:usize, width:usize, height:usize) -> TextDisplay {
+        TextDisplay{
+            x:x,
+            y:y,
+            width:width,
+            height:height
+        }
+    }
+
+    /// takes in a str slice and display as much as it can to the screen
+    pub fn display_string(&self, slice: &str) -> Result<(), &'static str>{
+        Ok(())
+    }
+
+    /// Function to set a cursor on the display at an (x,y) position. 
+    /// set_cursor() should accept coordinates within those specified by get_dimensions() and display to window
+    pub fn set_cursor(&self, line: u16, column: u16, reset:bool){
+
+    } 
+    /// Take the cursor off the display
+    pub fn disable_cursor(&mut self){
+
+    }
+    /// Display the cursor and let it blinks
+    pub fn cursor_blink(&self){
+
+    }
+
+    pub fn get_size(&self) -> (usize, usize, usize, usize){
+        (self.x, self.y, self.width, self.height)
+    }
+}
+
