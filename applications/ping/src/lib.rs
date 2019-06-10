112cbead599f7de30c90bb15ffc3c3cf2b8795f7
bshiberu1 <bms6@rice.edu>
2019-06-10 14:25:16 -0500

    fixing comments, removing macros, removing unwrap statements

--- applications/ping/src/lib.rs
+++ applications/ping/src/lib.rs
@@ -18,6 +18,7 @@ extern crate hashbrown;
 extern crate ota_update_client;
 extern crate getopts;
 
+
 use getopts::{Matches, Options};
 use core::str::FromStr;
 use hashbrown::HashMap;
@@ -36,47 +37,12 @@ use http_client::{millis_since, poll_iface};
 
 macro_rules! hpet_ticks {
     () => {
-        get_hpet().as_ref().ok_or("coudln't get HPET timer").unwrap().get_counter()
+        match get_hpet().as_ref().ok_or("coudln't get HPET timer") {
+            Ok(time) => time.get_counter(),
+            Err(_) => return println!("couldnt get HPET timer"),
+        }; 
     };
 }
-
-macro_rules! send_icmp_ping {
-    ( $repr_type:ident, $packet_type:ident, $ident:expr, $seq_no:expr,
-    $echo_payload:expr, $socket:expr, $remote_addr:expr ) => {{
-        let icmp_repr = $repr_type::EchoRequest {
-            ident: $ident,
-            seq_no: $seq_no,
-            data: &$echo_payload,
-        };
-
-        let icmp_payload = $socket
-            .send(icmp_repr.buffer_len(), $remote_addr)
-            .unwrap();
-
-        let icmp_packet = $packet_type::new_unchecked(icmp_payload);
-        (icmp_repr, icmp_packet)
-    }}
-}
-
-
-macro_rules! get_icmp_pong {
-    ( $repr_type:ident, $repr:expr, $payload:expr, $waiting_queue:expr, $remote_addr:expr,
-    $timestamp:expr, $received:expr, $total_time:expr ) => {{
-        if let $repr_type::EchoReply { seq_no, data, .. }     = $repr {
-            if let Some(_) = $waiting_queue.get(&seq_no) {
-                let packet_timestamp_ms = NetworkEndian::read_i64(data);
-                println!("{} bytes from {}: icmp_seq={}, time={}ms",
-                        data.len(), $remote_addr, seq_no,
-                        $timestamp - packet_timestamp_ms);
-                $waiting_queue.remove(&seq_no);
-                $received += 1;
-                $total_time += $timestamp - packet_timestamp_ms;
-            }
-        }
-    }}
-}
-
-
 
 
 #[no_mangle]
@@ -84,12 +50,12 @@ fn main(args: Vec<String>) -> isize {
 
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
-    opts.optopt("c", "count", "Amount of echo request packets to send (default: 4)", "N");
-    opts.optopt("i", "interval", "interval between packets being sent in miliseconds (default: 5000)", "N");
-    opts.optopt("t", "timeout", "maximum time between echo request and echo reply in milliseconds(default: 1500)", "N");
+    opts.optflag("v", "verbose", "a more detailed view of packets sent and received");
+    opts.optopt("c", "count", "amount of echo request packets to send (default: 4)", "N");
+    opts.optopt("i", "interval", "interval between packets being sent in miliseconds (default: 500)", "N");
+    opts.optopt("t", "timeout", "maximum time between echo request and echo reply in milliseconds (default: 10000)", "N");
     
-
-
+  
     let matches = match opts.parse(&args) {
         Ok(m) => m,
         Err(_f) => {
@@ -136,28 +102,31 @@ fn main(args: Vec<String>) -> isize {
     }
 }
 
-pub fn rmain(matches: &Matches, opts: Options, address: IpAddress) -> Result<(), &'static str> {
+pub fn rmain(matches: &Matches, _opts: Options, address: IpAddress) -> Result<(), &'static str> {
 
     
     let mut count = 4;
     let mut interval = 500;
-    let mut timeout = 15000;
+    let mut timeout = 10000;
+    let mut verbose = false;
     let did_work = true;
 
 
     if let Some(i) = matches.opt_default("c", "4") {
-        count = i.parse::<usize>().map_err(|_e| "couldn't parse number of num_tasks")?;
+        count = i.parse::<usize>().map_err(|_e| "couldn't parse number of packets")?;
     }
     if let Some(i) = matches.opt_default("i", "500") {
         interval = i.parse::<i64>().map_err(|_e| "couldn't parse interval")?;
     }
-    if let Some(i) = matches.opt_default("t", "1500") {
-        timeout = i.parse::<i64>().map_err(|_e| "couldn't parse interval")?;
+    if let Some(i) = matches.opt_default("t", "10000") {
+        timeout = i.parse::<i64>().map_err(|_e| "couldn't parse timeout length")?;
+    }
+    if matches.opt_present("v") {
+        verbose = true;
     }
     
-
     if did_work {
-        ping(address, count, interval, timeout);
+        ping(address, count, interval, timeout, verbose);
         Ok(())
     }
     else {
@@ -165,7 +134,7 @@ pub fn rmain(matches: &Matches, opts: Options, address: IpAddress) -> Result<(),
     }
 }
 
-//used to gain access to the ethernet interface
+/// Used to gain access to the ethernet interface
 fn get_default_iface() -> Result<NetworkInterfaceRef, String> {
     NETWORK_INTERFACES.lock()
         .iter()
@@ -174,41 +143,72 @@ fn get_default_iface() -> Result<NetworkInterfaceRef, String> {
         .ok_or_else(|| format!("no network interfaces available"))
 }
 
-fn ping(address: IpAddress, count: usize, interval: i64, timeout: i64) {
+// Retrieves the echo reply contained in the recieve buffer and prints data pertaining to the packet
+fn get_icmp_pong (waiting_queue: &mut HashMap<u16, i64>, times: &mut Vec<i64>, mut total_time: i64, 
+    repr: Icmpv4Repr, mut recieved: u16, remote_addr: IpAddress, timestamp: i64)  {
+    
+    if let Icmpv4Repr::EchoReply { seq_no, data, ..} = repr {
+        if let Some(_) = waiting_queue.get(&seq_no) {
+            let packet_timestamp_ms = NetworkEndian::read_i64(data);
+            
+            println!("{} bytes from {}: icmp_seq={}, time={}ms",
+                        data.len(), remote_addr, seq_no,
+                        timestamp - packet_timestamp_ms);
+            
+            waiting_queue.remove(&seq_no);
+            recieved += 1;
+            times.push((timestamp - packet_timestamp_ms) as i64);
+            total_time += timestamp - packet_timestamp_ms;
+        }
+    } 
+}
+
+fn ping(address: IpAddress, count: usize, interval: i64, timeout: i64, verbose: bool) {
 
     let startup_time = hpet_ticks!() as u64;
     let remote_addr = address;
+    let mut times = Vec::new();
     
-    //initialize the ICMP sockett using the smoltcp function using a transmit packet buffer 
-    //and receiving packet buffer
+    // Initialize the ICMP sockett using the smoltcp function using a transmit packet buffer 
+    // and receiving packet buffer
     //
-    //The payload storage contains the application data and the transport header, and metadata contains the ICMP type
-    //and ICMP code, which are used to classify between echo requests and echo replies
+    // The payload storage contains the application data and the transport header, and metadata contains the ICMP type
+    // and ICMP code, which are used to classify between echo requests and echo replies
     let icmp_rx_buffer = IcmpSocketBuffer::new(vec![IcmpPacketMetadata::EMPTY], vec![0; 256]);
     let icmp_tx_buffer = IcmpSocketBuffer::new(vec![IcmpPacketMetadata::EMPTY], vec![0; 256]);
     let icmp_socket = IcmpSocket::new(icmp_rx_buffer, icmp_tx_buffer);
     
-    //get the default ethernet interface to ping with
-    let iface = get_default_iface().unwrap(); 
+    // Get the default ethernet interface to ping with
+    let iface_result = get_default_iface();
+    let iface = match iface_result {
+        Ok(network) => network,
+        Err(err) => return println!("couldn't initialize the network: {}", err),
+    };
+
     let mut sockets = SocketSet::new(vec![]);
     let icmp_handle = sockets.add(icmp_socket);
-    let mut send_at = millis_since(startup_time as u64).unwrap() as i64; 
+    
+    let mut send_at = match millis_since(startup_time as u64) {
+        Ok(time) => time as i64,
+        Err(err) => return println!("couldn't get time since start_up: {}", err),
+    };
+    
     let mut seq_no = 0;
-    let mut received = 0;
-    let mut total_time = 0;
+    let received = 0;
+    let total_time = 0;
     let mut echo_payload = [0xffu8; 40];
 
-    //designate no checksum capabilities 
+    // Designate no checksum capabilities 
     let checksum_caps = ChecksumCapabilities::ignored();
     
-    //initiate a hashmap
+    // Initiate a hashmap
     let mut waiting_queue = HashMap::new();  
     
-    //portless icmp messages such as echo request require a 16-bit identifier to bind to
-    //so that only icmp message with this identifer can pass through the icmp socket
+    // Portless icmp messages such as echo request require a 16-bit identifier to bind to
+    // so that only icmp messages with this identifer can pass through the icmp socket
     let ident = 0x22b; 
 
-    //makes sure that the icmp handle can communicate with the given ethernet interface
+    // Makes sure that the icmp handle can communicate with the given ethernet interface
     loop {
         
         match poll_iface(&iface, &mut sockets, startup_time) {
@@ -218,55 +218,79 @@ fn ping(address: IpAddress, count: usize, interval: i64, timeout: i64) {
             }
         }
         {
-            let timestamp = millis_since(startup_time as u64).unwrap() as i64;
+            let timestamp = match millis_since(startup_time as u64) {
+                Ok(time) => time as i64,
+                Err(err) => return println!("couldn't get timestamp:{}", err),
+            };
             let mut socket = sockets.get::<IcmpSocket>(icmp_handle); 
             
-            //checks if the icmp socket is open, and only bind the identifier icmp to it if 
-            //it is closed
+            // Checks if the icmp socket is open, and only bind the identifier icmp to it if 
+            // it is closed
             if !socket.is_open() {
-                println!("the socket is binded to");
-                socket.bind(IcmpEndpoint::Ident(ident)).unwrap();
+                match socket.bind(IcmpEndpoint::Ident(ident)) {
+                    Ok(_) => (),
+                    Err(e) => return println!("the socket failed to bind: {}", e),
+                }; 
                 send_at = timestamp;
             }
             
-            //checks if the icmp sockett can send an echo request
-            if socket.can_send() && seq_no < count as u16 && send_at <= timestamp 
-                {
-                println!("\n -- icmp_seq={} -- \n", seq_no);
+            // Checks if the icmp sockett can send an echo request
+            if socket.can_send() && seq_no < count as u16 && send_at <= timestamp {
                 NetworkEndian::write_i64(&mut echo_payload, timestamp);
-                let (icmp_repr, mut icmp_packet) = send_icmp_ping!(
-                                Icmpv4Repr, Icmpv4Packet, ident, seq_no,
-                                echo_payload, socket, remote_addr);
+                let icmp_repr = Icmpv4Repr::EchoRequest{
+                        ident: ident,
+                        seq_no: seq_no,
+                        data: &echo_payload
+                    };
+                let icmp_payload = match socket.send(icmp_repr.buffer_len(), remote_addr) {
+                    Ok(payload) => payload,
+                    Err(_err) => return println!("the icmp socket cannot send"),
+                };
+                let mut icmp_packet = Icmpv4Packet::new_unchecked(icmp_payload);
+                
                 
                 icmp_repr.emit(&mut icmp_packet, &checksum_caps); //turns or "emits" the raw network stack into an icmpv4 packet,
-                println!("buffer length: {}", icmp_repr.buffer_len());
-                println!("checking checksum of packet, should be 0: {:?}", icmp_packet.checksum());
-                println!("checking echo_ident of packet, should be a value: {:?}", icmp_packet.echo_ident());
-                println!("checking msg_type of packet, should be an echo_request: {:?}", icmp_packet.msg_type());
-            
-            //insert the sequence number into the waiting que along with the timestamp after an echo
-            //request has been sent
+                if verbose {
+                    println!("buffer length: {}", icmp_repr.buffer_len());
+                    println!("checking checksum of packet, should be 0: {:?}", icmp_packet.checksum());
+                    println!("checking echo_ident of packet, should be a value: {:?}", icmp_packet.echo_ident());
+                    println!("checking msg_type of packet, should be an echo_request: {:?}", icmp_packet.msg_type());
+                }
+            // Insert the sequence number into the waiting que along with the timestamp after an echo
+            // Request has been sent
             waiting_queue.insert(seq_no, timestamp);
             seq_no += 1;
             send_at += interval;
             }
 
-            //once the socket can successfully recieve the echo reply, unload the payload and unwrap it
-            //then return the current time as well as wether the ping has been recieved         
+            // Once the socket can successfully recieve the echo reply, unload the payload and
+            // then return the current time as well as wether the ping has been recieved         
             if socket.can_recv() {
-                let (payload, _) = socket.recv().unwrap();
-                let icmp_packet = Icmpv4Packet::new_checked(&payload).unwrap();
-                let icmp_repr = 
-                    Icmpv4Repr::parse(&icmp_packet, &checksum_caps).unwrap(); //turns or "parses" the ICMPv4 packet into a raw level representation
-                    get_icmp_pong!(Icmpv4Repr, icmp_repr, payload,
-                                waiting_queue, remote_addr, timestamp, received, total_time);
-                println!("buffer length: {}", icmp_repr.buffer_len());
-                println!("checking checksum of packet, should be above 0: {:?}", icmp_packet.checksum());
-                println!("checking echo_ident of packet, should be a value: {:?}", icmp_packet.echo_ident());
-                println!("checking msg_type of packet, should be an echo_reply: {:?}", icmp_packet.msg_type());        
-                    }
+                let (payload, _) = match socket.recv() {
+                    Ok((packet_buff,end_point)) => (packet_buff, end_point),
+                    Err(err) => return println!("err: {} the receive buffer is empty", err), 
+                }; 
+                let icmp_packet = match Icmpv4Packet::new_checked(&payload) {
+                    Ok(packet) => packet,
+                    Err(err) => return println!("err: {}", err),
+                }; 
+                // Turns or "parses" the ICMPv4 packet into a raw level representation
+                let icmp_repr = match Icmpv4Repr::parse(&icmp_packet, &checksum_caps) {
+                    Ok(repr) => repr,
+                    Err(err) => return println!("err: {}", err),
+                }; 
+                
+        
+                get_icmp_pong(&mut waiting_queue, &mut times, total_time, icmp_repr, received, remote_addr, timestamp);
+                if verbose {
+                    println!("buffer length: {}", icmp_repr.buffer_len());
+                    println!("checking checksum of packet, should be above 0: {:?}", icmp_packet.checksum());
+                    println!("checking echo_ident of packet, should be a value: {:?}", icmp_packet.echo_ident());
+                    println!("checking msg_type of packet, should be an echo_reply: {:?}", icmp_packet.msg_type());                
+                }
+            }
             
-            //uses this retain function to decide whether the sequence you're currently looking at is timed out 
+            // Uses this retain function to decide whether the sequence you're currently looking at is timed out 
             waiting_queue.retain(|seq, from| {
                 if timestamp - *from <  timeout {
                     true
@@ -283,25 +307,39 @@ fn ping(address: IpAddress, count: usize, interval: i64, timeout: i64) {
     
 
     }
-    let mut avg_ping = 0;
     
-    if received == 0 {
+    // Computes ping min/avg/max
+    let avg_ping = if received != 0 {
+        total_time as f64 / (received as f64)
+    } else {
+        0 as f64
+    };
+     
+    let min_ping = match times.iter().min() {
+            Some(min) => min,
+            None => &(0 as i64),
+    };
+
+    let max_ping = match times.iter().max() {
+            Some(max) => max,
+            None => &(0 as i64),
+    };
         
-    }
-    else {
-        avg_ping = total_time/(received as i64);
-    }
     
-    println!("--- {} ping statistics ---", remote_addr);
-    println!("{} packets transmitted, {} received, {:.0}% packet loss, {}ms average latency",
-            seq_no, received, 100.0 * (seq_no - (received)) as f64 / seq_no as f64, avg_ping);
     
+    
+    println!("\n--- {} ping statistics ---", remote_addr);
+    println!("{} packets transmitted, {} received, {:.0}% packet loss \nrtt min/avg/max = {}/{}/{}",
+            seq_no, received, 100.0 * (seq_no - (received)) as f64 / seq_no as f64, min_ping, avg_ping as f64, max_ping);
+    if received == 0{         
+            println!("\nwarning: Ping/ICMP will not work in QEMU unless you specifically enable it. If you are able to ping  \nthe qemu gateway address 10.0.2.2 and not other addresses, your ICMP is most likely disabled");
+    }
 
 
 }
 
 fn print_usage(opts: &Options) -> isize {
-    let mut brief = format!("Usage: ping \n \n");
+    let mut brief = format!("Usage: ping DESTINATION \n \n");
 
     brief.push_str("pings an IPv4 address and returns ping statistics");
 
