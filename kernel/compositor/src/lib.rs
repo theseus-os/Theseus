64679716606410b77234d65952b8123a2125f7c0
snoword <qiuj@mail.bnu.edu.cn>
2019-06-21 15:17:45 -0500

    separate frame buffer and compositor

---
+++ kernel/compositor/src/lib.rs
@@ -0,0 +1,14 @@
+//! This crate defines a trait of Compositor.
+//! A compositor composites a list of buffers to a single buffer
+
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+
+/// The compositor trait.
+///* It composes a list of 2-D buffers to a single buffer
+pub trait Compositor<T> {
+    fn compose(bufferlist: Vec<(&T, i32, i32)>) -> Result<(), &'static str>;
+}
---
+++ kernel/frame_buffer_compositor/src/lib.rs
@@ -0,0 +1,83 @@
+//! This crate is a frame buffer manager. 
+//! * It defines a FrameBuffer structure and creates new framebuffers for applications
+//! * It defines a compositor and owns a final framebuffer which is mapped to the physical framebuffer. The compositor will composite a sequence of framebuffers and display them in the final framebuffer
+
+#![no_std]
+
+extern crate spin;
+extern crate acpi;
+extern crate memory;
+extern crate alloc;
+extern crate owning_ref;
+extern crate frame_buffer;
+extern crate compositor;
+#[macro_use] extern crate log;
+
+use owning_ref::BoxRefMut;
+use spin::{Mutex, Once};
+use memory::{FRAME_ALLOCATOR, Frame, PageTable, PhysicalAddress, 
+    EntryFlags, MappedPages, MemoryManagementInfo,
+    get_kernel_mmi_ref};
+use core::ops::DerefMut;
+use alloc::boxed::Box;
+use alloc::vec::Vec;
+use frame_buffer::{FrameBuffer, FINAL_FRAME_BUFFER};
+use compositor::Compositor;
+
+pub type Pixel = u32;
+
+// Every pixel is of u32 type
+const PIXEL_BYTES: usize = 4;
+
+/// The framebuffer compositor structure. It will hold the cache of updated framebuffers for better performance.
+/// Only framebuffers that have not changed will be redisplayed in the final framebuffer 
+pub struct FrameCompositor {
+    //Cache of updated framebuffers
+}
+
+impl Compositor<FrameBuffer> for FrameCompositor {
+    /// compose a list of framebuffers to the final framebuffer. Every item in the list is a reference to a framebuffer with its position
+    fn compose(bufferlist: Vec<(&FrameBuffer, i32, i32)>) -> Result<(), &'static str> {
+        let mut final_fb = FINAL_FRAME_BUFFER.try().ok_or("FrameCompositor fails to get the final frame buffer")?.lock();
+        let (final_width, final_height) = final_fb.get_size();        
+        let mut final_buffer = final_fb.buffer_mut();
+        // Check if the virtul frame buffer is in the mapped frame list
+        for (src_fb, offset_x, offset_y) in bufferlist {
+            let (src_width, src_height) = src_fb.get_size();
+
+            let final_x_end = offset_x + src_width as i32;
+            let final_y_end = offset_y + src_height as i32;
+
+            // skip if the framebuffer is not in the screen
+            if (final_x_end < 0 || offset_x > final_width as i32) {
+                break;
+            }
+            if (final_y_end < 0 || offset_y > final_height as i32) {
+                break;
+            }
+
+            let final_x_start = core::cmp::max(0, offset_x) as usize;
+            let final_y_start = core::cmp::max(0, offset_y) as usize;
+
+            // just composite the area that within the final buffer
+            let width = core::cmp::min(final_x_end as usize, final_width) - final_x_start;
+            let height = core::cmp::min(final_y_end as usize, final_height) - final_y_start;
+            
+            let src_buffer = src_fb.buffer();
+
+            for i in 0..height {
+                let dest_start = (final_y_start + i) * final_width + final_x_start;
+                let dest_end = dest_start + width;
+                let src_start = src_width * ((final_y_start + i) as i32 - offset_y) as usize + 
+                    (final_x_start as i32 - offset_x) as usize;
+                let src_end = src_start + width;
+
+                final_buffer[dest_start..dest_end].copy_from_slice(
+                    &(src_buffer[src_start..src_end])
+                );
+            }
+        }
+
+        Ok(())
+    }
+}
