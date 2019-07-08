9622a425ca1abd94840fb42b89f53141cf782a34
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2019-07-08 16:43:38 -0700

    PCI and interrupt improvements. Re-do support for ATA drives.

--- libs/port_io/src/lib.rs
+++ libs/port_io/src/lib.rs
@@ -6,101 +6,128 @@
 
 use core::marker::PhantomData;
 
-#[cfg(any(target_arch="x86", target_arch="x86_64"))]
-pub use x86::{inb, outb, inw, outw, inl, outl};
-
+// These cfg statements should cause compiler errors on non-x86 platforms.
 #[cfg(any(target_arch="x86", target_arch="x86_64"))]
 mod x86;
 
+#[cfg(any(target_arch="x86", target_arch="x86_64"))]
+pub use x86::{inb, outb, inw, outw, inl, outl};
 
-/// This trait is defined for any type which can be read or written over a
-/// port.  The processor supports I/O with `u8`, `u16` and `u32`.  The
-/// functions in this trait are all unsafe because they can write to
-/// arbitrary ports.
-pub trait InOut {
+
+/// This trait is defined for any type which can be read from a port.
+/// x86 processors support Port IO for `u8`, `u16` and `u32`.
+pub trait PortIn {
     /// Read a value from the specified port.
     unsafe fn port_in(port: u16) -> Self;
+}
 
+/// This trait is defined for any type which can be read from a port.
+/// x86 processors support Port IO for `u8`, `u16` and `u32`.
+pub trait PortOut {
     /// Write a value to the specified port.
     unsafe fn port_out(port: u16, value: Self);
 }
 
-impl InOut for u8 {
-    unsafe fn port_in(port: u16) -> u8 { inb(port) }
-    unsafe fn port_out(port: u16, value: u8) { outb(value, port); }
+impl PortOut for u8 {
+    unsafe fn port_out(port: u16, value: Self) {
+        outb(value, port);
+    }
+}
+impl PortOut for u16 {
+    unsafe fn port_out(port: u16, value: Self) {
+        outw(value, port);
+    }
+}
+impl PortOut for u32 {
+    unsafe fn port_out(port: u16, value: Self) {
+        outl(value, port);
+    }
 }
 
-impl InOut for u16 {
-    unsafe fn port_in(port: u16) -> u16 { inw(port) }
-    unsafe fn port_out(port: u16, value: u16) { outw(value, port); }
+impl PortIn for u8 {
+    unsafe fn port_in(port: u16) -> Self {
+        inb(port)
+    }
+}
+impl PortIn for u16 {
+    unsafe fn port_in(port: u16) -> Self {
+        inw(port)
+    }
+}
+impl PortIn for u32 {
+    unsafe fn port_in(port: u16) -> Self {
+        inl(port)
+    }
 }
 
-impl InOut for u32 {
-    unsafe fn port_in(port: u16) -> u32 { inl(port) }
-    unsafe fn port_out(port: u16, value: u32) { outl(value, port); }
-}
 
-/// An I/O port over an arbitrary type supporting the `InOut` interface.
-///
-/// This version of `Port` has safe `read` and `write` functions, and it's
-/// appropriate for communicating with hardware that can't violate Rust's
-/// safety guarantees.
+
+/// A readable and writable I/O port over an arbitrary type supporting the `InOut` interface,
+/// which is only `u8`, `u16`, or `u32`.
+// /// which only includes types that losslessly convert into a `u8`, `u16`, and `u32`.
 #[derive(Debug)]
-pub struct Port<T: InOut> {
-    // Port address.
+pub struct Port<T: PortIn + PortOut> {
+    /// Port address (in the I/O space), which is always 16 bits.
     port: u16,
-
-    // Zero-byte placeholder.  This is only here so that we can have a
-    // type parameter `T` without a compiler error.
-    phantom: PhantomData<T>,
+    _phantom: PhantomData<T>,
 }
-
-impl<T: InOut> Port<T> {
+impl<T: PortIn + PortOut> Port<T> {
     /// Create a new I/O port.
     pub const fn new(port: u16) -> Port<T> {
-        Port { port: port, phantom: PhantomData }
+        Port { port: port, _phantom: PhantomData }
     }
 
-    /// Read data from the port.  This is nominally safe, because you
-    /// shouldn't be able to get hold of a port object unless somebody
-    /// thinks it's safe to give you one.
+    /// Read data of size `T` from the port.
     pub fn read(&self) -> T {
         unsafe { T::port_in(self.port) }
     }
 
-    /// Write data to the port. This is unsafe because writing to an arbitrary port may cause problems.
+    /// Write data of size `T` to the port.
+    /// This is unsafe because writing to an arbitrary port may cause problems.
     pub unsafe fn write(&self, value: T) {
         T::port_out(self.port, value);
     }
 }
 
 
+/// A read-only I/O port over an arbitrary type supporting the `InOut` interface,
+/// which is only `u8`, `u16`, and `u32`.
+#[derive(Debug)]
+pub struct PortReadOnly<T: PortIn> {
+    /// Port address (in the I/O space), which is always 16 bits.
+    port: u16,
+    _phantom: PhantomData<T>,
+}
+impl<T: PortIn> PortReadOnly<T> {
+    /// Create a new read-only I/O port.
+    pub const fn new(port: u16) -> PortReadOnly<T> {
+        PortReadOnly { port: port, _phantom: PhantomData }
+    }
 
-// An unsafe I/O port over an arbitrary type supporting the `InOut`
-// interface.
-//
-// This version of `Port` has unsafe `read` and `write` functions, and
-// it's appropriate for speaking to hardware that can potentially corrupt
-// memory or cause undefined behavior.
-// #[derive(Debug)]
-// pub struct UnsafePort<T: InOut> {
-//     port: u16,
-//     phantom: PhantomData<T>,
-// }
+    /// Read data of size `T` from the port.
+    pub fn read(&self) -> T {
+        unsafe { T::port_in(self.port) }
+    }
+}
 
-// impl<T: InOut> UnsafePort<T> {
-//     /// Create a new I/O port.
-//     pub const unsafe fn new(port: u16) -> UnsafePort<T> {
-//         UnsafePort { port: port, phantom: PhantomData }
-//     }
 
-//     /// Read data from the port.
-//     pub unsafe fn read(&mut self) -> T {
-//         T::port_in(self.port)
-//     }
+/// A write-only I/O port over an arbitrary type supporting the `InOut` interface,
+/// which is only `u8`, `u16`, and `u32`.
+#[derive(Debug)]
+pub struct PortWriteOnly<T: PortOut> {
+    /// Port address (in the I/O space), which is always 16 bits.
+    port: u16,
+    _phantom: PhantomData<T>,
+}
+impl<T: PortOut> PortWriteOnly<T> {
+    /// Create a new read-only I/O port.
+    pub const fn new(port: u16) -> PortWriteOnly<T> {
+        PortWriteOnly { port: port, _phantom: PhantomData }
+    }
 
-//     /// Write data to the port.
-//     pub unsafe fn write(&mut self, value: T) {
-//         T::port_out(self.port, value);
-//     }
-// }
+    /// Write data of size `T` to the port.
+    /// This is unsafe because writing to an arbitrary port may cause problems.
+    pub unsafe fn write(&self, value: T) {
+        T::port_out(self.port, value);
+    }
+}
