3730268d0375b6abc5c44fb43e7d651972af9cb6
Kevin Boos <kevinaboos@gmail.com>
2019-07-18 13:48:59 -0700

    Better API for `StorageDevice` iteration, usage, and downcasting to conrete types like `AtaDrive`.

--- kernel/storage_device/src/lib.rs
+++ kernel/storage_device/src/lib.rs
@@ -4,6 +4,46 @@
 //! such as hard disk drives, optical discs, SSDs, etc. 
 //! All storage controllers should implement the `StorageController` trait,
 //! such as AHCI controllers and IDE controllers.
+//! 
+//! Below is a quick example of how to iterate through available `StorageDevice`s on the system
+//! and usage of the `StorageDevice` trait methods, as well as how to downcast it
+//! into a specific concrete type, such as an `AtaDrive`.
+//! ```rust
+//! if let Some(controller) = storage_manager::STORAGE_CONTROLLERS.lock().iter().next() {
+//!     if let Some(sd) = controller.lock().devices().next() {
+//!         {
+//!             // Call `StorageDevice` trait methods directly
+//!             let mut locked_sd = sd.lock();
+//!             debug!("Found drive with size {}, {} sectors", locked_sd.size_in_bytes(), locked_sd.size_in_sectors());
+//! 
+//!             // Here we downcast the `StorageDevice` into an `AtaDrive` so we can call `AtaDrive` methods.
+//!             if let Some(ata_drive) = locked_sd.as_any_mut().downcast_mut() {
+//!                 debug!("      drive was master? {}", ata_drive.is_master());
+//! 
+//!                 // Read 10 sectors from the drive
+//!                 let mut initial_buf: [u8; 5120] = [0; 5120]; // 10 sectors of bytes
+//!                 let sectors_read = ata_drive.read_pio(&mut initial_buf[..], 0);
+//!                 debug!("[SUCCESS] sectors_read: {:?}", sectors_read);
+//!                 debug!("{:?}", core::str::from_utf8(&initial_buf));
+//! 
+//!                 // Write 3 sectors to the drive
+//!                 let mut write_buf = [0u8; 512*3];
+//!                 for b in write_buf.chunks_exact_mut(16) {
+//!                     b.copy_from_slice(b"QWERTYUIOPASDFJK");
+//!                 }
+//!                 let bytes_written = ata_drive.write_pio(&write_buf[..], 2);
+//!                 debug!("WRITE_PIO {:?}", bytes_written);
+//!             }
+//!         }
+//!         // Read 10 sectors from the drive using the `StorageDevice` trait methods.
+//!         let mut after_buf: [u8; 5120] = [0; 5120];
+//!         let sectors_read = sd.lock().read_sectors(&mut after_buf[..], 0)?;
+//!         debug!("{:X?}", &after_buf[..]);
+//!         debug!("{:?}", core::str::from_utf8(&after_buf));
+//!         trace!("POST-WRITE READ_SECTORS {} sectors", sectors_read);
+//!     }
+//! }
+//! ```
 
 #![no_std]
 
@@ -25,7 +65,12 @@ pub trait StorageController {
     /// Returns an iterator of references to all `StorageDevice`s attached to this controller.
     /// The lifetime of the iterator and the device references it returns are both bound
     /// by the lifetime of this `StorageController`.
-    fn devices<'c>(&'c self) -> Box<(dyn Iterator<Item = &'c dyn StorageDevice> + 'c)>;
+    /// 
+    /// # Note
+    /// I would prefer to have this Iterator return a `&StorageDeviceRef`,
+    /// but Rust does not permit casts from `&Arc<Mutex<Struct>>` to `&Arc<Mutex<Trait>>`,
+    /// it only supports casts from `Arc<Mutex<Struct>>` to `Arc<Mutex<Trait>>`.
+    fn devices<'c>(&'c self) -> Box<(dyn Iterator<Item = StorageDeviceRef> + 'c)>;
 }
 
 /// A trait object wrapped in an Arc and Mutex that allows 
