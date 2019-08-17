813ec7d1e0aad8f95147bf6d6d77af98f9f20411
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2019-08-16 18:53:12 -0700

    Crate namespaces are now recursive. Tasks run within a namespace. (#185)
    * Default behavior is now for a single CrateNamespace including all of the kernel crates to be created upon boot, and another one including all of the application crates to be created shortly thereafter. The app namespace is built atop the kernel namespace and can use its symbols/crates. 
    
    * Tasks now run "within" a namespace, meaning that a Task is aware of which namespace it was loaded into. This allows applications to form a "process group", e.g., multiple application crates are in a single namespace, while several other application crates may be in a separate namespace, and they all are linked to an underlying single namespace. 
    
    * Cleaned up Task struct by removing several unnecessary Option wrappers, e.g., around MMI and kstack.
    
    * Application crates' hash suffixes are now preserved, just like for kernel crates. 
    
    * The `lsmod` app is now called `ns` for namespace.

--- kernel/environment/src/lib.rs
+++ kernel/environment/src/lib.rs
@@ -2,15 +2,22 @@
 
 extern crate alloc;
 extern crate fs_node;
+extern crate root;
 
-use alloc::string::String;
+use alloc::{
+    string::String,
+    sync::Arc,
+};
 use fs_node::DirRef;
 
-/// A structure that contains Environmnt variables for a given task
-/// For now, the one variable is the current working directory of the task, which is 
-/// stored as a strong pointer to a directory within the filesystem
+/// A structure that contains environment state for a given `Task` or group of `Task`s.
+/// 
+/// A default environment can be created with the following state:
+/// * The working directory is the `root` directory.
+///
 pub struct Environment {
-    /// The working directory for given tasks
+    /// The "current working directory", i.e., 
+    /// where a task's relative path begins upon first execution.
     pub working_dir: DirRef, 
 }
 
@@ -19,5 +26,13 @@ impl Environment {
     pub fn get_wd_path(&self) -> String {
         let wd = self.working_dir.lock();
         wd.get_absolute_path()
+    }
+}
+
+impl Default for Environment {
+    fn default() -> Environment {
+        Environment {
+            working_dir: Arc::clone(root::get_root()),
+        }
     }
 }
