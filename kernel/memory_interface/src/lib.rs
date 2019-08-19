16d68be5723dc35f84f05cd82d03b1a81b430141
snoword <qiuj@mail.bnu.edu.cn>
2019-08-19 06:35:21 -0500

    create new abstract layer of memory

---
+++ kernel/memory_interface/src/lib.rs
@@ -0,0 +1,31 @@
+//! This crate implements the virtual memory subsystem for Theseus,
+//! which is fairly robust and provides a unification between 
+//! arbitrarily mapped sections of memory and Rust's lifetime system. 
+//! Originally based on Phil Opp's blog_os. 
+
+#![no_std]
+#![feature(asm)]
+#![feature(ptr_internals)]
+#![feature(core_intrinsics)]
+#![feature(unboxed_closures)]
+#![feature(step_trait, range_is_empty)]
+
+extern crate memory_x86;
+extern crate memory;
+extern crate alloc;
+extern crate multiboot2;
+extern crate irq_safety;
+
+pub use memory_x86::{arch_init};
+pub use memory::*;
+
+use alloc::vec::Vec;
+use multiboot2::BootInformation;
+use irq_safety::MutexIrqSafe;
+use alloc::sync::Arc;
+
+pub fn init(boot_info: &BootInformation) 
+    -> Result<(Arc<MutexIrqSafe<MemoryManagementInfo>>, MappedPages, MappedPages, MappedPages, Vec<MappedPages>), &'static str> 
+{
+    arch_init(boot_info)
+}
