a450ed059c6d688e8704a87d1052743a0a9f04b8
Ramla-I <ijazramla@gmail.com>
2019-10-17 18:10:13 -0500

    updated error handling for libc::mman

--- applications/test_libc/src/lib.rs
+++ applications/test_libc/src/lib.rs
@@ -11,20 +11,38 @@ extern crate libc;
 use alloc::vec::Vec;
 use alloc::string::String;
 use libc::*;
+use libc::types::*;
+
 
 #[no_mangle]
 pub fn main(_args: Vec<String>) -> isize {
     println!("testing libc!!!");
+    let size = 64;
 
+    // malloc test
     unsafe {
-        let a = mman::malloc(64) as *mut u8; 
+        let a = mman::malloc(size) as *mut u8; 
+        *a = 0;
+        *a.offset(size as isize - 1) = size as u8;
 
-        *a = 0xF;
-        *a.offset(1) = 0xE;
-        *a.offset(2) = 0xD;
+        mman::free(a as *mut c_void);
+        // once added back to the heap, the size of this area is stored in the first byte
+        assert_eq!(*a, size as u8);
+    }
 
+    //mmap test 
+    unsafe {
+        let ptr = mman::mmap(0 as *mut c_void, 4096, 0, mman::MAP_ANON, 0, 0);
+        println!("mapped pages allocated start at: {:#X}", ptr as usize);
+        mman::munmap(ptr, 4096);
+        // mman::munmap(ptr, 4096);
+    }
 
-        println!("a = {}, {}, {}", *a, *a.offset(1), *a.offset(2));
+    //file test
+    unsafe {
+        let name = c_str::CStr::from_bytes_with_nul(b"hello\0").unwrap(); 
+        let fd = fs::open(name, fcntl::O_CREAT ,0);
+        // let a = fs::close(fd);
     }
      
     0
--- kernel/libc/src/mman.rs
+++ kernel/libc/src/mman.rs
@@ -22,56 +22,66 @@ pub const MAP_FIXED: c_int = 0x0010;
 pub const MAP_ANON: c_int = 0x0020;
 pub const MAP_ANONYMOUS: c_int = MAP_ANON;
 
+pub const NULL: *mut c_void = 0 as *mut c_void;
+
 lazy_static! {
     pub static ref MALLOC_LAYOUTS: Mutex<HashMap<VirtualAddress, Layout>> = Mutex::new(HashMap::new());
     pub static ref MMAP_MAPPINGS: Mutex<HashMap<VirtualAddress, MappedPages>> = Mutex::new(HashMap::new());
 }
 
-/// Creates a layout for the memory required and returns a pointer from the heap 
-/// void *malloc(size_t size);
+/// Returns a pointer to a portion of heap memory of "size" bytes.
 #[no_mangle]
 pub unsafe extern "C" fn malloc (size: size_t) -> *mut c_void{
-    debug!("In malloc");
-    // we set the alignment to 1 byte, need to check malloc's alignment requirements
+    // TODO: we set the alignment to 1 byte, need to check malloc's alignment requirements
     const alignment: usize = 1;
-    let layout = Layout::from_size_align(size, alignment).unwrap();
+    let layout =  match Layout::from_size_align(size, alignment) {
+        Ok(x) => x,
+        Err(x) => return NULL,
+    };
+
     let mem = unsafe{ alloc(layout) };
 
-    // remove below,  just for testing
-    unsafe {
-        *mem = 9;
-        *mem.offset(1) = 15;
-    }
-
-    let addr = VirtualAddress::new(mem as usize).unwrap();
-    MALLOC_LAYOUTS.lock().insert(addr, layout);
+    match VirtualAddress::new(mem as usize) {
+        Ok(x) => MALLOC_LAYOUTS.lock().insert(x, layout),
+        Err(x) => return NULL,
+    };    
 
     return mem as *mut c_void;
 }
 
+/// Deallocates the memory pointed to by "ptr"
 #[no_mangle]
 pub unsafe extern "C" fn free(ptr: *mut c_void) {
-    let addr = VirtualAddress::new(ptr as usize).unwrap();
-    let layout = MALLOC_LAYOUTS.lock().remove(&addr).unwrap();
-    dealloc(ptr as *mut u8, layout); 
+    let layout = match VirtualAddress::new(ptr as usize) {
+        Ok(x) =>  MALLOC_LAYOUTS.lock().remove(&x),
+        Err(x) => {
+            error!("libc::mman::free() could not convert ptr to virtual address: {:?}",x);    
+            return;
+        }
+    };
+    match layout {
+        Some(x) => dealloc(ptr as *mut u8, x),
+        None => {
+            error!("libc::mman::free() layout was not found");                
+            return;
+        }
+    }; 
 }
 
-/// void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
 /// Creates a new mapping in the virtual address space.
 /// 
 /// # Arguments
 /// * `addr`: Starting address of the new mapping. If it's NULL then the kernel decides the address of the new mapping.
 /// * `length`: length of the mapping which must be greater than zero.
 /// * `prot`: describes the desired memory protection of the mapping.
-/// * `flags`: determines whether updates to the mapping are visible to other processes mapping the same region,
-/// and whether updates are carried through to the underlying file
+/// * `flags`: determines whether updates to the mapping are visible to other processes mapping the same region, and whether updates are carried through to the underlying file
 /// * `fd`: file descriptor,
-/// * `offset`: offset in the file to write to. 
+/// * `offset`: offset in the file where the mapping will start. It must be a multiple of the page size. 
 /// 
-/// Right now we only consider the case of anonymous mappings without a given address.
+/// Right now this function only implements anonymous mappings without a given address, and ignores all protection flags.
 #[no_mangle]
 pub extern fn mmap(_addr: *mut c_void, length: size_t, prot: c_int, flags: c_int, fd: c_int, offset: off_t) -> *mut c_void {
-    //anonymous mapping
+    // Most systems support MAP_ANON and MAP_FIXED
     if flags & MAP_ANON == MAP_ANON {
         // allocate the number of pages
         let kernel_mmi_ref = memory::get_kernel_mmi_ref().unwrap();
@@ -79,25 +89,49 @@ pub extern fn mmap(_addr: *mut c_void, length: size_t, prot: c_int, flags: c_int
         let pages = memory::allocate_pages_by_bytes(length as usize).unwrap();
         let mapped_pages = kernel_mmi_ref.lock().page_table.map_allocated_pages(pages, Default::default() , allocator.lock().deref_mut()).unwrap();
 
+        let mapped_pages = match memory::get_kernel_mmi_ref() {
+            Some(kernel_mmi_ref) => {
+                match FRAME_ALLOCATOR.try() {
+                    Some(allocator) => {
+                        match memory::allocate_pages_by_bytes(length as usize) {
+                            Some(pages) => {
+                                kernel_mmi_ref.lock().page_table.map_allocated_pages(pages, Default::default() , allocator.lock().deref_mut())
+                            },
+                            None => return NULL,
+                        } 
+                    },
+                    None => return NULL,
+                }
+            },
+            None => return NULL,
+        };
         // zero the memory?
-
-        let addr = mapped_pages.start_address();
-        MMAP_MAPPINGS.lock().insert(addr, mapped_pages);
-
-        return addr.value() as *mut c_void;
+        match mapped_pages {
+            Ok(mp) => {
+                let addr = mp.start_address();
+                MMAP_MAPPINGS.lock().insert(addr, mp);
+                return addr.value() as *mut c_void;
+            }
+            Err(x) => {
+                error!("libc::mman::mmap(): could not get mapped pages: {:?}", x);
+                return NULL;
+            },
+        }
+        
     }
 
-    // Most systems support MAP_ANON and MAP_FIXED
     else {
-        error!("Unimplemented memory mapping!");
-        return 0 as *mut c_void;
+        error!("libc::mman::mmap(): unimplemented flag!");
+        return NULL;
     }
 
 }
 
+/// Unmaps a mapping starting at "addr" of "len" bytes
 #[no_mangle]
 pub extern fn munmap(addr: *mut c_void, len: size_t) {
-    let addr = VirtualAddress::new(addr as usize).unwrap();
-    let mapped_pages = MMAP_MAPPINGS.lock().remove(&addr).unwrap();
-    // mapped_pages.drop();
+    match VirtualAddress::new(addr as usize) {
+        Ok(x) => {MMAP_MAPPINGS.lock().remove(&x);},
+        Err(x) => error!("libc::mman::munmap(): Couldn't retrieve mapping: {:?}", x),
+    };
 }
