52cd2b7b7705192829538697191129ede349627d
snoword <qiuj@mail.bnu.edu.cn>
2019-12-07 04:46:36 -0600

    remove unwrap

--- kernel/frame_buffer_rgb/src/lib.rs
+++ kernel/frame_buffer_rgb/src/lib.rs
@@ -41,7 +41,7 @@ pub fn init() -> Result<(), &'static str> {
         FrameBufferRGB::new(buffer_width, buffer_height, Some(vesa_display_phys_start))?;
     let background = vec![0; buffer_width * buffer_height];
     FINAL_FRAME_BUFFER.call_once(|| Mutex::new(Box::new(framebuffer)));
-    FINAL_FRAME_BUFFER.try().ok_or("").unwrap().lock().composite_buffer(background.as_slice(), 0);
+    FINAL_FRAME_BUFFER.try().ok_or("")?.lock().composite_buffer(background.as_slice(), 0);
 
     Ok(())
 }
