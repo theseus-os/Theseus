5a13f6681f2bb04f1c427dc8cf49f7f96d94b188
Kevin Boos <kevinaboos@gmail.com>
2020-02-04 15:59:48 -0800

    undo irrelevant changes

--- applications/.old_static_binary_hello/src/main.rs
+++ applications/.old_static_binary_hello/src/main.rs
@@ -32,6 +32,7 @@ pub extern "C" fn panic_fmt(fmt: core::fmt::Arguments, file: &'static str, line:
     error!("\n\nPANIC in {} at line {}:{}:", file, line, column);
     error!("    {}", fmt);
 
+    // TODO: check out Redox's unwind implementation: https://github.com/redox-os/kernel/blob/b364d052f20f1aa8bf4c756a0a1ea9caa6a8f381/src/arch/x86_64/interrupt/trace.rs#L9
     loop {}
 }
 
