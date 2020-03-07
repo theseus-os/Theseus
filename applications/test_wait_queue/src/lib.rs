376943c5b08a6c685f3dac37a9bc8c8714b882c0
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2020-03-06 20:49:37 -0800

    Redesign spawn API, simplified into a single `TaskBuilder`. Closes #76. (#250)
    The two new relevant functions are `spawn::new_task_builder()` and `spawn::new_application_task_builder()`.

--- applications/test_wait_queue/src/lib.rs
+++ applications/test_wait_queue/src/lib.rs
@@ -17,7 +17,6 @@ use alloc::{
     sync::Arc,
 };
 use spin::Mutex;
-use spawn::KernelTaskBuilder;
 use wait_condition::{WaitCondition, WaitConditionFn};
 
 
@@ -42,18 +41,18 @@ fn rmain() -> Result<(), &'static str> {
     let wc2 = wc.clone();
 
 
-    let t1 = KernelTaskBuilder::new(wait_task, (wc, ready3))
+    let t1 = spawn::new_task_builder(wait_task, (wc, ready3))
         .name(String::from("wait_task"))
         .pin_on_core(my_cpu)
         .spawn()?;
 
-    let t2 = KernelTaskBuilder::new(notify_task, (wc2, ready2))
+    let t2 = spawn::new_task_builder(notify_task, (wc2, ready2))
         .name(String::from("notify_task"))
         .pin_on_core(my_cpu)
         .block()
         .spawn()?;
         
-    let t3 = KernelTaskBuilder::new(
+    let t3 = spawn::new_task_builder(
         |tref| {
             warn!("DeezNutz:  testing spurious wakeup on task {:?}", tref);
             tref.unblock();
