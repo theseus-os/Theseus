67f2002c7aa8198a5f1d64aaced31f14717cfe2f
Kevin Boos <1139460+kevinaboos@users.noreply.github.com>
2020-04-15 12:35:29 -0700

    Update to log v0.4.8, which avoids unsafe code and allows dynamically setting the log level (#271)

--- kernel/logger/src/lib.rs
+++ kernel/logger/src/lib.rs
@@ -4,11 +4,16 @@ extern crate serial_port;
 extern crate log;
 extern crate spin;
 
-use log::{LogRecord, LogLevel, SetLoggerError, LogMetadata, Log, ShutdownLoggerError};
+use log::{Record, Level, SetLoggerError, Metadata, Log};
 use core::fmt;
 use spin::Once;
 
-static LOG_LEVEL: LogLevel = LogLevel::Trace;
+
+/// The static logger instance, an empty struct that implements the `Log` trait.
+static LOGGER: Logger = Logger { };
+
+/// By default, Theseus will log 
+const DEFAULT_LOG_LEVEL: Level = Level::Trace;
 
 pub type LogOutputFunc = fn(fmt::Arguments);
 static MIRROR_VGA_FUNC: Once<LogOutputFunc> = Once::new();
@@ -53,28 +58,30 @@ pub fn mirror_to_vga(func: LogOutputFunc) {
 struct Logger { }
 
 impl Log for Logger {
-    fn enabled(&self, metadata: &LogMetadata) -> bool {
-        metadata.level() <= LOG_LEVEL
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= log::max_level()
     }
 
-    fn log(&self, record: &LogRecord) {
+    fn log(&self, record: &Record) {
         if !self.enabled(record.metadata()) {
             return;
         }
 
         let (level_str, color) = match record.level() {
-            LogLevel::Error => ("[E] ", LogColor::Red),
-            LogLevel::Warn =>  ("[W] ", LogColor::Yellow),
-            LogLevel::Info =>  ("[I] ", LogColor::Cyan),
-            LogLevel::Debug => ("[D] ", LogColor::Green),
-            LogLevel::Trace => ("[T] ", LogColor::Purple),
+            Level::Error => ("[E] ", LogColor::Red),
+            Level::Warn =>  ("[W] ", LogColor::Yellow),
+            Level::Info =>  ("[I] ", LogColor::Cyan),
+            Level::Debug => ("[D] ", LogColor::Green),
+            Level::Trace => ("[T] ", LogColor::Purple),
         };
-        let location = record.location();
+
+        let file_loc = record.file().unwrap_or("??");
+        let line_loc = record.line().unwrap_or(0);
         let _result = serial_port::write_fmt(format_args!("{}{}{}:{}: {}{}",
             color.as_terminal_string(),
             level_str,
-            location.file(),
-            location.line(),
+            file_loc,
+            line_loc,
             record.args(),
             LogColor::Reset.as_terminal_string(),
         ));
@@ -86,36 +93,32 @@ impl Log for Logger {
             // so we exclude the first and the last elements that set those colors.
             func(format_args!("{}{}:{}: {}",
                 level_str,
-                location.file(),
-                location.line(),
+                file_loc,
+                line_loc,
                 record.args(),
             ));
         }
     }
-}
 
-
-impl Logger {
     fn flush(&self) {
         // flushing the log is a no-op, since there is no write buffering yet
     }
 }
 
 
-
+/// Initialize the Theseus system logger, which writes log messages to the serial port. 
 pub fn init() -> Result<(), SetLoggerError> {
-    unsafe {
-        log::set_logger_raw(|max_log_level| {
-            static LOGGER: Logger = Logger { };
-            max_log_level.set(LOG_LEVEL.to_log_level_filter());
-            &LOGGER
-        })
-    }
+    log::set_logger(&LOGGER)?;
+    set_log_level(DEFAULT_LOG_LEVEL);
+    Ok(())
 }
 
-pub fn shutdown() -> Result<(), ShutdownLoggerError> {
-    log::shutdown_logger_raw().map(|logger| {
-        let logger = unsafe { &*(logger as *const Logger) };
-        logger.flush();
-    })
+/// Set the log level, which determines whether a given log message is actually logged. 
+/// 
+/// For example, if `Level::Trace` is set, all log levels will be logged.
+/// 
+/// If `Level::Info` is set, `debug!()` and `trace!()` will not be logged, 
+/// but `info!()`, `warn!()`, and `error!()` will be. 
+pub fn set_log_level(level: Level) {
+    log::set_max_level(level.to_level_filter())
 }
