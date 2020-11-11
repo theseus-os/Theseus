8d81907ba344f77645ffa0f3ccada5ea86ced1f5
Kevin Boos <kevinaboos@gmail.com>
2020-11-11 12:07:31 -0800

    new copy_latest_crate_objects tool is complete and works correctly, and is integrated into the Makefile

--- tools/copy_latest_crate_objects/src/main.rs
+++ tools/copy_latest_crate_objects/src/main.rs
@@ -36,15 +36,15 @@ use std::{
     env,
     fs::{self, DirEntry, File},
     io::{self, BufRead},
-    path::Path,
+    path::{Path, PathBuf},
 };
 use walkdir::WalkDir;
 
 
 /// Debug option: if true, print all crate names and their object file path. 
-const PRINT_CRATES: bool = true;
+const PRINT_CRATES: bool = false;
 /// Debug option: if both this and `PRINT_CRATES` are true, print sorted crate names. 
-const PRINT_SORTED: bool = true;
+const PRINT_SORTED: bool = false;
 
 
 /// The delimiter that goes at the end of object file prefixes,
@@ -60,57 +60,83 @@ fn main() -> Result<(), String> {
     opts.reqopt(
         "i", 
         "input",  
-        "path to the input directory of compiled crate object files, 
+        "(required) path to the input directory of compiled crate object files, 
          typically the `target`, e.g., \"/path/to/Theseus/target\"", 
-        "TARGET_DIR"
+        "INPUT_DIR"
     );
     opts.reqopt(
         "o", 
         "output",  
-        "path to the output directory where crate object files should be copied to, 
+        "(required) path to the output directory where crate object files should be copied to, 
          typically the OS image directory, e.g., \"/path/to/build/grub-isofiles/modules/\"", 
         "OUTPUT_DIR"
     );
     opts.reqopt(
         "k", 
         "kernel",  
-        "path to either the directory of kernel crates, or a file listing each kernel crate name, one per line",
+        "(required) path to either the directory of kernel crates,
+         or a file listing each kernel crate name, one per line",
         "KERNEL_CRATES"
     );
     opts.reqopt(
         "a", 
         "app",  
-        "path to either the directory of application crates, or a file listing each application crate name, one per line",
+        "(required) path to either the directory of application crates,
+         or a file listing each application crate name, one per line",
         "APP_CRATES"
     );
     opts.optopt(
         "",
-        "--kernel-prefix",
+        "kernel-prefix",
         "the prefix prepended to kernel crate object files when they're copied to the output directory (default: 'k#')",
         "PREFIX"
     );
     opts.optopt(
         "",
-        "--app-prefix",
+        "app-prefix",
         "the prefix prepended to application crate object files when they're copied to the output directory (default 'a#')",
         "PREFIX"
     );
+    opts.optmulti(
+        "e",
+        "extra-app",
+        "additional names of crates that should be treated as application crates. Can be provided multiple times",
+        "APP_CRATE_NAME"
+    );
+    opts.optmulti(
+        "c",
+        "core-files",
+        "paths to additional core files that should be treated as kernel crates, such. Can be provided multiple times",
+        "APP_CRATE_NAME"
+    );
     opts.optflag("h", "help", "print this help menu");
 
-    let matches = opts.parse(&args[1..]).map_err(|e| e.to_string())?;
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(e) => {
+            print_usage(opts);
+            return Err(e.to_string());
+        }
+    };
 
     if matches.opt_present("h") {
-        print_usage("cargo run -- ", opts);
+        print_usage(opts);
         return Ok(());
     }
 
-    let mut kernel_prefix = matches.opt_str("--kernel-prefix").unwrap_or("k".to_string());
+    let mut kernel_prefix = matches.opt_str("kernel-prefix").unwrap_or("k".to_string());
     if !kernel_prefix.ends_with(MODULE_PREFIX_DELIMITER) {
         kernel_prefix.push_str(MODULE_PREFIX_DELIMITER);
     }
-    let mut app_prefix = matches.opt_str("--app-prefix").unwrap_or("a".to_string());
+    if kernel_prefix.matches(MODULE_PREFIX_DELIMITER).count() > 1 {
+        return Err(format!("kernel-prefix {:?} must only contain one '#' character at the end!", kernel_prefix));
+    }
+    let mut app_prefix = matches.opt_str("app-prefix").unwrap_or("a".to_string());
     if !app_prefix.ends_with(MODULE_PREFIX_DELIMITER) {
         app_prefix.push_str(MODULE_PREFIX_DELIMITER);
+    }
+    if app_prefix.matches(MODULE_PREFIX_DELIMITER).count() > 1 {
+        return Err(format!("app-prefix {:?} must only contain one '#' character at the end!", app_prefix));
     }
 
     let kernel_arg = matches.opt_str("k").expect("no -k or --kernel arg provided");
@@ -132,7 +158,7 @@ fn main() -> Result<(), String> {
 
     let app_arg_path = fs::canonicalize(app_arg)
         .map_err(|e| format!("app arg was invalid path, error: {:?}", e))?;
-    let app_crates_set = if app_arg_path.is_file() {
+    let mut app_crates_set = if app_arg_path.is_file() {
         populate_crates_from_file(app_arg_path)
             .map_err(|e| format!("Error parsing app arg as file: {:?}", e))?
     } else if app_arg_path.is_dir() {
@@ -142,36 +168,51 @@ fn main() -> Result<(), String> {
         return Err(format!("Couldn't access -k/--app argument {:?} as a file or directory", app_arg_path));
     };
 
-    let (kernel_object_files, app_object_files, other_object_files) = parse_input_dir(
-        kernel_crates_set,
+    let extra_app_names = matches.opt_strs("e");
+    app_crates_set.extend(extra_app_names.iter().flat_map(|n| n.split_whitespace()).map(|s| s.to_string()));
+
+    let extra_core_files = matches.opt_strs("c");
+    let extra_core_file_paths = extra_core_files.iter()
+        .flat_map(|n| n.split_whitespace())
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let (app_object_files, kernel_object_files, other_object_files) = parse_input_dir(
         app_crates_set,
+        kernel_crates_set,
         input_dir,
     ).unwrap();
 
     // Now that we have obtained the lists of kernel, app, and other crates, 
     // we copy them into the output directory with the proper prefix. 
-    copy_files(&output_dir, kernel_object_files, &kernel_prefix).unwrap();
-    copy_files(&output_dir, app_object_files,    &app_prefix   ).unwrap();
-    copy_files(&output_dir, other_object_files,  &kernel_prefix).unwrap(); 
-    
-
-    
-    // // Write to output file (if provided) 
-    // if matches.opt_present("o") {
-    //     let output_file_path = matches.opt_str("o")
-    //         .ok_or_else(|| String::from("failed to match output file argument."))?;
-    //     write_content(grub_cfg_string, output_file_path);
-    // }
-    // // Otherwise, write to stdout by default
-    // else {
-    //     println!("{}", grub_cfg_string);
-    // }
+    // Also, we ensure that the specified output directory exists.
+    fs::create_dir_all(&output_dir).map_err(|e| format!("Error creating output directory {:?}, {:?}", output_dir, e))?;
+    copy_files(
+        &output_dir,
+        app_object_files.values().map(|d| d.path()),
+        &app_prefix
+    ).unwrap();
+    copy_files(
+        &output_dir,
+        kernel_object_files.values().map(|d| d.path()),
+        &kernel_prefix
+    ).unwrap();
+    copy_files(
+        &output_dir,
+        other_object_files.values().map(|d| d.path()),
+        &kernel_prefix
+    ).unwrap(); 
+    copy_files(
+        &output_dir,
+        extra_core_file_paths.into_iter().map(|s| PathBuf::from(s)),
+        &kernel_prefix
+    ).unwrap(); 
 
     Ok(())
 }
 
-fn print_usage(program: &str, opts: Options) {
-    let brief = format!("Usage: {} [options]", program);
+fn print_usage(opts: Options) {
+    let brief = format!("Usage: cargo run -- [options]");
     print!("{}", opts.usage(&brief));
 }
 
@@ -236,18 +277,18 @@ fn populate_crates_from_dir<P: AsRef<Path>>(dir_path: P) -> Result<HashSet<Strin
 /// See the top of this file for more details. 
 /// 
 /// Upon success, returns a tuple of:
-/// * kernel crates
 /// * application crates
+/// * kernel crates
 /// * all other crates
 /// 
 fn parse_input_dir(
-    kernel_crates: HashSet<String>,
     app_crates: HashSet<String>,
+    kernel_crates: HashSet<String>,
     input_dir: String,
 ) -> std::io::Result<(HashMap<String, DirEntry>, HashMap<String, DirEntry>, HashMap<String, DirEntry>)> {
 
-    let mut kernel_objects:  HashMap<String, DirEntry> = HashMap::new();
     let mut app_objects:     HashMap<String, DirEntry> = HashMap::new();
+    let mut kernel_objects:  HashMap<String, DirEntry> = HashMap::new();
     let mut other_objects:   HashMap<String, DirEntry> = HashMap::new();
 
     for dir_entry in fs::read_dir(input_dir)? {
@@ -260,9 +301,9 @@ fn parse_input_dir(
         let prefix = file_name.split("-").next().expect("object file name didn't have the crate/hash '-' delimiter");
         let modified_time = metadata.modified()?;
 
-
-        if kernel_crates.contains(prefix) {
-            match kernel_objects.entry(prefix.to_string()) {
+        // Check whether the object file is for a crate designated as an application, kernel, or other crate.
+        if app_crates.contains(prefix) {
+            match app_objects.entry(prefix.to_string()) {
                 Entry::Occupied(mut occupied) => {
                     if occupied.get().metadata()?.modified()? < modified_time {
                         occupied.insert(dir_entry);
@@ -272,8 +313,8 @@ fn parse_input_dir(
                     vacant.insert(dir_entry);
                 }
             }
-        } else if app_crates.contains(prefix) {
-            match app_objects.entry(prefix.to_string()) {
+        } else if kernel_crates.contains(prefix) {
+            match kernel_objects.entry(prefix.to_string()) {
                 Entry::Occupied(mut occupied) => {
                     if occupied.get().metadata()?.modified()? < modified_time {
                         occupied.insert(dir_entry);
@@ -291,33 +332,35 @@ fn parse_input_dir(
 
     // optional debug output
     if PRINT_CRATES {
-        println!("KERNEL OBJECTS:");
-        print_crates(&kernel_objects, PRINT_SORTED);
         println!("APPLICATION OBJECTS:");
         print_crates(&app_objects, PRINT_SORTED);
+        println!("KERNEL OBJECTS:");
+        print_crates(&kernel_objects, PRINT_SORTED);
         println!("OTHER OBJECTS:");
         print_crates(&other_objects, PRINT_SORTED);
     }
     
     Ok((
-        kernel_objects,
         app_objects,
+        kernel_objects,
         other_objects,
     ))
 }
 
 
-fn copy_files<P: AsRef<Path>>(
+/// Copies the source files given by the `values()` in `objects`
+/// to the given `output_dir`. 
+/// Prepends the given `prefix` onto the front of the output file names. 
+fn copy_files<'p, P: AsRef<Path>, I: Iterator<Item = PathBuf>>(
     output_dir: P,
-    objects: HashMap<String, DirEntry>,
+    objects: I,
     prefix: &str
 ) -> io::Result<()> {
-    
-    for (_source_crate, source_file) in objects {
+    for source_path in objects {
         let mut dest_path = output_dir.as_ref().to_path_buf();
-        dest_path.push(format!("{}{}", prefix, source_file.file_name().to_str().unwrap()));
-        println!("Copying {} to {}", source_file.path().display(), dest_path.display());
-        fs::copy(source_file.path(), dest_path)?;
+        dest_path.push(format!("{}{}", prefix, source_path.file_name().and_then(|osstr| osstr.to_str()).unwrap()));
+        // println!("Copying {} to {}", source_path.display(), dest_path.display());
+        fs::copy(source_path, dest_path)?;
     }
 
     Ok(())
