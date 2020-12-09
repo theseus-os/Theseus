7874de42ee5542b133faa080701caeea5ce5713f
Kevin Boos <kevinaboos@gmail.com>
2020-12-09 13:42:15 -0800

    successfully parsed all args passed to rustc from cargo/xargo

--- tools/theseus_xargo/src/main.rs
+++ tools/theseus_xargo/src/main.rs
@@ -24,6 +24,7 @@
 // #![feature(rustc_private)] 
 
 extern crate getopts;
+extern crate clap;
 extern crate walkdir;
 extern crate regex;
 extern crate itertools;
@@ -48,8 +49,8 @@ use walkdir::WalkDir;
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     let env_vars: HashMap<String, String> = env::vars().collect();
-    println!("----------- Command-line Arguments ----------\n{:#?}", args);
-    println!("----------- Environment Variables -----------\n{:#?}", env_vars);
+    // println!("----------- Command-line Arguments ----------\n{:#?}", args);
+    // println!("----------- Environment Variables -----------\n{:#?}", env_vars);
 
     let mut opts = Options::new();
     opts.parsing_style(getopts::ParsingStyle::StopAtFirstFree);
@@ -97,17 +98,19 @@ fn main() -> Result<(), String> {
     println!("VERBOSE_LEVEL: {:?}", verbose_count);
 
     let stderr_captured = run_initial_xargo(env_vars, cargo_cmd_string.clone(), verbose_count)?;
-    println!("\n\n------------------- STDERR --------------------- \n{}", stderr_captured.join("\n\n"));
+    // println!("\n\n------------------- STDERR --------------------- \n{}", stderr_captured.join("\n\n"));
 
     if cargo_cmd_string.split_whitespace().next() != Some("build") {
         println!("Exiting after completing non-'build' xargo command.");
         return Ok(());
     }
 
-    let mut prebuilts_sorted: Vec<String> = prebuilt_crates_set.into_iter().collect();
-    prebuilts_sorted.sort();
-    println!("\n\n------------------- PREBUILT CRATES --------------------- \n{}", prebuilts_sorted.join("\n"));
-    
+
+    // re-execute the rustc commands that we captured from the original xargo/cargo verbose output. 
+    for original_cmd in &stderr_captured {
+        run_rustc_command(original_cmd, &prebuilt_crates_set, input_dir_path.as_path())?;
+    }
+
 
     Ok(())
 }
@@ -131,12 +134,19 @@ fn print_usage(opts: Options) {
     print!("{}", opts.usage(&brief));
 }
 
+// The commands we care about capturing starting with "Running `" and end with "`".
+const COMMAND_START: &str = "Running `";
+const COMMAND_END:   &str = "`";
+const RUSTC_START:   &str = "rustc --crate-name";
+const BUILD_SCRIPT:  &str = "build-script-build";
+
+
 /// Runs the actual xargo command, e.g., xargo build, 
 /// with all of the arguments specified on the command line. 
 ///
 /// Returns the captured content of content written to `stderr` by the xargo command,
 /// as a list of lines.
-fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verbose_count: usize) -> Result<Vec<String>, String> {
+fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verbose_level: usize) -> Result<Vec<String>, String> {
     println!("FULL ARGS: {}", full_args);
 
     let mut cmd = Command::new("xargo");
@@ -160,7 +170,7 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
             .filter_map(|line| line.ok())
             .for_each(|line| {
                 // Cargo only prints to stdout for build script output, only if very verbose.
-                if verbose_count >= 2 {
+                if verbose_level >= 2 {
                     println!("{}", line);
                 }
                 stdout_logs.push(line);
@@ -178,6 +188,11 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
     let mut pending_multiline_cmd = false;
     let mut original_multiline = String::new();
 
+    // Capture every line that cargo writes to stderr. 
+    // We only re-echo the lines that should be outputted by the verbose level specified.
+    // The complexity below is due to the fact that a verbose command printed by cargo
+    // may span multiple lines, so we need to detect the beginning and end of a multi-line command
+    // and merge it into a single line in our captured output. 
     stderr_reader.lines()
         .filter_map(|line| line.ok())
         .for_each(|original_line| {
@@ -188,7 +203,7 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
                 (line_stripped.contains("--crate-name") && line_stripped.contains("--crate-type"))
                 || line_stripped.ends_with("build-script-build`");
 
-            if line_stripped.starts_with("Running `") {
+            if line_stripped.starts_with(COMMAND_START) {
                 // Here, we've reached the beginning of a rustc command, which we actually do care about. 
                 stderr_logs.push(line_stripped.to_string());
                 pending_multiline_cmd = !is_final_line;
@@ -211,7 +226,7 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
                         return; // continue to the next line
                     }
                 } else {
-                    // do nothing: this is an unrelated line of output that we don't care about.
+                    // Here: this is an unrelated line of output that isn't a command we want to capture.
                     original_multiline.clear(); // = String::from(&original_line);
                 }
             }
@@ -220,12 +235,12 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
             // But if the user didn't ask for that, then we shouldn't print that verbose output here. 
             // Verbose output lines start with "Running `", "+ ", or "[".
             let should_print = |stripped_line: &str| {
-                verbose_count > 0 ||  // print everything if verbose
+                verbose_level > 0 ||  // print everything if verbose
                 (
                     // print only "Compiling" and warning/error lines if not verbose
                     !stripped_line.starts_with("+ ")
                     && !stripped_line.starts_with("[")
-                    && !stripped_line.starts_with("Running `")
+                    && !stripped_line.starts_with(COMMAND_START)
                 )
             };
             if !original_multiline.is_empty() && is_final_line {
@@ -250,6 +265,177 @@ fn run_initial_xargo(_env_vars: HashMap<String, String>, full_args: String, verb
     }
 
     Ok(stderr_logs)
+}
+
+
+/// Takes the given `original_cmd` that was captured from the verbose output of cargo/xargo,
+/// and parses/modifies it to link against (depend on) the corresponding crate of the same name
+/// from the list of prebuilt crates. 
+///
+/// The actual dependency files (.rmeta/.rlib) for the prebuilt crates should be located in the `prebuilt_dir`. 
+///
+/// Returns an error if the command fails. 
+fn run_rustc_command(
+    original_cmd: &str,
+    prebuilt_crates: &HashSet<String>,
+    prebuilt_dir: &Path
+) -> Result<(), String> {
+    let command = if original_cmd.starts_with(COMMAND_START) && original_cmd.ends_with(COMMAND_END) {
+        let end_index = original_cmd.len() - COMMAND_END.len();
+        &original_cmd[COMMAND_START.len() .. end_index]
+    } else {
+        return Err(format!("Unexpected formatting in capture command (must start with {:?} and end with {:?}. Command: {:?}", 
+            original_cmd, COMMAND_START, COMMAND_END,
+        ));
+    };
+
+    // Skip build script invocations, as I don't think we need to re-run those. 
+    if command.ends_with(BUILD_SCRIPT) {
+        return Ok(());
+    }
+    
+    println!("\n\nLooking at original command:\n{}", command);
+    let start_of_rustc_cmd = command.find(RUSTC_START).ok_or_else(|| 
+        format!("Couldn't find {:?} in command:\n{:?}", RUSTC_START, command)
+    )?;
+    let command_without_env = &command[start_of_rustc_cmd ..];
+
+    // The arguments in the command that we care about are:
+    //  *  "-L dependency=<dir>"
+    //  *  "--extern <crate_name>=<crate_file>.rmeta"
+    //
+    // Below, we use `clap` to find those argumnets and replace them. 
+    //
+    // First, we parse the following part:
+    // "rustc --crate-name <crate_name> <crate_source_file> <all_other_args>"
+    let top_level_matches = clap::App::new("rustc")
+        // The first argument that we want to see, --crate-name.
+        .arg(clap::Arg::with_name("crate-name")
+            .long("crate-name")
+            .takes_value(true)
+            .required(true)
+        )
+        .setting(clap::AppSettings::DisableHelpFlags)
+        .setting(clap::AppSettings::DisableHelpFlags)
+        .setting(clap::AppSettings::AllowExternalSubcommands)
+        .setting(clap::AppSettings::ColorNever)
+        .get_matches_from_safe(command_without_env.split_whitespace());
+    
+    let top_level_matches = top_level_matches.map_err(|e| 
+        format!("Missing support for argument found in captured rustc command: {}", e)
+    )?;
+    // println!("\nTop-level Matches: {:#?}", top_level_matches);
+
+    // Clap will parse the args as such:
+    // * the --crate-name will be the first argument
+    // * the path to the crate's main file will be the first subcommand
+    // * that subcommand's arguments will include ALL OTHER arguments that we care about, specified below.
+
+    let crate_name = top_level_matches.value_of("crate-name").unwrap();
+    let (crate_source_file, additional_args) = top_level_matches.subcommand();
+    let additional_args = additional_args.unwrap();
+
+    println!("\nGot match info:\ncrate-name: {:?}\ncrate_source_file: {:?}\nadditional_args: {:#?}", crate_name, crate_source_file, additional_args);
+    let args_after_source_file = additional_args.values_of("").unwrap();
+
+    // Second, we parse all other args in the command that followed the crate source file. 
+    let matches = clap::App::new("")
+        // The -L linker arg we care about
+        .arg(clap::Arg::with_name("L")
+            .short("L")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // The --extern crate arg we care about
+        .arg(clap::Arg::with_name("extern")
+            .long("extern")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about -C arguments
+        .arg(clap::Arg::with_name("C")
+            .short("C")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about -D arguments
+        .arg(clap::Arg::with_name("DZ")
+            .short("D")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about -Z arguments
+        .arg(clap::Arg::with_name("")
+            .short("Z")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about the --crate-type argument
+        .arg(clap::Arg::with_name("crate-type")
+            .long("crate-type")
+            .takes_value(true)
+        )
+        // We don't care about the --emit arguments
+        .arg(clap::Arg::with_name("emit")
+            .long("emit")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about the --out-dir argument
+        .arg(clap::Arg::with_name("out-dir")
+            .long("out-dir")
+            .takes_value(true)
+        )
+        // We don't care about the --error-format argument
+        .arg(clap::Arg::with_name("error-format")
+            .long("error-format")
+            .takes_value(true)
+        )
+        // We don't care about the --json argument
+        .arg(clap::Arg::with_name("json")
+            .long("json")
+            .takes_value(true)
+        )
+        // We don't care about the --target argument
+        .arg(clap::Arg::with_name("target")
+            .long("target")
+            .takes_value(true)
+        )
+        // We don't care about the --sysroot argument
+        .arg(clap::Arg::with_name("sysroot")
+            .long("sysroot")
+            .takes_value(true)
+        )
+        // We don't care about the --cfg arguments
+        .arg(clap::Arg::with_name("cfg")
+            .long("cfg")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // We don't care about the --cap-lints arguments
+        .arg(clap::Arg::with_name("cap-lints")
+            .long("cap-lints")
+            .takes_value(true)
+            .multiple(true)
+        )
+        // Note: add any other arguments that you encounter in a rustc invocation here.
+        .setting(clap::AppSettings::DisableHelpFlags)
+        .setting(clap::AppSettings::DisableHelpFlags)
+        .setting(clap::AppSettings::AllowExternalSubcommands)
+        .setting(clap::AppSettings::ColorNever)
+        .get_matches_from_safe(args_after_source_file);
+    
+    let matches = matches.map_err(|e| 
+        format!("Missing support for argument found in captured rustc command: {}", e)
+    )?;
+
+    println!("\n\nMatches: {:#?}", matches);
+
+
+
+    let env = &command[.. start_of_rustc_cmd];
+
+    Ok(())
 }
 
 
