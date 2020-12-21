4a124f4aedd5449e067b677c88ac3b71bce7b123
Ramla-I <ijazramla@gmail.com>
2020-12-21 12:50:39 -0600

    Add ixgbe driver to support the Intel 82599 NIC, and support language-level NIC virtualization (#332)
    This commit also generally refactors networking driver code to deduplicate it and condense shared functionality. Below is a description of changes to certain crates.
    
    * `device_manager` - added detection of the 82599 NIC
    
    * `nic_initialization` - removed register types and replaced with a register struct
    
    * `nic_queues` - Added QueueRegister traits so that queues can store their own registers, and we can create these queue objects for all drivers. Refactored common code between the NIC drivers into send and receive functions for the queue.
    
    * `intel_ethernet` - added advanced transmit descriptor and removed register types since we now have a register trait.
    
    * `e1000` - moved register struct definitions to a separate file. Registers are divided into four sets so that we can separate the Rx and Tx registers.
    
    * `physcial_nic` - a NIC must implement this trait if it wants to support virtualization. This trait defines functions that return Rx and Tx queues to the original NIC when dropped.
    
    * `virtual_nic` - a struct which contains a set of queues, and implements the `NetworkInterfaceCard` trait. Its drop handler returns the queues to the physical NIC.

--- kernel/nic_initialization/src/lib.rs
+++ kernel/nic_initialization/src/lib.rs
@@ -13,6 +13,7 @@ extern crate owning_ref;
 extern crate intel_ethernet;
 extern crate nic_buffers;
 extern crate volatile;
+extern crate nic_queues;
 
 use core::ops::DerefMut;
 use memory::{get_frame_allocator_ref, EntryFlags, PhysicalMemoryArea, FrameRange, PhysicalAddress, allocate_pages_by_bytes, get_kernel_mmi_ref, MappedPages, create_contiguous_mapping};
@@ -22,13 +23,9 @@ use alloc::{
     boxed::Box,
 };
 use owning_ref::BoxRefMut;
-use intel_ethernet::{
-    descriptors::{RxDescriptor, TxDescriptor},
-    types::*
-};
+use intel_ethernet::descriptors::{RxDescriptor, TxDescriptor};
 use nic_buffers::ReceiveBuffer;
-use volatile::Volatile;
-
+use nic_queues::{RxQueueRegisters, TxQueueRegisters};
 
 /// The mapping flags used for pages that the NIC will map.
 pub const NIC_MAPPING_FLAGS: EntryFlags = EntryFlags::from_bits_truncate(
@@ -104,16 +101,11 @@ pub fn init_rx_buf_pool(num_rx_buffers: usize, buffer_size: u16, rx_buffer_pool:
 /// # Arguments
 /// * `num_desc`: number of descriptors in the queue
 /// * `rx_buffer_pool`: pool from which to take receive buffers
-/// * `buffer_size`: size of each buffer in the pool
-/// * `rdbal`: register to store the lower (least significant) 32 bits of the physical address of the array of receive descriptors
-/// * `rdbah`: register to store the higher (most significant) 32 bits of the physical address of the array of receive descriptors
-/// * `rdlen`: register to store the length in bytes of the array of receive descriptors
-/// * `rdh`: register to store the receive descriptor head index
-/// * `rdt`: register to store the receive descriptor tail index
-pub fn init_rx_queue<T: RxDescriptor>(num_desc: usize, rx_buffer_pool: &'static mpmc::Queue<ReceiveBuffer>, buffer_size: usize, 
-            rdbal: &mut Volatile<Rdbal>, rdbah: &mut Volatile<Rdbah>, rdlen: &mut Volatile<Rdlen>, rdh: &mut Volatile<Rdh>, rdt: &mut Volatile<Rdt>)
-                -> Result<(BoxRefMut<MappedPages, [T]>, Vec<ReceiveBuffer>), &'static str> {
-    
+/// * `buffer_size`: size of each buffer in the pool in bytes
+/// * `rxq_regs`: registers needed to set up a receive queue 
+pub fn init_rx_queue<T: RxDescriptor, S:RxQueueRegisters>(num_desc: usize, rx_buffer_pool: &'static mpmc::Queue<ReceiveBuffer>, buffer_size: usize, rxq_regs: &mut S)
+    -> Result<(BoxRefMut<MappedPages, [T]>, Vec<ReceiveBuffer>), &'static str> 
+{    
     let size_in_bytes_of_all_rx_descs_per_queue = num_desc * core::mem::size_of::<T>();
     
     // Rx descriptors must be 128 byte-aligned, which is satisfied below because it's aligned to a page boundary.
@@ -147,15 +139,15 @@ pub fn init_rx_queue<T: RxDescriptor>(num_desc: usize, rx_buffer_pool: &'static 
     let rx_desc_phys_addr_higher = (rx_descs_starting_phys_addr.value() >> 32) as u32;
     
     // write the physical address of the rx descs ring
-    rdbal.write(rx_desc_phys_addr_lower);
-    rdbah.write(rx_desc_phys_addr_higher);
+    rxq_regs.set_rdbal(rx_desc_phys_addr_lower);
+    rxq_regs.set_rdbah(rx_desc_phys_addr_higher);
 
     // write the length (in total bytes) of the rx descs array
-    rdlen.write(size_in_bytes_of_all_rx_descs_per_queue as u32); // should be 128 byte aligned, minimum 8 descriptors
+    rxq_regs.set_rdlen(size_in_bytes_of_all_rx_descs_per_queue as u32); // should be 128 byte aligned, minimum 8 descriptors
     
     // Write the head index (the first receive descriptor)
-    rdh.write(0);
-    rdt.write(0);   
+    rxq_regs.set_rdh(0);
+    rxq_regs.set_rdt(0);   
 
     Ok((rx_descs, rx_bufs_in_use))        
 }
@@ -164,15 +156,10 @@ pub fn init_rx_queue<T: RxDescriptor>(num_desc: usize, rx_buffer_pool: &'static 
 /// 
 /// # Arguments
 /// * `num_desc`: number of descriptors in the queue
-/// * `tdbal`: register to store the lower (least significant) 32 bits of the physical address of the array of transmit descriptors
-/// * `tdbah`: register to store the higher (most significant) 32 bits of the physical address of the array of transmit descriptors
-/// * `tdlen`: register to store the length in bytes of the array of transmit descriptors
-/// * `tdh`: register to store the transmit descriptor head index
-/// * `tdt`: register to store the transmit descriptor tail index
-pub fn init_tx_queue<T: TxDescriptor>(num_desc: usize, tdbal: &mut Volatile<Tdbal>, tdbah: &mut Volatile<Tdbah>, tdlen: &mut Volatile<Tdlen>, 
-            tdh: &mut Volatile<Tdh>, tdt: &mut Volatile<Tdt>) 
-                -> Result<BoxRefMut<MappedPages, [T]>, &'static str> {
-
+/// * `txq_regs`: registers needed to set up a transmit queue
+pub fn init_tx_queue<T: TxDescriptor, S: TxQueueRegisters>(num_desc: usize, txq_regs: &mut S) 
+    -> Result<BoxRefMut<MappedPages, [T]>, &'static str> 
+{
     let size_in_bytes_of_all_tx_descs = num_desc * core::mem::size_of::<T>();
     
     // Tx descriptors must be 128 byte-aligned, which is satisfied below because it's aligned to a page boundary.
@@ -192,15 +179,15 @@ pub fn init_tx_queue<T: TxDescriptor>(num_desc: usize, tdbal: &mut Volatile<Tdba
     let tx_desc_phys_addr_higher = (tx_descs_starting_phys_addr.value() >> 32) as u32;
 
     // write the physical address of the tx descs array
-    tdbal.write(tx_desc_phys_addr_lower); 
-    tdbah.write(tx_desc_phys_addr_higher); 
+    txq_regs.set_tdbal(tx_desc_phys_addr_lower); 
+    txq_regs.set_tdbah(tx_desc_phys_addr_higher); 
 
     // write the length (in total bytes) of the tx descs array
-    tdlen.write(size_in_bytes_of_all_tx_descs as u32);               
+    txq_regs.set_tdlen(size_in_bytes_of_all_tx_descs as u32);               
     
     // write the head index and the tail index (both 0 initially because there are no tx requests yet)
-    tdh.write(0);
-    tdt.write(0);
+    txq_regs.set_tdh(0);
+    txq_regs.set_tdt(0);
 
     Ok(tx_descs)
 }
--- kernel/nic_queues/src/lib.rs
+++ kernel/nic_queues/src/lib.rs
@@ -1,13 +1,15 @@
 //! Defines the receive and transmit queues that store a ring of DMA descriptors and related information.
 //! 
 //! Receive and transmit queues are used across all NICs to keep track of incoming and outgoing packets.
-//! HW queues used by the NIC only consist of the ring of DMA descriptors. The SW queues defined here hold the ring of DMA descriptors that it shares with the HW,
-//! as well as other information such as the buffers received from the queues, the tail register for each queue and the cpu the queue is mapped to.
+//! HW queues used by the NIC only consist of the ring of DMA descriptors.
+//! The SW queues defined here hold the ring of DMA descriptors that it shares with the HW,
+//! as well as other information such as the buffers received from the queues,
+//! the tail register for each queue and the cpu the queue is mapped to.
 
 #![no_std]
 
+#[macro_use] extern crate log;
 extern crate alloc;
-extern crate log;
 extern crate memory;
 extern crate intel_ethernet;
 extern crate nic_buffers;
@@ -18,23 +20,55 @@ use alloc::{
     vec::Vec,
     collections::VecDeque
 };
-use memory::MappedPages;
+use memory::{MappedPages, create_contiguous_mapping, EntryFlags};
 use intel_ethernet::descriptors::{RxDescriptor, TxDescriptor};
-use nic_buffers::{ReceiveBuffer, ReceivedFrame};
+use nic_buffers::{ReceiveBuffer, ReceivedFrame, TransmitBuffer};
 
+/// The mapping flags used for pages that the NIC will map.
+pub const NIC_MAPPING_FLAGS: EntryFlags = EntryFlags::from_bits_truncate(
+    EntryFlags::PRESENT.bits() |
+    EntryFlags::WRITABLE.bits() |
+    EntryFlags::NO_CACHE.bits() |
+    EntryFlags::NO_EXECUTE.bits()
+);
+
+/// The register trait that gives access to only those registers required for receiving a packet.
+/// The Rx queue control registers can only be accessed by the physical NIC.
+pub trait RxQueueRegisters {
+    fn set_rdbal(&mut self, value: u32);
+    fn set_rdbah(&mut self, value: u32);
+    fn set_rdlen(&mut self, value: u32);
+    fn set_rdh(&mut self, value: u32);
+    fn set_rdt(&mut self, value: u32);
+}
+
+/// The register trait that gives access to only those registers required for sending a packet.
+/// The Tx queue control registers can only be accessed by the physical NIC.
+pub trait TxQueueRegisters {
+    fn set_tdbal(&mut self, value: u32);
+    fn set_tdbah(&mut self, value: u32);
+    fn set_tdlen(&mut self, value: u32);
+    fn set_tdh(&mut self, value: u32);
+    fn set_tdt(&mut self, value: u32);
+}
 
 /// A struct that holds all information for one receive queue.
 /// There should be one such object per queue.
-pub struct RxQueue<T: RxDescriptor> {
+pub struct RxQueue<S: RxQueueRegisters, T: RxDescriptor> {
     /// The number of the queue, stored here for our convenience.
     pub id: u8,
+    /// Registers for this receive queue
+    pub regs: S,
     /// Receive descriptors
     pub rx_descs: BoxRefMut<MappedPages, [T]>,
+    /// The number of receive descriptors in the descriptor ring
+    pub num_rx_descs: u16,
     /// Current receive descriptor index
     pub rx_cur: u16,
     /// The list of rx buffers, in which the index in the vector corresponds to the index in `rx_descs`.
     /// For example, `rx_bufs_in_use[2]` is the receive buffer that will be used when `rx_descs[2]` is the current rx descriptor (rx_cur = 2).
     pub rx_bufs_in_use: Vec<ReceiveBuffer>,
+    pub rx_buffer_size_bytes: u16,
     /// The queue of received Ethernet frames, ready for consumption by a higher layer.
     /// Just like a regular FIFO queue, newly-received frames are pushed onto the back
     /// and frames are popped off of the front.
@@ -43,21 +77,108 @@ pub struct RxQueue<T: RxDescriptor> {
     pub received_frames: VecDeque<ReceivedFrame>,
     /// The cpu which this queue is mapped to. 
     /// This in itself doesn't guarantee anything, but we use this value when setting the cpu id for interrupts and DCA.
-    pub cpu_id: u8,
+    pub cpu_id: Option<u8>,
+    /// Pool where `ReceiveBuffer`s are stored.
+    pub rx_buffer_pool: &'static mpmc::Queue<ReceiveBuffer>,
+    /// The filter id for the physical NIC filter that is set for this queue
+    pub filter_num: Option<u8>
 }
 
+impl<S: RxQueueRegisters, T: RxDescriptor> RxQueue<S,T> {
+    /// Polls the queue and removes all received packets from it.
+    /// The received packets are stored in the receive queue's `received_frames` FIFO queue.
+    pub fn poll_queue_and_store_received_packets(&mut self) -> Result<(), &'static str> {
+        let mut cur = self.rx_cur as usize;
+       
+        let mut receive_buffers_in_frame: Vec<ReceiveBuffer> = Vec::new();
+        let mut _total_packet_length: u16 = 0;
 
+        while self.rx_descs[cur].descriptor_done() {
+            // get information about the current receive buffer
+            let length = self.rx_descs[cur].length();
+            _total_packet_length += length as u16;
+            // error!("poll_queue_and_store_received_packets {}: received descriptor of length {}", self.id, length);
+            
+            // Now that we are "removing" the current receive buffer from the list of receive buffers that the NIC can use,
+            // (because we're saving it for higher layers to use),
+            // we need to obtain a new `ReceiveBuffer` and set it up such that the NIC will use it for future receivals.
+            let new_receive_buf = match self.rx_buffer_pool.pop() {
+                Some(rx_buf) => rx_buf,
+                None => {
+                    warn!("NIC RX BUF POOL WAS EMPTY.... reallocating! This means that no task is consuming the accumulated received ethernet frames.");
+                    // if the pool was empty, then we allocate a new receive buffer
+                    let len = self.rx_buffer_size_bytes;
+                    let (mp, phys_addr) = create_contiguous_mapping(len as usize, NIC_MAPPING_FLAGS)?;
+                    ReceiveBuffer::new(mp, phys_addr, len, self.rx_buffer_pool)
+                }
+            };
+
+            // actually tell the NIC about the new receive buffer, and that it's ready for use now
+            self.rx_descs[cur].set_packet_address(new_receive_buf.phys_addr);
+
+            // Swap in the new receive buffer at the index corresponding to this current rx_desc's receive buffer,
+            // getting back the receive buffer that is part of the received ethernet frame
+            self.rx_bufs_in_use.push(new_receive_buf);
+            let mut current_rx_buf = self.rx_bufs_in_use.swap_remove(cur); 
+            current_rx_buf.length = length as u16; // set the ReceiveBuffer's length to the size of the actual packet received
+            receive_buffers_in_frame.push(current_rx_buf);
+
+            // move on to the next receive buffer to see if it's ready for us to take
+            self.rx_cur = (cur as u16 + 1) % self.num_rx_descs;
+            self.regs.set_rdt(cur as u32); 
+
+            if self.rx_descs[cur].end_of_packet() {
+                let buffers = core::mem::replace(&mut receive_buffers_in_frame, Vec::new());
+                self.received_frames.push_back(ReceivedFrame(buffers));
+            } else {
+                warn!("NIC::poll_queue_and_store_received_packets(): Received multi-rxbuffer frame, this scenario not fully tested!");
+            }
+            self.rx_descs[cur].reset_status();
+            cur = self.rx_cur as usize;
+        }
+
+        Ok(())
+    }
+
+    /// Returns the earliest received ethernet frame.
+    pub fn return_frame(&mut self) -> Option<ReceivedFrame> {
+        self.received_frames.pop_front()
+    }
+}
 
 /// A struct that holds all information for a transmit queue. 
 /// There should be one such object per queue.
-pub struct TxQueue<T: TxDescriptor> {
+pub struct TxQueue<S: TxQueueRegisters, T: TxDescriptor> {
     /// The number of the queue, stored here for our convenience.
     pub id: u8,
+    /// Registers for this transmit queue
+    pub regs: S,
     /// Transmit descriptors 
     pub tx_descs: BoxRefMut<MappedPages, [T]>,
+    /// The number of transmit descriptors in the descriptor ring
+    pub num_tx_descs: u16,
     /// Current transmit descriptor index
     pub tx_cur: u16,
     /// The cpu which this queue is mapped to. 
     /// This in itself doesn't guarantee anything but we use this value when setting the cpu id for interrupts and DCA.
-    pub cpu_id : u8,
+    pub cpu_id : Option<u8>
 }
+
+impl<S: TxQueueRegisters, T: TxDescriptor> TxQueue<S,T> {
+    /// Sends a packet on the transmit queue
+    /// 
+    /// # Arguments:
+    /// * `transmit_buffer`: buffer containing the packet to be sent
+    pub fn send_on_queue(&mut self, transmit_buffer: TransmitBuffer) {
+        self.tx_descs[self.tx_cur as usize].send(transmit_buffer.phys_addr, transmit_buffer.length);  
+        // update the tx_cur value to hold the next free descriptor
+        let old_cur = self.tx_cur;
+        self.tx_cur = (self.tx_cur + 1) % self.num_tx_descs;
+        // update the tdt register by 1 so that it knows the previous descriptor has been used
+        // and has a packet to be sent
+        self.regs.set_tdt(self.tx_cur as u32);
+        // Wait for the packet to be sent
+        self.tx_descs[old_cur as usize].wait_for_packet_tx();
+    }
+}
+
