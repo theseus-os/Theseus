d7c20d3eee4de2078c587b3a8f34694437036e23
kevinaboos <kevinaboos@users.noreply.github.com>
2021-07-01 22:43:36 +0000

    Redesign I/O traits to offer generic block-based and byte-based reader/writer traits and types (#395) 266707fd2db005800227bbb026de004a97205542

---
+++ doc/implementors/block_io/trait.BlockIo.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["ata"] = [{"text":"impl <a class=\"trait\" href=\"block_io/trait.BlockIo.html\" title=\"trait block_io::BlockIo\">BlockIo</a> for <a class=\"struct\" href=\"ata/struct.AtaDrive.html\" title=\"struct ata::AtaDrive\">AtaDrive</a>","synthetic":false,"types":["ata::AtaDrive"]}];
+implementors["block_io"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ doc/implementors/block_io/trait.BlockReader.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["ata"] = [{"text":"impl <a class=\"trait\" href=\"block_io/trait.BlockReader.html\" title=\"trait block_io::BlockReader\">BlockReader</a> for <a class=\"struct\" href=\"ata/struct.AtaDrive.html\" title=\"struct ata::AtaDrive\">AtaDrive</a>","synthetic":false,"types":["ata::AtaDrive"]}];
+implementors["block_io"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
