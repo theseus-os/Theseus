2cd626d75cc5505f465b6b97991a1d1e2814196b
kevinaboos <kevinaboos@users.noreply.github.com>
2021-07-16 19:59:10 +0000

    Rename `block_io` to `io` as it now contains non-block-based I/O types (#411) 051e52782658a3e0f11c486d8656e71da1f7ba07

--- doc/implementors/block_io/trait.KnownLength.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {};
-implementors["ata"] = [{"text":"impl <a class=\"trait\" href=\"block_io/trait.KnownLength.html\" title=\"trait block_io::KnownLength\">KnownLength</a> for <a class=\"struct\" href=\"ata/struct.AtaDrive.html\" title=\"struct ata::AtaDrive\">AtaDrive</a>","synthetic":false,"types":["ata::AtaDrive"]}];
-implementors["block_io"] = [];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- doc/implementors/io/trait.ByteReader.js
+++ doc/implementors/io/trait.ByteReader.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["block_io"] = [];
+implementors["io"] = [];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
