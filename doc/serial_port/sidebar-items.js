4948fe9ec2eda713a8558e8767ad3b320112ccdb
kevinaboos <kevinaboos@users.noreply.github.com>
2021-07-30 22:00:50 +0000

    Enable regular I/O over serial ports, e.g., COM1 on x86 (#422)
    * Implement very basic interrupt handler for all COM serial ports.
    
    * `SerialPort` instances are no longer exclusively owned by the logger, as they must be accessible from within the serial port interrupt handler and other kernel/application crates.
    
    * Add `-serial mon:stdio` option to `QEMU_FLAGS` to use QEMU's monitor muxed with stdio console together, but as a non-default option right now.
    
    * Implement `bare_io` `Read` and `Write` traits for the `SerialPort`.
    
    * Add simple `test_serial_echo` app. 204397b393e0fb3c4ed72961c57245f32645fcca

--- doc/serial_port/sidebar-items.js
+++ doc/serial_port/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["COM1_BASE_PORT",""],["COM2_BASE_PORT",""],["COM3_BASE_PORT",""],["COM4_BASE_PORT",""]],"struct":[["SerialPort","A serial port and its various data and control registers."]]});
+initSidebarItems({"enum":[["SerialPortAddress","The base port I/O addresses for COM serial ports."]],"fn":[["get_serial_port","Obtains a reference to the [`SerialPort`] specified by the given [`SerialPortAddress`]."]],"struct":[["SerialPort","A serial port and its various data and control registers."]]});
