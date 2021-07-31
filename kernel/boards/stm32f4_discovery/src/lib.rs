d2c059e387ec4aa8d5eedefac4e05c76aaab44e6
Jacob Earle <jacob.earle@yale.edu>
2021-07-31 13:36:51 -0400

    Removing extraneous old code blocks and revising documentation for STM_PERIPHERALS

--- kernel/boards/stm32f4_discovery/src/lib.rs
+++ kernel/boards/stm32f4_discovery/src/lib.rs
@@ -5,20 +5,12 @@
 cfg_if!{
 if #[cfg(target_vendor = "stm32f407")] {
     use stm32f4::stm32f407;
-    use core::cell::RefCell;
-    use cortex_m::interrupt::{self, Mutex};
     use lazy_static::lazy_static;
     use irq_safety::MutexIrqSafe;
 
     lazy_static!{
         /// This struct exposes device-specific peripherals conforming to the `rust2svd` API.
-        /// In order to allow safe sharing, we must first wrap the `Peripherals` struct in a `RefCell` to add the `Sync` trait, then we can use `cortex_m::interrupt::Mutex` to allow peripherals to be locked.
-        /// For more information on why we must wrap the `Peripherals` struct in a `RefCell` and a `Mutex`, see [here](https://docs.rust-embedded.org/book/concurrency/index.html)
-        // pub static ref STM_PERIPHERALS : Mutex<RefCell<stm32f407::Peripherals>> = {
-        //     let p = stm32f407::Peripherals::take().unwrap();
-        //     Mutex::new(RefCell::new(p))
-        // };
-
+        /// In order to allow safe sharing, we utilize `irq_safety::MutexIrqSafe`
         pub static ref STM_PERIPHERALS : MutexIrqSafe<stm32f407::Peripherals> = {
             let p = stm32f407::Peripherals::take().unwrap();
             MutexIrqSafe::new(p)
--- kernel/boards/stm32f4_discovery/src/uart/mod.rs
+++ kernel/boards/stm32f4_discovery/src/uart/mod.rs
@@ -1,48 +1,9 @@
 //! Implements UART specific functionality for the STM32F4 Discovery Board 
-use cortex_m::interrupt;
 use core::fmt;
 use crate::STM_PERIPHERALS;
 
 /// Initialize UART for use.
 pub fn uart_init() {
-    // interrupt::free(|cs| {
-    //     let p = STM_PERIPHERALS.borrow(cs).borrow();
-    //     let uart = &p.USART2;
-
-    //     // initializing gpio
-    //     let gpioa = &p.GPIOA;
-    //     let rcc = &p.RCC;
-
-    //     // initializing clock
-    //     rcc.ahb1enr.write(|w| w.gpioaen().bit(true));
-    //     // initialize uart clock
-    //     rcc.apb1enr.write(|w| w.usart2en().bit(true));
-
-    //     // set up PA2 and PA3 pins for alternate function
-    //     gpioa.afrl.modify(|_,w| w.afrl2().bits(0b0111).afrl3().bits(0b0111));
-    //     gpioa.moder.modify(|_,w| w.moder2().bits(0b10).moder3().bits(0b10));
-
-    //     // configure pin output speeds to high
-    //     gpioa.ospeedr.modify(|_,w| w.ospeedr2().bits(0b10).ospeedr3().bits(0b10));
-
-    //     // Enable the USART
-    //     uart.cr1.modify(|_,w| w.ue().bit(true));
-
-    //     // Set the word length
-    //     uart.cr1.modify(|_,w| w.m().bit(false));
-
-    //     // Program the number of stop bits
-    //     uart.cr2.modify(|_,w| w.stop().bits(0));
-
-    //     // Disable DMA transfer
-    //     uart.cr3.modify(|_,w| w.dmat().bit(false));
-
-    //     // Select the desired baudrate, in this case 16 MHz / 104.1875 = 9600 bits/second
-    //     uart.brr.modify(|_,w| w.div_mantissa().bits(104).div_fraction().bits(3));
-
-    //     // Initialize uart for reading and writing
-    //     uart.cr1.modify(|_,w| w.te().bit(true).re().bit(true));
-    // });
     {
         let p = STM_PERIPHERALS.lock();
         let uart = &p.USART2;
@@ -107,15 +68,6 @@ pub struct SerialPort;
 
 impl fmt::Write for SerialPort {
     fn write_str(&mut self, s: &str) -> fmt::Result {
-        // interrupt::free(|cs| {
-        //     let p = STM_PERIPHERALS.borrow(cs).borrow();
-        //     let usart = &p.USART2;
-        //     for byte in s.as_bytes().iter() {
-        //         while usart.sr.read().txe().bit_is_clear() {} 
-
-        //         usart.dr.write(|w| w.dr().bits(u16::from(*byte)));
-        //     }
-        // });
         {
             let p = STM_PERIPHERALS.lock();
             let usart = &p.USART2;
