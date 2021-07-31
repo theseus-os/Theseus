404544930a6a383fd2e6536ba95bbe5a9890cc18
Jacob Earle <jacob.earle@yale.edu>
2021-07-31 09:01:54 -0400

    Started changing cortex_m's built in mutex to irq_safety mutex

--- kernel/stm32f4_discovery/src/lib.rs
+++ kernel/stm32f4_discovery/src/lib.rs
@@ -3,10 +3,12 @@
 
 cfg_if!{
 if #[cfg(target_vendor = "stm32f407")] {
+    extern crate irq_safety;
     use stm32f4::stm32f407;
     use core::cell::RefCell;
     use cortex_m::interrupt::{self, Mutex};
     use lazy_static::lazy_static;
+    use irq_safety::MutexIrqSafe;
 
     lazy_static!{
         pub static ref STM_PERIPHERALS : MutexIrqSafe<stm32f407::Peripherals> = {
