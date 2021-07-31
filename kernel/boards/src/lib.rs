23c4aab0e29b00f6f88429ea8864fc6248b54fa6
Jacob Earle <jacob.earle@yale.edu>
2021-07-31 13:38:37 -0400

    Copying revised directory structure and use of MutexIrqSafe from arm-testing branch

---
+++ kernel/boards/src/lib.rs
@@ -0,0 +1,2 @@
+//! This crate does not implement any functionality itself, but instead contains a subcrate that implements the platform-specific functionality for each ARM microcontroller we support.
+#![no_std]
---
+++ kernel/boards/stm32f4_discovery/src/lib.rs
@@ -0,0 +1,27 @@
+//! Implements platform specific functionalities for the STM32F4_Discovery board, exposing its peripherals and providing ways of interacting with them.
+#![no_std]
+#[macro_use] extern crate cfg_if;
+
+cfg_if!{
+if #[cfg(target_vendor = "stm32f407")] {
+    use stm32f4::stm32f407;
+    use lazy_static::lazy_static;
+    use irq_safety::MutexIrqSafe;
+
+    lazy_static!{
+        /// This struct exposes device-specific peripherals conforming to the `rust2svd` API.
+        /// In order to allow safe sharing, we utilize `irq_safety::MutexIrqSafe`
+        pub static ref STM_PERIPHERALS : MutexIrqSafe<stm32f407::Peripherals> = {
+            let p = stm32f407::Peripherals::take().unwrap();
+            MutexIrqSafe::new(p)
+        };
+    }
+
+    pub mod uart;
+
+    /// Initializes device peripherals for use.
+    pub fn init_peripherals () {
+        uart::uart_init();
+    }
+}
+}
