2348eb750ba7b6029400dbdfb44d429832d1fd13
Jacob Earle <jacob.earle@yale.edu>
2021-08-06 17:10:08 -0400

    Added dummy implementation of 'serial ports' when semihosting must be used

--- kernel/nano_core/src/lib.rs
+++ kernel/nano_core/src/lib.rs
@@ -293,7 +293,13 @@ extern "C" fn main() -> ! {
         }
     }
 
-    let logger_serial_ports = [serial_port::SerialPortAddress::USART2];  // some servers use COM2 instead. 
+    #[cfg(target_vendor = "stm32f407")]
+    let default_logger_port = serial_port::SerialPortAddress::USART2;
+
+    #[cfg(target_vendor = "unknown")]
+    let default_logger_port = serial_port::SerialPortAddress::Semihost;
+
+    let logger_serial_ports = [default_logger_port];  // some servers use COM2 instead. 
     logger::init(None, &logger_serial_ports).unwrap();
 
     info!("memory and logging initialized");
