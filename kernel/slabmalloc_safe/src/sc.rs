99a0fdfce97c5415c02c97ef85f295477e66de71
Yechan Bae <yechan@gatech.edu>
2021-08-12 11:47:32 -0400

    Safety adjustmen for deallocate functions

--- kernel/slabmalloc_safe/src/sc.rs
+++ kernel/slabmalloc_safe/src/sc.rs
@@ -278,7 +278,11 @@ impl SCAllocator {
     /// May return an error in case an invalid `layout` is provided.
     /// The function may also move internal slab pages between lists partial -> empty
     /// or full -> partial lists.
-    pub fn deallocate(&mut self, ptr: NonNull<u8>, layout: Layout) -> Result<(), &'static str> {
+    ///
+    /// # Safety
+    /// The caller must ensure that `ptr` argument is returned from [`Self::allocate()`]
+    /// and `layout` argument is correct.
+    pub unsafe fn deallocate(&mut self, ptr: NonNull<u8>, layout: Layout) -> Result<(), &'static str> {
         assert!(layout.size() <= self.size);
         assert!(self.size <= (MappedPages8k::SIZE - CACHE_LINE_SIZE));
         // trace!(
@@ -294,7 +298,7 @@ impl SCAllocator {
             .ok_or("pointer to deallocate was an invalid virtual address")?;
 
         // Figure out which page we are on and retrieve a reference to it
-        let new_layout = unsafe { Layout::from_size_align_unchecked(self.size, layout.align()) };
+        let new_layout = Layout::from_size_align_unchecked(self.size, layout.align());
 
         let (ret, slab_page_is_empty, slab_page_was_full, list_id) = {
             // find slab page from partial slabs
