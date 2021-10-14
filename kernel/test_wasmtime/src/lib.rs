c8f9d1a46d5c2d2034efa5e6477660e2f9cdb0ac
Kevin Boos <kevinaboos@gmail.com>
2021-10-14 14:28:35 -0700

    wasmtime-types works on no_std

--- kernel/test_wasmtime/src/lib.rs
+++ kernel/test_wasmtime/src/lib.rs
@@ -7,8 +7,7 @@ extern crate alloc;
 #[macro_use] extern crate log;
 
 // extern crate wasmparser; // wasmparser is working on no_std
-extern crate cranelift_entity; // cranelift-entity is working on no_std with the "enable-serde" feature.
-
-// extern crate wasmtime_types; 
+// extern crate cranelift_entity; // cranelift-entity (with the "enable-serde" feature) is working on no_std
+extern crate wasmtime_types; 
 
 // extern crate wasmtime;
