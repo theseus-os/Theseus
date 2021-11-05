6b5aa5e6de3a44b0a97e624102b4162c1051d695
Jacob Earle <jacob.earle@yale.edu>
2021-11-05 12:28:58 -0400

    Fixed issue with ordering of delayed tasks in the priority_queue

--- kernel/captain/src/armv7em.rs
+++ kernel/captain/src/armv7em.rs
@@ -43,12 +43,14 @@ pub fn init() -> ! {
     spawn::create_idle_task(Some(0)).unwrap();
 
     // Build and spawn two tasks.
-    let tb1 = new_task_builder(task_hello, 233);
-    tb1.spawn().unwrap();
-    let tb2 = new_task_builder(task_world, 466);
-    tb2.spawn().unwrap();
+    //let tb1 = new_task_builder(task_hello, 233);
+    //tb1.spawn().unwrap();
+    //let tb2 = new_task_builder(task_world, 466);
+    //tb2.spawn().unwrap();
     let tb3 = new_task_builder(task_delay_ten_seconds, 1);
     tb3.spawn().unwrap();
+    let tb4 = new_task_builder(task_delay_two_seconds, 2);
+    tb4.spawn().unwrap();
 
     interrupt::free(move |cs| {
         let mut p = PERIPHERALS.borrow(cs).borrow_mut();
--- kernel/scheduler/src/delay.rs
+++ kernel/scheduler/src/delay.rs
@@ -1,4 +1,5 @@
 use core::sync::atomic::{Ordering, AtomicUsize};
+use core::cmp::Reverse;
 use priority_queue::priority_queue::PriorityQueue;
 use hashbrown::hash_map::DefaultHashBuilder;
 use irq_safety::MutexIrqSafe;
@@ -7,7 +8,7 @@ use task::get_my_current_task;
 lazy_static! {
 	/// List of all delayed tasks in the system
 	/// Implemented as a priority queue where the key is the unblocking time and the value is the id of the task
-	static ref DELAYED_TASKLIST: MutexIrqSafe<PriorityQueue<usize, usize, DefaultHashBuilder>> = MutexIrqSafe::new(PriorityQueue::with_default_hasher());
+	static ref DELAYED_TASKLIST: MutexIrqSafe<PriorityQueue<usize, Reverse<usize>, DefaultHashBuilder>> = MutexIrqSafe::new(PriorityQueue::with_default_hasher());
 }
 
 /// Keeps track of the next task that needs to unblock, by default, it is the maximum time
@@ -17,7 +18,7 @@ pub static NEXT_DELAYED_TASK_UNBLOCK_TIME : AtomicUsize = AtomicUsize::new(usize
 /// Helper function adds the id associated with a TaskRef to the list of delayed tasks with priority equal to the time when the task must resume work
 /// If the resume time is less than the current earliest resume time, we will update it
 fn add_to_delayed_tasklist(taskid: usize, resume_time: usize) {
-	DELAYED_TASKLIST.lock().push(taskid, resume_time);
+	DELAYED_TASKLIST.lock().push(taskid, Reverse(resume_time));
 	
 	let next_unblock_time = NEXT_DELAYED_TASK_UNBLOCK_TIME.load(Ordering::SeqCst);
 	if (resume_time < next_unblock_time) {
@@ -34,7 +35,7 @@ pub fn remove_next_task_from_delayed_tasklist() {
 	}
 
 	match delayed_tasklist.peek() {
-		Some((new_taskid, new_resume_time)) => NEXT_DELAYED_TASK_UNBLOCK_TIME.store(*new_resume_time, Ordering::SeqCst),
+		Some((new_taskid, Reverse(new_resume_time))) => NEXT_DELAYED_TASK_UNBLOCK_TIME.store(*new_resume_time, Ordering::SeqCst),
 		None => NEXT_DELAYED_TASK_UNBLOCK_TIME.store(usize::MAX, Ordering::SeqCst),
 	}
 	}
