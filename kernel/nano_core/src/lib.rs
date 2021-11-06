5ea968be961dc470674c8c067ccf81323fa1ef22
Kevin Boos <kevinaboos@gmail.com>
2021-11-06 16:24:34 -0700

    cleanup, disable `tls_test` because it doesn't work with dynamically-loaded TLS data sections yet.

--- kernel/nano_core/src/lib.rs
+++ kernel/nano_core/src/lib.rs
@@ -227,7 +227,7 @@ pub extern "C" fn nano_core_start(
 
     // TODO: REMOVE: just a hack to force the thread_local_storage crate's code/data sections to be included.
     //               this won't actually be invoked because the captain shouldn't return.
-    thread_local_storage::test_tls(3);
+    // thread_local_storage::test_tls(3);
 
     // the captain shouldn't return ...
     try_exit!(Err("captain::init returned unexpectedly... it should be an infinite loop (diverging function)"));
