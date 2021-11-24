e5e805f38f2539dcfb0c2a82d70a153087f7850a
Kevin Boos <kevinaboos@gmail.com>
2021-11-24 13:50:22 -0800

    Update submodules: * `crc32fast` has been updated, so we can remove our fork of it. * Finished porting `object`'s write feature to no_std, PR in progress. * Finished porting `region` to Theseus.

--- kernel/test_wasmtime/src/lib.rs
+++ kernel/test_wasmtime/src/lib.rs
@@ -10,7 +10,7 @@ extern crate alloc;
 // extern crate cranelift_entity; // cranelift-entity (with the "enable-serde" feature) is working on no_std
 // extern crate wasmtime_types; // wasmtime-types is working on no_std
 // extern crate wasmtime_environ;  // wasmtime-environ is working on no_std
-
+// extern crate region;  // region is working on no_std
 
 // extern crate wasmtime_runtime;  // wasmtime-runtime is a WIP on no_std
 
