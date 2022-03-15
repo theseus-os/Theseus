5387e9cc06deaaa6d4a04c40c76f556f458f0298
Kevin Boos <kevinaboos@gmail.com>
2022-03-14 19:30:46 -0700

    the `wasmtime-runtime` crate builds but is not yet incomplete.
    Some changes made to other dependent crates to accommodate them.

--- kernel/test_wasmtime/src/lib.rs
+++ kernel/test_wasmtime/src/lib.rs
@@ -3,8 +3,8 @@
 
 #![no_std]
 
-extern crate alloc;
-#[macro_use] extern crate log;
+// extern crate alloc;
+// #[macro_use] extern crate log;
 
 // extern crate wasmparser; // wasmparser is working on no_std
 // extern crate cranelift_entity; // cranelift-entity (with the "enable-serde" feature) is working on no_std
@@ -12,6 +12,7 @@ extern crate alloc;
 // extern crate wasmtime_environ;  // wasmtime-environ is working on no_std
 // extern crate region;  // region is working on no_std
 
-// extern crate wasmtime_runtime;  // wasmtime-runtime is a WIP on no_std
+extern crate wasmtime_runtime;  // wasmtime-runtime is a WIP on no_std
 
 // extern crate wasmtime;
+
