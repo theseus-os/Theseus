feee8d56240d57748b74ed31c2ff61171970d300
Kevin Boos <kevinaboos@gmail.com>
2022-05-26 19:12:08 -0700

    [no ci] WIP experimenting with conditional inclusion of crates via a central `theseus_config` crate that declares multiple Cargo-level features.

---
+++ theseus_features/src/lib.rs
@@ -0,0 +1,6 @@
+//! This crate exists solely to hold a centralized list of features 
+//! used across all Theseus crates for conditional compilation and configuration.
+//! 
+//! It has no code, logic, or data.
+
+#![no_std]
