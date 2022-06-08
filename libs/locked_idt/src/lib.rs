b61b1f206bc167d5c9a2549a829a0d8941195447
Kevin Boos <kevinaboos@gmail.com>
2022-06-08 11:54:42 -0700

    Use upstream `x86_64` crate instead of our own fork.
    * Move `LockedIdt` wrapper type to its own library crate.
    
    * Upgrade crates that use `x86_64` or `LockedIdt` to the 2018 edition.

---
+++ libs/locked_idt/src/lib.rs
@@ -0,0 +1,39 @@
+//! A simple wrapper struct around an x86_64 Interrupt Descriptor Table (IDT).
+
+#![no_std]
+
+use irq_safety::{MutexIrqSafe, MutexIrqSafeGuard};
+use x86_64::structures::idt::InterruptDescriptorTable;
+
+/// A thread-safe and interrupt-safe wrapper around [`InterruptDescriptorTable`]. 
+/// 
+/// This type offers interior mutability, allowing interrupt handlers to be added/changed/removed,
+/// but preserves safety by guaranteeing that only a static object can be loaded.
+#[derive(Debug)]
+pub struct LockedIdt {
+    idt: MutexIrqSafe<InterruptDescriptorTable>,
+}
+impl LockedIdt {
+    /// Creates a new IDT filled with non-present entries.
+    pub const fn new() -> LockedIdt {
+        LockedIdt {
+            idt: MutexIrqSafe::new(InterruptDescriptorTable::new()),
+        }
+    }
+
+    /// Obtains the lock on the inner IDT and loads it into the current CPU
+    /// using the `lidt` command.
+    pub fn load(&'static self) {
+        unsafe { self.idt.lock().load_unsafe(); }
+    } 
+
+    /// Obtains the lock on the inner IDT and returns a guard that derefs into it.
+    /// 
+    /// Interrupts are also disabled until the guard falls out of scope,
+    /// at which point they are re-enabled iff they were previously enabled
+    /// when this function was invoked. 
+    /// and the lock will be dropped when the guard falls out of scope.
+    pub fn lock(&self) -> MutexIrqSafeGuard<InterruptDescriptorTable> {
+        self.idt.lock()
+    }
+}
