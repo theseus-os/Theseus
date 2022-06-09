c608cda1b0ff6422c82d24b7da67e73c748a631c
Kevin Boos <kevinaboos@gmail.com>
2022-06-09 14:44:10 -0700

    Remove unneeded kernel crate to invoke wasmtime. We can do it straight from an application crate.

--- applications/test_wasmtime/src/lib.rs
+++ applications/test_wasmtime/src/lib.rs
@@ -13,6 +13,8 @@ use alloc::{
     vec::Vec,
 };
 use path::Path;
+use anyhow::Result;
+use wasmtime::*;
 
 
 pub fn main(args: Vec<String>) -> isize {
@@ -47,8 +49,50 @@ fn rmain(args: Vec<String>) -> Result<(), String> {
         ));
     }
 
-    wasmtime_runner::run_hello_world(bytes.as_slice())
+    run_hello_world(bytes.as_slice())
         .map_err(|e| format!("{}", e))?;
 
+    Ok(())
+}
+
+
+
+/// Taken from `wasmtime/crates/wasmtime/src/lib.rs` docs example code.
+pub fn run_hello_world(hello_world_cwasm_contents: &[u8]) -> Result<()> {
+    // Modules can be compiled through either the text or binary format
+    let engine = Engine::default();
+    // Theseus note: `Module::new()` requires `#[cfg(compiler)]` for wasmtime,
+    // such that it can perform JIT compilation of the WASM binary. 
+    // We currently don't support that, so we have to use `Module::deserialize()`.
+    // Old code: 
+    // ```
+    // let wat = r#"
+    //     (module
+    //         (import "host" "hello" (func $host_hello (param i32)))
+    //         (func (export "hello")
+    //             i32.const 3
+    //             call $host_hello)
+    //     )
+    // "#;
+    // let module = Module::new(&engine, wat)?;
+    // ```
+    let module = unsafe {
+        Module::deserialize(&engine, hello_world_cwasm_contents)? 
+    };
+    // All wasm objects operate within the context of a "store". Each
+    // `Store` has a type parameter to store host-specific data, which in
+    // this case we're using `4` for.
+    let mut store = Store::new(&engine, 4);
+    let host_hello = Func::wrap(&mut store, |caller: Caller<'_, u32>, param: i32| {
+        println!("Got {} from WebAssembly", param);
+        println!("my host state is: {}", caller.data());
+    });
+    // Instantiation of a module requires specifying its imports and then
+    // afterwards we can fetch exports by name, as well as asserting the
+    // type signature of the function with `get_typed_func`.
+    let instance = Instance::new(&mut store, &module, &[host_hello.into()])?;
+    let hello = instance.get_typed_func::<(), (), _>(&mut store, "hello")?;
+    // And finally we can call the wasm!
+    hello.call(&mut store, ()).map_err(anyhow::Error::msg)?;
     Ok(())
 }
