199b5da0557cbd573b9a300d799dc712e4a19325
Klim Tsoutsman <klim@tsoutsman.com>
2022-07-12 17:52:42 +1000

    Add timer abstraction
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

---
+++ kernel/timer/src/lib.rs
@@ -0,0 +1,20 @@
+pub trait Timer {
+    fn value() -> Timespec;
+
+    // TODO: enable, period/frequency, and accuracy
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Timespec {
+    pub secs: u64,
+    pub nanos: u32,
+}
+
+impl Timespec {
+    pub fn zero() -> Self {
+        Self {
+            secs: 0,
+            nanos: 0,
+        }
+    }
+}
