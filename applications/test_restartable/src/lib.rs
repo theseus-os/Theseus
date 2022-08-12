1057746d00fa16af5d4091f62c952c66bfd72901
Kevin Boos <kevinaboos@gmail.com>
2022-08-11 22:19:44 -0700

    Support flexible arguments for restartable tasks
    * The first spawned instance of a restartable task will always use the
    argument passed into the `new_task_builder()`.
    
    * The subsequent restarted instances of that task can optionally use a
    different argument for each newly-restarted task instance.

--- applications/test_restartable/src/lib.rs
+++ applications/test_restartable/src/lib.rs
@@ -178,35 +178,35 @@ pub fn main(args: Vec<String>) -> isize {
     if matches.opt_present("s"){
         let taskref1  = new_task_builder(simple_restartable_loop, exit_method)
             .name(String::from("restartable_loop"))
-            .spawn_restartable()
+            .spawn_restartable(None)
             .expect("Couldn't start the restartable task"); 
 
         taskref1.join().expect("Task 1 join failed");
     } else if matches.opt_present("l"){
         let taskref1  = new_task_builder(restartable_loop_with_lock, exit_method)
             .name(String::from("restartable_loop with lock"))
-            .spawn_restartable()
+            .spawn_restartable(None)
             .expect("Couldn't start the restartable task"); 
 
         taskref1.join().expect("Task 1 join failed");
     } else if matches.opt_present("f"){
         let taskref1  = new_task_builder(restartable_lock_fail, exit_method)
             .name(String::from("restartable_lock_fail"))
-            .spawn_restartable()
+            .spawn_restartable(None)
             .expect("Couldn't start the restartable task"); 
 
         taskref1.join().expect("Task 1 join failed");
     } else if matches.opt_present("m"){
         let taskref1  = new_task_builder(restartable_lock_modified, exit_method)
             .name(String::from("restartable_lock_fail"))
-            .spawn_restartable()
+            .spawn_restartable(None)
             .expect("Couldn't start the restartable task"); 
 
         taskref1.join().expect("Task 1 join failed");
     } else if matches.opt_present("r"){
         let taskref1  = new_task_builder(restartable_lock_recursive, exit_method)
             .name(String::from("restartable_lock_fail"))
-            .spawn_restartable()
+            .spawn_restartable(None)
             .expect("Couldn't start the restartable task"); 
 
         taskref1.join().expect("Task 1 join failed");
