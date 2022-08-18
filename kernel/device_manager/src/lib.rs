5423124bd54003c015ec66b68c797d1a7686b550
Kevin Boos <kevinaboos@gmail.com>
2022-08-18 12:47:15 -0700

    Refactor PS2 mouse out of `interrupts` crate
    * Inverts the dependency chain from `interrupts` -> `mouse` to
      `mouse` -> `interrupts`, which is more typical.
    
    * The PS2 mouse driver now registers its own interrupt as normal.

--- kernel/device_manager/src/lib.rs
+++ kernel/device_manager/src/lib.rs
@@ -96,7 +96,7 @@ pub fn init(key_producer: Queue<Event>, mouse_producer: Queue<Event>) -> Result<
     init_serial_port(SerialPortAddress::COM2);
 
     keyboard::init(key_producer)?;
-    mouse::init(mouse_producer);
+    mouse::init(mouse_producer)?;
 
     // Initialize/scan the PCI bus to discover PCI devices
     for dev in pci::pci_device_iter() {
