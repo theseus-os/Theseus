ab43452d30dfe1e67aaf70477da067a1dd7317c0
Nathan Royer <61582713+NathanRoyer@users.noreply.github.com>
2022-10-26 00:10:37 +0200

    Removed wildcard imports from memory crate (#665)

--- kernel/memory/src/lib.rs
+++ kernel/memory/src/lib.rs
@@ -32,15 +32,18 @@ mod paging;
 #[cfg(mapper_spillful)]
 pub mod paging;
 
+pub use self::paging::{MappedPages, PageTable, Mapper};
 
-pub use self::paging::*;
+pub use memory_structs::{Frame, Page, FrameRange, PageRange, VirtualAddress, PhysicalAddress};
+pub use page_allocator::{AllocatedPages, allocate_pages, allocate_pages_at,
+    allocate_pages_by_bytes, allocate_pages_by_bytes_at};
 
-pub use memory_structs::*;
-pub use page_allocator::*;
-pub use frame_allocator::*;
+pub use frame_allocator::{AllocatedFrames, MemoryRegionType, PhysicalMemoryRegion,
+    allocate_frames_by_bytes_at, allocate_frames_by_bytes, allocate_frames_at};
 
 #[cfg(target_arch = "x86_64")]
-use memory_x86_64::*;
+use memory_x86_64::{BootInformation, get_kernel_address, get_boot_info_mem_area, find_section_memory_bounds,
+    get_vga_mem_addr, get_modules_address, tlb_flush_virt_addr, tlb_flush_all, get_p4};
 
 #[cfg(target_arch = "x86_64")]
 pub use memory_x86_64::EntryFlags;// Export EntryFlags so that others does not need to get access to memory_<arch>.
