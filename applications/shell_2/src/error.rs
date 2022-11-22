c4fc8eb6675d70e3bd0fa7ac4d8c7cd9330b3904
Klim Tsoutsman <klim@tsoutsman.com>
2022-11-23 10:17:39 +1100

    Document shell errors
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- applications/shell_2/src/error.rs
+++ applications/shell_2/src/error.rs
@@ -4,8 +4,16 @@ pub type Result<T> = core::result::Result<T, Error>;
 
 #[derive(Clone, Debug)]
 pub enum Error {
+    /// The user requested the shell to exit.
     ExitRequested,
+    /// The shell could not access its task struct.
     CurrentTaskUnavailable,
+    /// The command input by the user could not be resolved.
+    ///
+    /// The input command is stored in the field.
     CommandNotFound(String),
+    /// The command returned with a non-zero exit code.
+    ///
+    /// The exit code is stored in the field.
     Command(isize),
 }
