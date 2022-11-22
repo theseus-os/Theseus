9d5e0fbddc4e1dd02a0e8ac8476c84c45cd5a1c5
Nathan Royer <nathan.royer.pro@gmail.com>
2022-11-22 17:41:31 +0100

    Copied x86_64 crates: memory, no_drop, page_table_entry

---
+++ aarch64/kernel/memory/src/lib.rs
@@ -0,0 +1,267 @@
+//! This crate implements the virtual memory subsystem for Theseus,
+//! which is fairly robust and provides a unification between 
+//! arbitrarily mapped sections of memory and Rust's lifetime system. 
+//! Originally based on Phil Opp's blog_os. 
+
+#![no_std]
+#![feature(ptr_internals)]
+#![feature(unboxed_closures)]
+#![feature(result_option_inspect)]
+
+extern crate spin;
+extern crate multiboot2;
+extern crate alloc;
+#[macro_use] extern crate log;
+extern crate irq_safety;
+extern crate kernel_config;
+extern crate atomic_linked_list;
+extern crate xmas_elf;
+extern crate bit_field;
+#[cfg(target_arch = "x86_64")]
+extern crate memory_x86_64;
+extern crate x86_64;
+extern crate memory_structs;
+extern crate page_table_entry;
+extern crate page_allocator;
+extern crate frame_allocator;
+extern crate zerocopy;
+extern crate no_drop;
+
+
+#[cfg(not(mapper_spillful))]
+mod paging;
+
+#[cfg(mapper_spillful)]
+pub mod paging;
+
+pub use self::paging::{
+    PageTable, Mapper, Mutability, Mutable, Immutable,
+    MappedPages, BorrowedMappedPages, BorrowedSliceMappedPages,
+};
+
+pub use memory_structs::{Frame, Page, FrameRange, PageRange, VirtualAddress, PhysicalAddress};
+pub use page_allocator::{AllocatedPages, allocate_pages, allocate_pages_at,
+    allocate_pages_by_bytes, allocate_pages_by_bytes_at};
+
+pub use frame_allocator::{AllocatedFrames, MemoryRegionType, PhysicalMemoryRegion,
+    allocate_frames_by_bytes_at, allocate_frames_by_bytes, allocate_frames_at};
+
+#[cfg(target_arch = "x86_64")]
+use memory_x86_64::{BootInformation, get_kernel_address, get_boot_info_mem_area, find_section_memory_bounds,
+    get_vga_mem_addr, get_modules_address, tlb_flush_virt_addr, tlb_flush_all, get_p4};
+
+#[cfg(target_arch = "x86_64")]
+pub use memory_x86_64::EntryFlags;// Export EntryFlags so that others does not need to get access to memory_<arch>.
+
+use spin::Once;
+use irq_safety::MutexIrqSafe;
+use alloc::vec::Vec;
+use alloc::sync::Arc;
+use no_drop::NoDrop;
+use kernel_config::memory::KERNEL_OFFSET;
+pub use kernel_config::memory::PAGE_SIZE;
+
+/// The memory management info and address space of the kernel
+static KERNEL_MMI: Once<MmiRef> = Once::new();
+
+/// A shareable reference to a `MemoryManagementInfo` struct wrapper in a lock.
+pub type MmiRef = Arc<MutexIrqSafe<MemoryManagementInfo>>;
+
+/// Returns a reference to the kernel's `MemoryManagementInfo`, if initialized.
+/// If not, it returns `None`.
+pub fn get_kernel_mmi_ref() -> Option<&'static MmiRef> {
+    KERNEL_MMI.get()
+}
+
+
+/// This holds all the information for a `Task`'s memory mappings and address space
+/// (this is basically the equivalent of Linux's mm_struct)
+#[derive(Debug)]
+pub struct MemoryManagementInfo {
+    /// the PageTable that should be switched to when this Task is switched to.
+    pub page_table: PageTable,
+    
+    /// a list of additional virtual-mapped Pages that have the same lifetime as this MMI
+    /// and are thus owned by this MMI, but is not all-inclusive (e.g., Stacks are excluded).
+    pub extra_mapped_pages: Vec<MappedPages>,
+}
+
+
+/// A convenience function that creates a new memory mapping by allocating frames that are contiguous in physical memory.
+/// If contiguous frames are not required, then see [`create_mapping()`](fn.create_mapping.html).
+/// Returns a tuple containing the new `MappedPages` and the starting PhysicalAddress of the first frame,
+/// which is a convenient way to get the physical address without walking the page tables.
+/// 
+/// # Locking / Deadlock
+/// Currently, this function acquires the lock on the frame allocator and the kernel's `MemoryManagementInfo` instance.
+/// Thus, the caller should ensure that the locks on those two variables are not held when invoking this function.
+pub fn create_contiguous_mapping(size_in_bytes: usize, flags: EntryFlags) -> Result<(MappedPages, PhysicalAddress), &'static str> {
+    let kernel_mmi_ref = get_kernel_mmi_ref().ok_or("create_contiguous_mapping(): KERNEL_MMI was not yet initialized!")?;
+    let allocated_pages = allocate_pages_by_bytes(size_in_bytes).ok_or("memory::create_contiguous_mapping(): couldn't allocate contiguous pages!")?;
+    let allocated_frames = allocate_frames_by_bytes(size_in_bytes).ok_or("memory::create_contiguous_mapping(): couldn't allocate contiguous frames!")?;
+    let starting_phys_addr = allocated_frames.start_address();
+    let mp = kernel_mmi_ref.lock().page_table.map_allocated_pages_to(allocated_pages, allocated_frames, flags)?;
+    Ok((mp, starting_phys_addr))
+}
+
+
+/// A convenience function that creates a new memory mapping. The pages allocated are contiguous in memory but there's
+/// no guarantee that the frames they are mapped to are also contiguous in memory. If contiguous frames are required
+/// then see [`create_contiguous_mapping()`](fn.create_contiguous_mapping.html).
+/// Returns the new `MappedPages.` 
+/// 
+/// # Locking / Deadlock
+/// Currently, this function acquires the lock on the kernel's `MemoryManagementInfo` instance.
+/// Thus, the caller should ensure that lock is not held when invoking this function.
+pub fn create_mapping(size_in_bytes: usize, flags: EntryFlags) -> Result<MappedPages, &'static str> {
+    let kernel_mmi_ref = get_kernel_mmi_ref().ok_or("create_contiguous_mapping(): KERNEL_MMI was not yet initialized!")?;
+    let allocated_pages = allocate_pages_by_bytes(size_in_bytes).ok_or("memory::create_mapping(): couldn't allocate pages!")?;
+    kernel_mmi_ref.lock().page_table.map_allocated_pages(allocated_pages, flags)
+}
+
+
+static BROADCAST_TLB_SHOOTDOWN_FUNC: Once<fn(PageRange)> = Once::new();
+
+/// Set the function callback that will be invoked every time a TLB shootdown is necessary,
+/// i.e., during page table remapping and unmapping operations.
+pub fn set_broadcast_tlb_shootdown_cb(func: fn(PageRange)) {
+    BROADCAST_TLB_SHOOTDOWN_FUNC.call_once(|| func);
+}
+
+
+
+/// Initializes the virtual memory management system.
+/// Consumes the given BootInformation, because after the memory system is initialized,
+/// the original BootInformation will be unmapped and inaccessible.
+/// 
+/// Returns the following tuple, if successful:
+///  1. the kernel's new `PageTable`, which is now currently active,
+///  2. the `MappedPages` of the kernel's text section,
+///  3. the `MappedPages` of the kernel's rodata section,
+///  4. the `MappedPages` of the kernel's data section,
+///  5. a tuple of the stack's underlying guard page (an `AllocatedPages` instance) and the actual `MappedPages` backing it,
+///  6. the `MappedPages` holding the bootloader info,
+///  7. the kernel's list of *other* higher-half MappedPages that needs to be converted to a vector after heap initialization, and which should be kept forever,
+///  8. the kernel's list of identity-mapped MappedPages that needs to be converted to a vector after heap initialization, and which should be dropped before starting the first userspace program. 
+pub fn init(
+    boot_info: &BootInformation
+) -> Result<(
+    PageTable,
+    NoDrop<MappedPages>,
+    NoDrop<MappedPages>,
+    NoDrop<MappedPages>,
+    (AllocatedPages, NoDrop<MappedPages>),
+    MappedPages,
+    [Option<NoDrop<MappedPages>>; 32],
+    [Option<NoDrop<MappedPages>>; 32],
+), &'static str> {
+    // Get the start and end addresses of the kernel, boot info, boot modules, etc.
+    let (kernel_phys_start, kernel_phys_end, kernel_virt_end) = get_kernel_address(&boot_info)?;
+    let (boot_info_paddr_start, boot_info_paddr_end) = get_boot_info_mem_area(&boot_info)?;
+    let (modules_start_paddr, modules_end_paddr) = get_modules_address(&boot_info);
+    debug!("bootloader info memory: p{:#X} to p{:#X}, bootloader modules: p{:#X} to p{:#X}", 
+        boot_info_paddr_start, boot_info_paddr_end, modules_start_paddr, modules_end_paddr,
+    );
+    debug!("kernel_phys_start: p{:#X}, kernel_phys_end: p{:#X} kernel_virt_end = v{:#x}",
+        kernel_phys_start, kernel_phys_end, kernel_virt_end
+    );
+
+    // In addition to the information about the hardware's physical memory map provided by the bootloader,
+    // Theseus chooses to reserve the following regions of physical memory for specific use.
+    let low_memory_frames   = FrameRange::from_phys_addr(PhysicalAddress::zero(), 0x10_0000); // suggested by most OS developers
+    let kernel_frames       = FrameRange::from_phys_addr(kernel_phys_start, kernel_phys_end.value() - kernel_phys_start.value());
+    let boot_modules_frames = FrameRange::from_phys_addr(modules_start_paddr, modules_end_paddr.value() - modules_start_paddr.value());
+    let boot_info_frames    = FrameRange::from_phys_addr(boot_info_paddr_start, boot_info_paddr_end.value() - boot_info_paddr_start.value());
+    
+    // Add the VGA display's memory region to the list of reserved physical memory areas.
+    // Currently this is covered by the first 1MiB region, but it's okay to duplicate it here.
+    let (vga_start_paddr, vga_size, _vga_flags) = memory_x86_64::get_vga_mem_addr()?;
+    let vga_display_frames = FrameRange::from_phys_addr(vga_start_paddr, vga_size);
+    
+    // Now set up the list of free regions and reserved regions so we can initialize the frame allocator.
+    let mut free_regions: [Option<PhysicalMemoryRegion>; 32] = Default::default();
+    let mut free_index = 0;
+    let mut reserved_regions: [Option<PhysicalMemoryRegion>; 32] = Default::default();
+    let mut reserved_index = 0;
+
+    reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(low_memory_frames, MemoryRegionType::Reserved));
+    reserved_index += 1;
+    reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(kernel_frames, MemoryRegionType::Reserved));
+    reserved_index += 1;
+    reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(boot_modules_frames, MemoryRegionType::Reserved));
+    reserved_index += 1;
+    reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(boot_info_frames, MemoryRegionType::Reserved));
+    reserved_index += 1;
+    reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(vga_display_frames, MemoryRegionType::Reserved));
+    reserved_index += 1;
+
+    for area in boot_info.memory_map_tag()
+        .ok_or("Multiboot2 boot information has no physical memory map information")?
+        .all_memory_areas()
+    {
+        let frames = FrameRange::from_phys_addr(PhysicalAddress::new_canonical(area.start_address() as usize), area.size() as usize);
+        if area.typ() == multiboot2::MemoryAreaType::Available {
+            free_regions[free_index] = Some(PhysicalMemoryRegion::new(frames, MemoryRegionType::Free));
+            free_index += 1;
+        } else {
+            reserved_regions[reserved_index] = Some(PhysicalMemoryRegion::new(frames, MemoryRegionType::Reserved));
+            reserved_index += 1;
+        }
+    }
+
+    let into_alloc_frames_fn = frame_allocator::init(free_regions.iter().flatten(), reserved_regions.iter().flatten())?;
+    debug!("Initialized new frame allocator!");
+    frame_allocator::dump_frame_allocator_state();
+
+    page_allocator::init(VirtualAddress::new_canonical(kernel_phys_end.value()))?;
+    debug!("Initialized new page allocator!");
+    page_allocator::dump_page_allocator_state();
+
+    // Initialize paging, which creates a new page table and maps all of the current code/data sections into it.
+    paging::init(boot_info, into_alloc_frames_fn)
+        .inspect(|(new_page_table, ..)| {
+            debug!("Done with paging::init(). new page table: {:?}", new_page_table);
+        })
+}
+
+/// Finishes initializing the memory management system after the heap is ready.
+/// 
+/// Returns the following tuple:
+///  * The kernel's new [`MemoryManagementInfo`], representing the initial virtual address space,
+///  * The kernel's list of identity-mapped [`MappedPages`],
+///    which must not be dropped until all AP (additional CPUs) are fully booted,
+///    but *should* be dropped before starting the first user application. 
+pub fn init_post_heap(
+    page_table: PageTable,
+    mut higher_half_mapped_pages: [Option<NoDrop<MappedPages>>; 32],
+    mut identity_mapped_pages: [Option<NoDrop<MappedPages>>; 32],
+    heap_mapped_pages: MappedPages
+) -> (MmiRef, NoDrop<Vec<MappedPages>>) {
+    // HERE: heap is initialized! We can now use `alloc` types.
+
+    page_allocator::convert_to_heap_allocated();
+    frame_allocator::convert_to_heap_allocated();
+
+    let mut higher_half_mapped_pages: Vec<MappedPages> = higher_half_mapped_pages
+        .iter_mut()
+        .filter_map(|opt| opt.take().map(NoDrop::into_inner))
+        .collect();
+    higher_half_mapped_pages.push(heap_mapped_pages);
+    let identity_mapped_pages: Vec<MappedPages> = identity_mapped_pages
+        .iter_mut()
+        .filter_map(|opt| opt.take().map(NoDrop::into_inner))
+        .collect();
+    let identity_mapped_pages = NoDrop::new(identity_mapped_pages);
+   
+    // Construct the kernel's memory mgmt info, i.e., its address space info
+    let kernel_mmi = MemoryManagementInfo {
+        page_table,
+        extra_mapped_pages: higher_half_mapped_pages,
+    };
+
+    let kernel_mmi_ref = KERNEL_MMI.call_once( || {
+        Arc::new(MutexIrqSafe::new(kernel_mmi))
+    });
+
+    (kernel_mmi_ref.clone(), identity_mapped_pages)
+}
---
+++ aarch64/kernel/memory/src/paging/table.rs
@@ -0,0 +1,142 @@
+// Copyright 2016 Philipp Oppermann. See the README.md
+// file at the top-level directory of this distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use super::PageTableEntry;
+use kernel_config::memory::{PAGE_SHIFT, ENTRIES_PER_PAGE_TABLE};
+use super::super::{VirtualAddress, EntryFlags};
+use core::ops::{Index, IndexMut};
+use core::marker::PhantomData;
+use zerocopy::FromBytes;
+
+
+/// Theseus uses the 511th entry of the P4 table for mapping the higher-half kernel, 
+/// so it uses the 510th entry of P4 for the recursive mapping.
+/// 
+/// NOTE: this must be kept in sync with the recursive index in `kernel_config/memory.rs`
+///       and `nano_core/<arch>/boot.asm`.
+///
+/// See these links for more: 
+/// * <http://forum.osdev.org/viewtopic.php?f=1&p=176913>
+/// * <http://forum.osdev.org/viewtopic.php?f=15&t=25545>
+pub const P4: *mut Table<Level4> = 0o177777_776_776_776_776_0000 as *mut _; 
+                                         // ^p4 ^p3 ^p2 ^p1 ^offset  
+                                         // ^ 0o776 means that we're always looking at the 510th entry recursively
+
+#[derive(FromBytes)]
+pub struct Table<L: TableLevel> {
+    entries: [PageTableEntry; ENTRIES_PER_PAGE_TABLE],
+    level: PhantomData<L>,
+}
+
+impl<L: TableLevel> Table<L> {
+    /// Zero out (clear) all entries in this page table frame. 
+    pub(crate) fn zero(&mut self) {
+        for entry in self.entries.iter_mut() {
+            entry.zero();
+        }
+    }
+}
+
+impl<L: HierarchicalLevel> Table<L> {
+    /// Uses the given `index` as an index into this table's list of entries.
+    ///
+    /// Returns the virtual address of the next lowest page table:
+    /// if `self` is a P4-level `Table`, then this returns a P3-level `Table`,
+    /// and so on for P3 -> P3 and P2 -> P1.
+    fn next_table_address(&self, index: usize) -> Option<VirtualAddress> {
+        let entry_flags = self[index].flags();
+        if entry_flags.contains(EntryFlags::PRESENT) && !entry_flags.is_huge() {
+            let table_address = self as *const _ as usize;
+            let next_table_vaddr: usize = (table_address << 9) | (index << PAGE_SHIFT);
+            Some(VirtualAddress::new_canonical(next_table_vaddr))
+        } else {
+            None
+        }
+    }
+
+    /// Returns a reference to the next lowest-level page table.
+    /// 
+    /// A convenience wrapper around `next_table_address()`; see that method for more.
+    pub fn next_table(&self, index: usize) -> Option<&Table<L::NextLevel>> {
+        // convert the next table address from a raw pointer back to a Table type
+        self.next_table_address(index).map(|vaddr| unsafe { &*(vaddr.value() as *const _) })
+    }
+
+    /// Returns a mutable reference to the next lowest-level page table.
+    /// 
+    /// A convenience wrapper around `next_table_address()`; see that method for more.
+    pub fn next_table_mut(&mut self, index: usize) -> Option<&mut Table<L::NextLevel>> {
+        self.next_table_address(index).map(|vaddr| unsafe { &mut *(vaddr.value() as *mut _) })
+    }
+
+    /// Returns a mutable reference to the next lowest-level page table, 
+    /// creating and initializing a new one if it doesn't already exist.
+    /// 
+    /// A convenience wrapper around `next_table_address()`; see that method for more.
+    ///
+    /// TODO: return a `Result` here instead of panicking.
+    pub fn next_table_create(
+        &mut self,
+        index: usize,
+        flags: EntryFlags,
+    ) -> &mut Table<L::NextLevel> {
+        if self.next_table(index).is_none() {
+            assert!(!self[index].flags().is_huge(), "mapping code does not support huge pages");
+            let af = frame_allocator::allocate_frames(1).expect("next_table_create(): no frames available");
+            self[index].set_entry(af.as_allocated_frame(), flags.into_writable() | EntryFlags::PRESENT); // must be PRESENT | WRITABLE for x86_64
+            self.next_table_mut(index).unwrap().zero();
+            core::mem::forget(af); // we currently forget frames allocated as page table frames since we don't yet have a way to track them.
+        }
+        self.next_table_mut(index).unwrap()
+    }
+}
+
+impl<L: TableLevel> Index<usize> for Table<L> {
+    type Output = PageTableEntry;
+
+    fn index(&self, index: usize) -> &PageTableEntry {
+        &self.entries[index]
+    }
+}
+
+impl<L: TableLevel> IndexMut<usize> for Table<L> {
+    fn index_mut(&mut self, index: usize) -> &mut PageTableEntry {
+        &mut self.entries[index]
+    }
+}
+
+pub trait TableLevel {}
+
+pub enum Level4 {}
+#[allow(dead_code)]
+pub enum Level3 {}
+#[allow(dead_code)]
+pub enum Level2 {}
+pub enum Level1 {}
+
+impl TableLevel for Level4 {}
+impl TableLevel for Level3 {}
+impl TableLevel for Level2 {}
+impl TableLevel for Level1 {}
+
+pub trait HierarchicalLevel: TableLevel {
+    type NextLevel: TableLevel;
+}
+
+impl HierarchicalLevel for Level4 {
+    type NextLevel = Level3;
+}
+
+impl HierarchicalLevel for Level3 {
+    type NextLevel = Level2;
+}
+
+impl HierarchicalLevel for Level2 {
+    type NextLevel = Level1;
+}
