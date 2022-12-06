8f91743550f6e982db990763bb955c32bed9889d
Klim Tsoutsman <klim@tsoutsman.com>
2022-12-06 18:52:32 +1100

    Add test application
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- applications/test_async/src/lib.rs
+++ applications/test_async/src/lib.rs
@@ -4,10 +4,25 @@ extern crate alloc;
 
 use alloc::{string::String, vec::Vec};
 use app_io::println;
+use dreadnought::{execute, FutureExt};
 
 pub fn main(_: Vec<String>) -> isize {
-    dreadnought::execute(async {
+    execute(async {
         println!("Hello, asynchronous world!");
     });
+
+    execute(async {
+        let result = dreadnought::select_biased! {
+            result = dreadnought::future::pending() => result,
+            result = foo().fuse() => result,
+        };
+        assert_eq!(result, 42);
+    });
+
     0
+}
+
+async fn foo() -> u8 {
+    println!("called foo");
+    42
 }
--- kernel/dreadnought/src/lib.rs
+++ kernel/dreadnought/src/lib.rs
@@ -2,18 +2,22 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, sync::Arc, task::Wake};
+use alloc::{sync::Arc, task::Wake};
 use core::{
     future::Future,
     task::{Context, Poll},
 };
 use spin::Mutex;
 
+pub use futures::{future, select_biased, FutureExt};
+
+/// Executes a future.
 pub fn execute<F, O>(future: F) -> O
 where
     F: Future<Output = O>,
 {
-    let mut future = Box::pin(future);
+    // Pin the future onto the stack. This works because we don't send it anywhere.
+    futures::pin_mut!(future);
     let activated = Arc::new(Mutex::new(false));
     let task = task::get_my_current_task().unwrap();
     let waker = core::task::Waker::from(Arc::new(Waker {
@@ -40,7 +44,13 @@ where
     }
 }
 
-pub struct Waker {
+/// A waker that unblocks the given task when awoken.
+struct Waker {
+    /// Whether the waker has been activated.
+    ///
+    /// This field ensures `execute` detects if the waker was activated prior to
+    /// `execute` blocking the task. The field cannot be an atomic as the lock
+    /// must be held while blocking or unblocking the task.
     activated: Arc<Mutex<bool>>,
     task: task::TaskRef,
 }
