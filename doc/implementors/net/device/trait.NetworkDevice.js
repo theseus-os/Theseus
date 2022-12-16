181f513cdb43644222a87188127a7cba0795bce3
kevinaboos <kevinaboos@users.noreply.github.com>
2022-12-16 07:08:02 +0000

    Implement `net::NetworkDevice` for `IxgbeNic` (#738)
    * Allows the ixgbe driver to be used with the newer
      network abstractions.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com> 8e042f24e8de150f817cb558681b0098625c1f85

--- doc/implementors/net/device/trait.NetworkDevice.js
+++ doc/implementors/net/device/trait.NetworkDevice.js
@@ -1,3 +1,4 @@
 (function() {var implementors = {
-"e1000":[["impl NetworkDevice for <a class=\"struct\" href=\"e1000/struct.E1000Nic.html\" title=\"struct e1000::E1000Nic\">E1000Nic</a>"]]
+"e1000":[["impl NetworkDevice for <a class=\"struct\" href=\"e1000/struct.E1000Nic.html\" title=\"struct e1000::E1000Nic\">E1000Nic</a>"]],
+"ixgbe":[["impl NetworkDevice for <a class=\"struct\" href=\"ixgbe/struct.IxgbeNic.html\" title=\"struct ixgbe::IxgbeNic\">IxgbeNic</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
