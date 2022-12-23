1d17aa1e4f5fd70d8bb586c77605f8747c333ac8
kevinaboos <kevinaboos@users.noreply.github.com>
2022-12-23 00:40:39 +0000

    Implement `time::ClockSource` for the`Hpet` (#739)
    * Implement `time::ClockSource` for `HpetTimer`
    
    Implementing the trait revealed some flaws in the design of the `time`
    crate:
    - Using the period is easier than using the frequency
    - The `duration_to_instant` and `instant_to_duration` functions aren't
      necessary because we know the period/frequency.
    - `Instant` was missing a `new` function.
    
    This commit also adds `seconds_counter` as a simple test that the
    monotonic clock is working correctly.
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    
    * Update documentation
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    
    * Use reference returned by `init_hpet`
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    
    * Use `fetch_update` when registering clock sources
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    
    * Deduplicate `Instant::duration_since`
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>
    Co-authored-by: Kevin Boos <kevinaboos@gmail.com> 5a3b1cc5a8beb1d08fe44f30f4cb6f684dc309d9

---
+++ doc/implementors/time/trait.ClockSource.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"hpet":[["impl ClockSource for <a class=\"struct\" href=\"hpet/struct.Hpet.html\" title=\"struct hpet::Hpet\">Hpet</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
