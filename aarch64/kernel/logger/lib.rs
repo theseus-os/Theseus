d78006dcf093c318fb8b5e7d39b7903a60b5f2d4
Nathan Royer <nathan.royer.pro@gmail.com>
2023-01-02 10:22:10 +0100

    Minimal doc for `logger` on aarch64

--- aarch64/kernel/logger/lib.rs
+++ aarch64/kernel/logger/lib.rs
@@ -7,12 +7,14 @@ use irq_safety::MutexIrqSafe;
 
 type QemuVirtUart = PL011<UART1>;
 
-pub struct Logger {
+/// This wraps a UART channel handle.
+pub struct QemuVirtUartLogger {
     pub(crate) uart: MutexIrqSafe<QemuVirtUart>,
 }
 
-impl Log for Logger {
+impl Log for QemuVirtUartLogger {
     fn enabled(&self, _metadata: &Metadata) -> bool {
+        // allow all messages
         true
     }
 
@@ -20,6 +22,9 @@ impl Log for Logger {
         let mut mutable_uart = self.uart.lock();
 
         if self.enabled(record.metadata()) {
+            // result is discarded because we
+            // have no alternative way to signal
+            // an issue to the user
             let _ = write!(&mut mutable_uart, "{} - {}\r\n", record.level(), record.args());
         }
     }
@@ -27,13 +32,21 @@ impl Log for Logger {
     fn flush(&self) {}
 }
 
-static mut LOGGER: Option<Logger> = None;
+// Global logger Singleton
+static mut LOGGER: Option<QemuVirtUartLogger> = None;
 
+/// Initialize the internal global "LOGGER" singleton
+/// and sets it as the system-wide logger for the `log`
+/// crate.
+///
+/// Bootstrapping code must call this as early
+/// as possible for all log messages to show up
+/// on the UART output of Qemu.
 pub fn init() -> Result<(), &'static str> {
     set_max_level(STATIC_MAX_LEVEL);
 
     let uart1 = UART1::take().unwrap();
-    let logger = Logger {
+    let logger = QemuVirtUartLogger {
         uart: MutexIrqSafe::new(QemuVirtUart::new(uart1)),
     };
 
