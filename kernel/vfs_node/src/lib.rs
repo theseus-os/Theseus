6beb91042db78d6a7f22a98f170dfc0846967eee
amab8901 <amab8901@protonmail.com>
2023-01-09 09:57:00 +0100

    Reverse #[allow(clippy::new_ret_no_self)]

--- kernel/vfs_node/src/lib.rs
+++ kernel/vfs_node/src/lib.rs
@@ -32,7 +32,6 @@ pub struct VFSDirectory {
 
 impl VFSDirectory {
     /// Creates a new directory and passes a pointer to the new directory created as output
-    #[allow(clippy::new_ret_no_self)]
     pub fn new(name: String, parent: &DirRef)  -> Result<DirRef, &'static str> {
         // creates a copy of the parent pointer so that we can add the newly created folder to the parent's children later
         let directory = VFSDirectory {
