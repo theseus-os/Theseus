c0f00b933208197b53469eb15c4c90222482e911
amab8901 <amab8901@protonmail.com>
2023-01-11 11:56:24 +0100

    Fix comparison_chain lint

--- applications/deps/src/lib.rs
+++ applications/deps/src/lib.rs
@@ -406,13 +406,16 @@ fn find_crate(crate_name: &str) -> Result<(StrRef, StrongCrateRef), String> {
 fn find_section(section_name: &str) -> Result<StrongSectionRef, String> {
     let namespace = get_my_current_namespace();
     let matching_symbols = namespace.find_symbols_starting_with(section_name);
-    if matching_symbols.len() == 1 {
-        return matching_symbols[0].1.upgrade()
+    
+    match matching_symbols.len() {
+        1 => {
+            return matching_symbols[0].1.upgrade()
             .ok_or_else(|| format!("Found matching symbol name but couldn't get reference to section"));
-    } else if matching_symbols.len() > 1 {
-        return Err(matching_symbols.into_iter().map(|(k, _v)| k).collect::<Vec<String>>().join("\n"));
-    } else {
-        // continue on
+        },
+        2.. => {
+            return Err(matching_symbols.into_iter().map(|(k, _v)| k).collect::<Vec<String>>().join("\n"));
+        },
+        _ => {},
     }
 
     // If it wasn't a global section in the symbol map, then we need to find its containing crate
