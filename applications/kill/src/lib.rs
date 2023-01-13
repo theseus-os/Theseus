7a857a71b1f111daed3505f36bc72c749a57aa6e
amab8901 <amab8901@protonmail.com>
2023-01-13 11:49:48 +0100

    Fix CI lint

--- applications/kill/src/lib.rs
+++ applications/kill/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 extern crate alloc;
 #[macro_use] extern crate app_io;
-// #[macro_use] extern crate debugit;
 
 extern crate task;
 extern crate runqueue;
@@ -35,79 +34,9 @@ pub fn main(args: Vec<String>) -> isize {
     return -1;
 }
 
-/*
-    let reap = matches.opt_present("r");
-
-    for task_id_str in matches.free.iter() {
-        match task_id_str.parse::<usize>(){
-            Ok(task_id) => {
-                match kill_task(task_id, reap) {
-                    Ok(_) => { }
-                    Err(e) => {
-                        println!("{}", e);
-                        return -1;
-                    }
-                }
-            }, 
-            _ => { 
-                println!("Invalid argument {}, not a valid task ID (usize)", task_id_str); 
-                return -1;
-            },
-        };   
-    }
-    0
-}
-
-
-fn kill_task(task_id: usize, reap: bool) -> Result<(), String> {
-    if let Some(task_ref) = task::get_task(task_id) {
-        if task_ref.kill(task::KillReason::Requested)
-            .and_then(|_| runqueue::remove_task_from_all(&task_ref))
-            .is_ok() 
-        {
-            println!("Killed task {}", &*task_ref);
-            if reap {
-                match task_ref.take_exit_value() {
-                    Some(exit_val) => { 
-                        println!("Reaped task {}, got exit value {}", task_id, debugit!(exit_val));
-                        Ok(())
-                    }
-                    _ => {
-                        Err(format!("Failed to reap task {}", task_id))
-                    }
-                }
-            } 
-            else {
-                // killed the task successfully, no reap request, so return success.
-                Ok(())
-            }
-        }
-        else if reap {
-            // if we failed to kill the task, but it was a reap request, then reap it anyway.
-            match task_ref.take_exit_value() {
-                Some(exit_val) => {
-                    println!("Reaped task {}, got exit value {}", task_id, debugit!(exit_val));
-                    Ok(())
-                }
-                _ => {
-                    Err(format!("Failed to reap task {}", task_id))
-                }
-            }
-        }
-        else {
-            // failed to kill the task, no reap request, so return failure.
-            Err(format!("Failed to kill task {}, it was already exited.", task_id))
-        }
-    }
-    else {
-        Err(format!("Task ID {} does not exist", task_id))
-    }
-}
-*/
-
 #[allow(dead_code)]
 fn print_usage(opts: Options) -> isize {
-    let brief = format!("Usage: kill [OPTS] TASK_ID");
+    let brief = "Usage: kill [OPTS] TASK_ID".to_string();
     println!("{}", opts.usage(&brief));
     0
 }
