dcbc0daa9619275b94918679d8416832f16c448c
Kevin Boos <kevinaboos@gmail.com>
2023-01-17 10:18:40 +0800

    allow useless conversion

--- kernel/pmu_x86/src/lib.rs
+++ kernel/pmu_x86/src/lib.rs
@@ -705,6 +705,7 @@ pub fn start_samples(event_type: EventType, event_per_sample: u32, task_id: Opti
     // This check can never trigger since `event_per_sample` is a `u32`
     // and is therefore by definition in the range `u32::MIN..=u32::MAX`.
     // We'll check anyways, just in case `event_per_sample`'s type is changed.
+    #[allow(clippy::useless_conversion)]
     if u32::try_from(event_per_sample).is_err() {
         return Err("Number of events per sample invalid: must be within unsigned 32 bit");
     }
