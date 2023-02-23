15ad092ad4ae746076f79ad0868bd411ef29ecdd
Klim Tsoutsman <klim@tsoutsman.com>
2023-02-23 19:32:31 +1100

    Add top-level crate documentation
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- kernel/idle/src/lib.rs
+++ kernel/idle/src/lib.rs
@@ -1,3 +1,9 @@
+//! CPU idle management.
+//!
+//! Currently, this crate is incomplete. In future it will provide an idle loop
+//! which dynamically selects a sleep state for the CPU based on a set of
+//! heuristics.
+
 #![no_std]
 
 mod arch;
