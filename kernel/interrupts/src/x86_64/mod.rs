22a88a00383cb6445932988711b5fae9b18705f9
Nathan Royer <61582713+NathanRoyer@users.noreply.github.com>
2023-02-27 08:31:41 +0100

    aarch64: support interrupts via GIC, plus generic timer (#727)
    * Adds basic support for handling interrupts on aarch64. 
    
    * Supports ARMv8 Generic Interrupt Controller (GIC),
      both versions 2 and 3, but is currently hardcoded for v3
      which is selected in QEMU by our Makefile.
      Also supports GIC distributor and redistributor interfaces.
    
    * Only uses/supports (non-secure) Group 1 interrupts (IRQs),
      secure-mode Group 1 interrupts or Group 0 interrupts (e.g., FIQs).
    
    * Uses the existing interface on x86_64 as a model for
      accessing the GIC and registering/deregistering interrupts,
      though the interface is a bit ad-hoc and not yet complete.

--- kernel/interrupts/src/x86_64/mod.rs
+++ kernel/interrupts/src/x86_64/mod.rs
@@ -1,10 +1,3 @@
-//! Basic interrupt handling structures and simple handler routines.
-
-#![no_std]
-#![feature(abi_x86_interrupt)]
-
-#![allow(dead_code)]
-
 pub use pic::IRQ_BASE_OFFSET;
 
 use x86_64::structures::idt::{InterruptStackFrame, HandlerFunc};
