aed9ac0de7f35228883595adafa0d08789b8ff12
Nathan Royer <nathan.royer.pro@gmail.com>
2023-04-04 17:29:51 +0200

    Rename default arm_boards file to `unselected.rs`

--- kernel/arm_boards/src/unselected.rs
+++ kernel/arm_boards/src/unselected.rs
@@ -1,6 +1,7 @@
-//! Default board config file.
+//! Invalid board config file, selected by default if you didn't select one.
 //!
 //! This will result in a compile-time error if used in an aarch64 build.
+//! On x86_64 it's perfectly OK though.
 
 #[cfg(target_arch = "aarch64")]
 compile_error!("Please select a board config feature in the arm_boards crate");
