a70c795a561c5cc776dbe06a87f1b1497c7d193e
Klim Tsoutsman <klim@tsoutsman.com>
2023-04-07 12:37:07 +1000

    Add new `sync` API and basic sync primitives (#922)
    * Introduce a core `Mutex` primitive that allows for
      abstract "augmented" lock/unlock behaviors.
    
    *All sync primitives now have a generic type parameter
      that implements `sync::Flavour`, which modify how the 
      sync primitive behaves before locking and after unlocking.
      Current variants include:
      * `sync_spin`: simply spins with no extra behavior
        * This will replace `spin::Mutex`.
      * `sync_preemption`: disables preemption
        * This will replace `mutex_preempt`
      * `sync_irq`: disabling interrupts
        * This will replace `irq_safety::MutexIrqSafe`
      * blocking the current task
        * This will replace `MutexSleep`
    
    * `DeadlockPrevention` is a more limited trait implemented for:
      * simple spinlocks (effectively a no-op)
      * disabling preemption
      * disabling interrupts
    
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

---
+++ kernel/sync_block/src/lib.rs
@@ -0,0 +1,50 @@
+#![feature(negative_impls)]
+#![no_std]
+
+use sync::{mutex, Flavour};
+use wait_queue::WaitQueue;
+
+/// A synchronisation flavour that blocks the current thread while waiting for
+/// the lock to become available.
+#[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Block {}
+
+impl Flavour for Block {
+    // #[allow(clippy::declare_interior_mutable_const)]
+    // const INIT: Self::LockData = WaitQueue::new();
+
+    type LockData = WaitQueue;
+
+    type Guard = ();
+
+    fn new() -> Self::LockData {
+        WaitQueue::new()
+    }
+
+    #[inline]
+    fn try_lock_mutex<'a, T>(
+        mutex: &'a mutex::SpinMutex<T>,
+        _: &'a Self::LockData,
+    ) -> Option<(mutex::SpinMutexGuard<'a, T>, Self::Guard)> {
+        mutex.try_lock().map(|guard| (guard, ()))
+    }
+
+    #[inline]
+    fn lock_mutex<'a, T>(
+        mutex: &'a mutex::SpinMutex<T>,
+        data: &'a Self::LockData,
+    ) -> (mutex::SpinMutexGuard<'a, T>, Self::Guard) {
+        // This must be a strong compare exchange, otherwise we could block ourselves
+        // when the mutex is unlocked and never be unblocked.
+        if let Some(guards) = Self::try_lock_mutex(mutex, data) {
+            guards
+        } else {
+            data.wait_until(&|| Self::try_lock_mutex(mutex, data)).unwrap()
+        }
+    }
+
+    #[inline]
+    fn post_unlock(data: &Self::LockData) {
+        data.notify_one();
+    }
+}
