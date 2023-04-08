38d1723a46f5e62784c560272cac7f66e9694915
Klim Tsoutsman <klim@tsoutsman.com>
2023-04-08 10:02:40 +1000

    Update `wait_guard` docs
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- kernel/wait_guard/src/lib.rs
+++ kernel/wait_guard/src/lib.rs
@@ -13,22 +13,22 @@ impl WaitGuard {
     /// automatically unblock that Task when it is dropped.
     ///
     /// Returns an error if the task cannot be blocked; see
-    /// [`task::Task::block()`] for more details.
+    /// [`task::Task::block`] for more details.
     pub fn new(task: TaskRef) -> Result<Self, RunState> {
         task.block()?;
         Ok(WaitGuard { task })
     }
 
-    /// Blocks the task guarded by this waitguard, which is useful to re-block a
-    /// task after it spuriously woke up.
+    /// Blocks the task guarded by this guard, which is useful to re-block a
+    /// task after it spuriously woken up.
     ///
     /// Returns an error if the task cannot be blocked; see
-    /// [`task::Task::block()`] for more details.
+    /// [`task::Task::block`] for more details.
     pub fn block_again(&self) -> Result<RunState, RunState> {
         self.task.block()
     }
 
-    /// Returns a reference to the `Task` being blocked in this `WaitGuard`.
+    /// Returns a reference to the task being blocked by this guard.
     pub fn task(&self) -> &TaskRef {
         &self.task
     }
@@ -36,6 +36,6 @@ impl WaitGuard {
 
 impl Drop for WaitGuard {
     fn drop(&mut self) {
-        self.task.unblock().unwrap();
+        let _ = self.task.unblock();
     }
 }
