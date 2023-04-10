faeb1abfd765466f2dd9b195d7363bb4ab5630de
Klim Tsoutsman <klim@tsoutsman.com>
2023-04-10 21:23:02 +1000

    Add `allocate_in_cls`
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- kernel/cls/src/lib.rs
+++ kernel/cls/src/lib.rs
@@ -11,23 +11,42 @@ use sync_spin::Mutex;
 
 pub use cls_proc::cpu_local;
 
-pub fn init(_cpu_id: CpuId) -> Result<(), ()> {
-    // TODO: Check it isn't being initialised twice.
-    static CPU_LOCAL_DATA_REGIONS: Mutex<BTreeMap<u32, u32>> =
-        Mutex::new(BTreeMap::new());
+const CLS_SIZE: usize = 4096;
 
-    let data_region =
-        memory::create_mapping(4096, PteFlags::new().writable(true).valid(true)).map_err(|_| ())?;
+static CPU_LOCAL_DATA_REGIONS: Mutex<BTreeMap<CpuId, u32>> = Mutex::new(BTreeMap::new());
+
+pub fn init(cpu_id: CpuId) -> Result<(), ()> {
+    // TODO: Check it isn't being initialised twice.
+
+    CPU_LOCAL_DATA_REGIONS.lock().insert(cpu_id, 0);
+
+    let data_region = memory::create_mapping(CLS_SIZE, PteFlags::new().writable(true).valid(true))
+        .map_err(|_| ())?;
     let pointer = data_region.start_address().value() as *mut u8;
 
     // TODO: Support custom initialisers.
-    unsafe { core::ptr::write_bytes::<u8>(pointer, 0, 4096) };
+    unsafe { core::ptr::write_bytes::<u8>(pointer, 0, CLS_SIZE) };
 
     arch::set_cls_register(data_region.start_address());
 
     Ok(())
 }
 
-pub fn allocate_cls(cpu_id: CpuId) -> Result<(), ()> {
-    todo!();
+/// Allocates a region in the CLS.
+///
+/// Returns the offset into the CLS at which the regions starts.
+pub fn allocate_in_cls(cpu_id: CpuId, size: usize) -> Result<usize, ()> {
+    let mut data_regions = CPU_LOCAL_DATA_REGIONS.lock();
+    let offset = data_regions.get_mut(&cpu_id).ok_or(())?;
+
+    let free_bytes = CLS_SIZE - *offset as usize;
+
+    if size > free_bytes {
+        return Err(());
+    }
+
+    let current_offset = *offset as usize;
+    *offset += size as u32;
+
+    Ok(current_offset)
 }
