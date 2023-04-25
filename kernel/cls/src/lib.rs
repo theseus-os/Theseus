39812133b09b7200da898f3be7f7b1a905746752
Klim Tsoutsman <klim@tsoutsman.com>
2023-04-25 15:36:28 +1000

    Temp
    Signed-off-by: Klim Tsoutsman <klim@tsoutsman.com>

--- kernel/cls/src/lib.rs
+++ kernel/cls/src/lib.rs
@@ -29,9 +29,7 @@ impl State {
     }
 }
 
-pub fn init(_cpu_id: CpuId) -> Result<(), ()> {
-    // TODO: Check CLS isn't being initialised twice.
-
+pub fn init(cpu_id: CpuId) -> Result<(), ()> {
     let data_region = memory::create_mapping(CLS_SIZE, PteFlags::new().writable(true).valid(true))
         .map_err(|_| ())?;
     let pointer = data_region.start_address().value() as *mut u8;
@@ -39,7 +37,12 @@ pub fn init(_cpu_id: CpuId) -> Result<(), ()> {
     // TODO: Support custom initialisers.
     unsafe { core::ptr::write_bytes::<u8>(pointer, 0, CLS_SIZE) };
 
+
+    let mut state = STATE.lock();
+    assert_eq!(state.bytes_used, 0);
+    // TODO: Check CLS isn't being initialised twice.
     arch::set_cls_register(data_region.start_address());
+    state.data_regions.insert(cpu_id, data_region);
 
     Ok(())
 }
