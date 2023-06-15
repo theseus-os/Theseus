87a30097998e1075a3805082f4f1f02987d3c425
ramla-i <ijazramla@gmail.com>
2023-06-15 11:31:04 -0400

    comments

--- libs/range_inclusive/src/lib.rs
+++ libs/range_inclusive/src/lib.rs
@@ -12,41 +12,48 @@ mod test;
 use core::iter::Step;
 use core::ops::{RangeBounds, Bound, Bound::Included};
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct RangeInclusive<Idx: Clone + PartialOrd> {
     pub(crate) start: Idx,
     pub(crate) end: Idx
 }
 
 impl<Idx: Clone + PartialOrd> RangeInclusive<Idx> {
+    /// Creates a new inclusive range.
     #[inline]
     pub const fn new(start: Idx, end: Idx) -> Self {
         Self{ start, end }
     }
 
+    /// Returns the lower bound of the range (inclusive).
     #[inline]
     pub const fn start(&self) -> &Idx {
         &self.start
     }
 
+    /// Returns the upper bound of the range (inclusive).
     #[inline]
     pub const fn end(&self) -> &Idx {
         &self.end
     }
 
+    /// Destructures the `RangeInclusive` into (lower bound, upper (inclusive) bound).
     #[inline]
     pub fn into_inner(self) -> (Idx, Idx) {
         (self.start, self.end)
     }
 
+    /// Returns `true` if the range contains no items.
     pub fn is_empty(&self) -> bool {
         !(self.start <= self.end)
     }
 
+    /// Returns an iterator with the same `start` and `end` values as the range.
     pub fn iter(&self) -> RangeInclusiveIterator<Idx> {
         RangeInclusiveIterator { offset: self.start.clone(), end: self.end.clone() }
     }
 
+    /// Returns `true` if `item` is contained in the range.
     pub fn contains<U>(&self, item: &U) -> bool
     where
         Idx: PartialOrd<U>,
@@ -74,8 +81,12 @@ impl<'a, Idx: Clone + PartialOrd + Step> IntoIterator for &'a RangeInclusive<Idx
     }
 }
 
+/// An iterator for the `RangeInclusive` type.
+/// By creating a separate iterator, we don't need to mutate the original range.
 pub struct RangeInclusiveIterator<Idx> {
-    offset: Idx,
+    /// The current value of the iterator which is returned by `next()`.
+    current: Idx,
+    /// The end value of the iterator.
     end: Idx
 }
 
@@ -83,11 +94,11 @@ impl<A: Step> Iterator for RangeInclusiveIterator<A> {
     type Item = A;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.offset > self.end {
+        if self.current > self.end {
             None
         } else {
-            let n = Step::forward_checked(self.offset.clone(), 1).expect("`Step` invariants not upheld");
-            Some(core::mem::replace(&mut self.offset, n))
+            let n = Step::forward_checked(self.current.clone(), 1).expect("`Step` invariants not upheld");
+            Some(core::mem::replace(&mut self.current, n))
         }
     }
 }
