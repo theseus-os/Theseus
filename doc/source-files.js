49291c90bdb7085cef924cdb04e79f2c1646cc95
kevinaboos <kevinaboos@users.noreply.github.com>
2023-06-21 18:15:11 +0000

    Rename `scheduler_priority` to `scheduler_epoch`, plus cleanup (#980)
    Preface to a future PR that renames `scheduler_realtime` to
    `scheduler_priority`, and to a series of PRs that revamp schedulers.
    
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com> e6e255f2f8bff2d0fdd6e6ccba362c36b93ab5e0

--- doc/source-files.js
+++ doc/source-files.js
@@ -131,11 +131,11 @@ var sourcesIndex = JSON.parse('{\
 "rsdt":["",[],["lib.rs"]],\
 "rtc":["",[],["lib.rs"]],\
 "runqueue":["",[],["lib.rs"]],\
-"runqueue_priority":["",[],["lib.rs"]],\
+"runqueue_epoch":["",[],["lib.rs"]],\
 "runqueue_realtime":["",[],["lib.rs"]],\
 "runqueue_round_robin":["",[],["lib.rs"]],\
 "scheduler":["",[],["lib.rs"]],\
-"scheduler_priority":["",[],["lib.rs"]],\
+"scheduler_epoch":["",[],["lib.rs"]],\
 "scheduler_realtime":["",[],["lib.rs"]],\
 "scheduler_round_robin":["",[],["lib.rs"]],\
 "sdt":["",[],["lib.rs"]],\
