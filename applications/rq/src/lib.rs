0f2a721939a79c8160f15edba09cf58a1de40d82
Klimenty Tsoutsman <klim@tsoutsman.com>
2023-07-04 13:12:15 +1000

    Clone run queue in `rq` app
    Avoids disabling preemption for the duration of the program which can
    interfere with printing to the terminal.
    
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com>

--- applications/rq/src/lib.rs
+++ applications/rq/src/lib.rs
@@ -42,7 +42,7 @@ pub fn main(args: Vec<String>) -> isize {
 
         println!("\n{} (apic: {}, proc: {})", core_type, apic_id, processor); 
         
-        if let Some(runqueue) = runqueue::get_runqueue(apic_id.value() as u8).map(|rq| rq.read()) {
+        if let Some(runqueue) = runqueue::get_runqueue(apic_id.value() as u8).map(|rq| rq.read().clone()) {
             let mut runqueue_contents = String::new();
             for task in runqueue.iter() {
                 writeln!(runqueue_contents, "    {} ({}) {}", 
