5c269ad26f51c7bd64bdba86f575f02bd9d2f656
Klim Tsoutsman <klim@tsoutsman.com>
2023-07-07 05:57:05 +1000

    `hull` shell: add operators (`&`, `|`), jobs, history, and more (#994)
    `hull` is now almost feature complete.
    
    Not quite ready to replace the existing `shell` app,
    but is implemented better and more canonically than `shell`.
    
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com>

--- applications/hull/src/error.rs
+++ applications/hull/src/error.rs
@@ -1,6 +1,7 @@
 //! Shell-specific errors.
 
 use alloc::string::String;
+use app_io::println;
 use task::RunState;
 
 pub type Result<T> = core::result::Result<T, Error>;
@@ -22,9 +23,27 @@ pub enum Error {
     /// Failed to kill a task.
     KillFailed,
     /// Failed to spawn a task.
-    SpawnFailed,
+    SpawnFailed(&'static str),
     /// Failed to unblock a task.
     ///
     /// The current runstate is stored in the field.
     UnblockFailed(RunState),
+}
+
+impl Error {
+    /// Prints this error if it is recoverable, otherwise returning the error.
+    pub(crate) fn print(self) -> Result<()> {
+        match self {
+            Error::ExitRequested => return Err(Error::ExitRequested),
+            Error::CurrentTaskUnavailable => return Err(Error::CurrentTaskUnavailable),
+            Error::Command(exit_code) => println!("exit {}", exit_code),
+            Error::CommandNotFound(command) => println!("{}: command not found", command),
+            Error::SpawnFailed(s) => println!("failed to spawn task: {s}"),
+            Error::KillFailed => println!("failed to kill task"),
+            Error::UnblockFailed(state) => {
+                println!("failed to unblock task with state {:?}", state)
+            }
+        }
+        Ok(())
+    }
 }
