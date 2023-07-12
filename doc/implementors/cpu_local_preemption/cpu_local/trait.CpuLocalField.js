c86d7aa334835b4f80240e74d465b6f971e7eb1a
kevinaboos <kevinaboos@users.noreply.github.com>
2023-07-12 17:59:26 +0000

    Replace `irq_safety` sync primitives with `sync_irq` (#1006)
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com> fe7d82a897622fc1ff680be435def2e0fa98dab7

--- doc/implementors/cpu_local_preemption/cpu_local/trait.CpuLocalField.js
+++ doc/implementors/cpu_local_preemption/cpu_local/trait.CpuLocalField.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "cpu_local_preemption":[],
 "per_cpu":[["impl CpuLocalField for <a class=\"struct\" href=\"per_cpu/struct.CpuLocalCpuId.html\" title=\"struct per_cpu::CpuLocalCpuId\">CpuLocalCpuId</a>"]],
-"task":[["impl CpuLocalField for <a class=\"struct\" href=\"task/struct.TaskSwitchPreemptionGuard.html\" title=\"struct task::TaskSwitchPreemptionGuard\">TaskSwitchPreemptionGuard</a>"],["impl CpuLocalField for <a class=\"struct\" href=\"task/struct.DropAfterTaskSwitch.html\" title=\"struct task::DropAfterTaskSwitch\">DropAfterTaskSwitch</a>"]]
+"task":[["impl CpuLocalField for <a class=\"struct\" href=\"task/struct.DropAfterTaskSwitch.html\" title=\"struct task::DropAfterTaskSwitch\">DropAfterTaskSwitch</a>"],["impl CpuLocalField for <a class=\"struct\" href=\"task/struct.TaskSwitchPreemptionGuard.html\" title=\"struct task::TaskSwitchPreemptionGuard\">TaskSwitchPreemptionGuard</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
