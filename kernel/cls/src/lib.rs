d71057fab14bfd9d381c03449d104ae38d071a4a
Klimenty Tsoutsman <klim@tsoutsman.com>
2023-08-04 09:37:32 +1000

    Add docs to `cls`
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com>

--- kernel/cls/src/lib.rs
+++ kernel/cls/src/lib.rs
@@ -8,13 +8,18 @@ extern crate alloc;
 
 pub use cls_macros::cpu_local;
 
-pub trait Guard: sealed::Sealed {}
+/// A trait abstracting over guards that ensure atomicity with respect to the
+/// current CPU.
+///
+/// This trait is "sealed" and cannot be implemented by anything outside this
+/// crate.
+pub trait CpuAtomicGuard: sealed::Sealed {}
 
 impl sealed::Sealed for irq_safety::HeldInterrupts {}
-impl Guard for irq_safety::HeldInterrupts {}
+impl CpuAtomicGuard for irq_safety::HeldInterrupts {}
 
 impl sealed::Sealed for preemption::PreemptionGuard {}
-impl Guard for preemption::PreemptionGuard {}
+impl CpuAtomicGuard for preemption::PreemptionGuard {}
 
 mod sealed {
     pub trait Sealed {}
