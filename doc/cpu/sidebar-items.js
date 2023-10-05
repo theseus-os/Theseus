4ed30408369394747dd774134f1d3343d8d902f3
kevinaboos <kevinaboos@users.noreply.github.com>
2023-10-05 04:49:35 +0000

    Add new, more robust scheduler test (#1045)
    * The new `test_scheduler` is significantly more robust than
      the old one. Currently, the test isn't particularly useful because
      we don't have task migration enabled, but #1042 will add
      implicit task migration when unblocking a task.
      * Hence, the test currently focuses on blocking/unblocking tasks.
    
    * Add a function to iterate over all initialized CPUs.
    
    Signed-off-by: Klimenty Tsoutsman <klim@tsoutsman.com> e9416d62bec3b68274a69f302102705894756def

--- doc/cpu/sidebar-items.js
+++ doc/cpu/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["bootstrap_cpu","cpu_count","current_cpu","is_bootstrap_cpu"],"struct":["CpuId","OptionalCpuId"]};
+window.SIDEBAR_ITEMS = {"fn":["bootstrap_cpu","cpu_count","cpus","current_cpu","is_bootstrap_cpu"],"struct":["CpuId","OptionalCpuId"]};
